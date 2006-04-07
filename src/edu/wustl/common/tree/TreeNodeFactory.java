b84874fcec705fed4c531e1eaab0319dfcb7179c
Gautam <Gautam>
2006-04-07 10:14:55 +0000

    Tree generation classes.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@6 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/tree/TreeNodeFactory.java
@@ -0,0 +1,43 @@
+/*
+ * Created on Aug 8, 2005
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package edu.wustl.common.tree;
+
+import edu.wustl.common.tree.TreeNodeData;
+import edu.wustl.common.tree.StorageContainerTreeNode;
+import edu.wustl.common.tree.TissueSiteTreeNode;
+import edu.wustl.common.tree.TreeNode;
+import edu.wustl.common.util.global.Constants;
+
+
+/**
+ * @author gautam_shetty
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class TreeNodeFactory
+{
+    public static TreeNode getTreeNode(int treeType)
+    {
+        TreeNode treeNode = null;
+        switch(treeType)
+        {
+            case Constants.TISSUE_SITE_TREE_ID:
+                treeNode = new TissueSiteTreeNode();
+            	break;
+            case Constants.STORAGE_CONTAINER_TREE_ID:
+                treeNode = new StorageContainerTreeNode();
+            	break;
+            case Constants.QUERY_RESULTS_TREE_ID:
+            	treeNode = new TreeNodeData();
+                break;
+        }
+        
+        return treeNode;
+    }
+
+}
