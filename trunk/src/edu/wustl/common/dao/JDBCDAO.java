1ab7231045db4dd1a69efc42f63f86211e5d03af
Aarti <Aarti>
2006-06-23 11:14:13 +0000

    changes to allow queries with 'DISTINCT' keyword
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@79 38c78225-d16a-144f-a689-2cd41fac0795

--- trunk/src/edu/wustl/common/dao/JDBCDAO.java
+++ trunk/src/edu/wustl/common/dao/JDBCDAO.java
@@ -183,19 +183,39 @@ public class JDBCDAO extends AbstractDAO
     }
     
     /**
+     * Returns the ResultSet containing all the rows according to the columns specified 
+     * from the table represented in sourceObjectName.
+     * @param sourceObjectName The table name.
+     * @param selectColumnName The column names in select clause.
+     * @param onlyDistictRows true if only distict rows should be selected
+     * @return The ResultSet containing all the rows according to the columns specified 
+     * from the table represented in sourceObjectName.
+     * @throws ClassNotFoundException
+     * @throws SQLException
+     */
+    public List retrieve(String sourceObjectName, String[] selectColumnName, boolean onlyDistinctRows)
+            throws DAOException
+    {
+    	Logger.out.debug(" Only distinct rows:"+onlyDistinctRows);
+        return retrieve(sourceObjectName, selectColumnName, null, null, null,
+                null,onlyDistinctRows);
+    }
+    
+    /**
      * Retrieves the records for class name in sourceObjectName according to field values passed in the passed session.
      * @param selectColumnName An array of field names in select clause.
      * @param whereColumnName An array of field names in where clause.
      * @param whereColumnCondition The comparision condition for the field values. 
      * @param whereColumnValue An array of field values.
      * @param joinCondition The join condition.
-     * @param The session object.
+     * @param onlyDistictRows true if only distict rows should be selected
      */
     public List retrieve(String sourceObjectName, String[] selectColumnName,
             String[] whereColumnName, String[] whereColumnCondition,
-            Object[] whereColumnValue, String joinCondition)
+            Object[] whereColumnValue, String joinCondition, boolean onlyDistinctRows)
             throws DAOException
     {
+    	Logger.out.debug(" Only distinct rows:"+onlyDistinctRows);
         List list = null;
         
         try
@@ -210,6 +230,13 @@ public class JDBCDAO extends AbstractDAO
             //Prepares the select clause of the query.
             if ((selectColumnName != null) && (selectColumnName.length > 0))
             {
+            	//Bug# 2003: Limiting the define view does not remove duplicates
+            	if(onlyDistinctRows)
+            	{
+            		Logger.out.debug(" Adding distinct to query " );
+            		query.append(" DISTINCT ");
+            	}
+            	//END Bug# 2003
                 int i;
                 for (i = 0; i < (selectColumnName.length - 1); i++)
                 {
@@ -252,6 +279,23 @@ public class JDBCDAO extends AbstractDAO
         }
         
         return list;
+    }
+    
+    /**
+     * Retrieves the records for class name in sourceObjectName according to field values passed in the passed session.
+     * @param selectColumnName An array of field names in select clause.
+     * @param whereColumnName An array of field names in where clause.
+     * @param whereColumnCondition The comparision condition for the field values. 
+     * @param whereColumnValue An array of field values.
+     * @param joinCondition The join condition.
+     * @param The session object.
+     */
+    public List retrieve(String sourceObjectName, String[] selectColumnName,
+            String[] whereColumnName, String[] whereColumnCondition,
+            Object[] whereColumnValue, String joinCondition)
+            throws DAOException
+    {
+    	return retrieve(sourceObjectName,selectColumnName,whereColumnName,whereColumnCondition,whereColumnValue,joinCondition,false);
     }
     
     /**
