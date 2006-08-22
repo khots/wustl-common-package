33b49d35ab1774f23d5640ed385f1a3471107979
srinivas_vassadi <srinivas_vassadi>
2006-08-22 05:27:02 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@109 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ trunk/src/edu/wustl/common/domain/Department.java
@@ -0,0 +1,100 @@
+/**
+ * <p>Title: Department Class</p>
+ * <p>Description: A department to which a User belongs to.  </p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Mandar Deshmukh
+ * @version 1.00
+ */
+
+package edu.wustl.common.domain;
+
+import edu.wustl.common.actionForm.DepartmentForm;
+import edu.wustl.common.actionForm.AbstractActionForm;
+import edu.wustl.common.domain.AbstractDomainObject;
+/**
+ * A department to which a User belongs to.
+ *  * @hibernate.class table="CATISSUE_DEPARTMENT"
+ */
+public class Department extends AbstractDomainObject implements java.io.Serializable
+{
+	private static final long serialVersionUID = 1234567890L;
+
+	/**
+	 * System generated unique systemIdentifier.
+	 */
+	protected Long systemIdentifier;
+
+	/**
+	 * Name of the department.
+	 */
+	protected String name;
+
+	/**
+	 * NOTE: Do not delete this constructor. Hibernet uses this by reflection API.
+	 * */
+	public Department()
+	{
+		
+	}
+	
+	public Department(AbstractActionForm form)
+	{
+		setAllValues(form);
+	}
+
+	/**
+	 * Returns the systemIdentifier assigned to department.
+	 * 
+	 * @hibernate.id name="systemIdentifier" column="IDENTIFIER" type="long"
+	 * length="30" unsaved-value="null" generator-class="native"
+	 * @hibernate.generator-param name="sequence" value="CATISSUE_DEPARTMENT_SEQ"
+	 * @return a unique systemIdentifier assigned to the department.
+	 */
+	public Long getSystemIdentifier()
+	{
+		return systemIdentifier;
+	}
+
+	/**
+	 * Sets an systemIdentifier for the department.
+	 * 
+	 * @param systemIdentifier Unique systemIdentifier to be assigned to the department.
+	 */
+	public void setSystemIdentifier(Long systemIdentifier)
+	{
+		this.systemIdentifier = systemIdentifier;
+	}
+
+	/**
+	 * Returns the name of the department.
+	 * 
+	 * @hibernate.property name="name" type="string" column="NAME" length="50"
+	 * not-null="true" unique="true"
+	 * @return name of the department.
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Sets the name of the department.
+	 * 
+	 * @param name Name of the department.
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+	/* (non-Javadoc)
+	 * @see edu.wustl.catissuecore.domain.AbstractDomainObject#setAllValues(edu.wustl.catissuecore.actionForm.AbstractActionForm)
+	 */
+	public void setAllValues(AbstractActionForm abstractForm) 
+	{
+		DepartmentForm departmentForm = (DepartmentForm)abstractForm;
+		
+		this.name = departmentForm.getName().trim();
+	}
+}
---
+++ trunk/src/edu/wustl/common/domain/Institution.java
@@ -0,0 +1,104 @@
+/**
+ * <p>Title: Institution Class</p>
+ * <p>Description:  An institution to which a user belongs to.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Mandar Deshmukh
+ * @version 1.00
+ */
+
+package edu.wustl.common.domain;
+
+import java.io.Serializable;
+
+import edu.wustl.common.actionForm.InstitutionForm;
+import edu.wustl.common.actionForm.AbstractActionForm;
+import edu.wustl.common.domain.AbstractDomainObject;
+
+/**
+ * An institution to which a user belongs to.
+ * @hibernate.class table="CATISSUE_INSTITUTION"
+ */
+public class Institution extends AbstractDomainObject implements Serializable
+{
+	private static final long serialVersionUID = 1234567890L;
+	
+	/**
+	 * System generated unique systemIdentifier.
+	 */
+	protected Long systemIdentifier;
+	
+	/**
+	 * Name of the Institution.
+	 */
+	protected String name;
+
+	/**
+	 * NOTE: Do not delete this constructor. Hibernet uses this by reflection API.
+	 * */
+	public Institution()
+	{
+		
+	}
+	
+	public Institution(AbstractActionForm form)
+	{
+		setAllValues(form);
+	}
+	
+	/**
+	 * Returns the unique systemIdentifier assigned to institution.
+	 * @hibernate.id name="systemIdentifier" column="IDENTIFIER" type="long"
+	 * length="30" unsaved-value="null" generator-class="native"
+	 * @hibernate.generator-param name="sequence" value="CATISSUE_INSTITUTION_SEQ"
+	 * @return A unique systemIdentifier assigned to the institution.
+	 * @see #setIdentifier(int)
+	 * */
+	public Long getSystemIdentifier()
+	{
+		return systemIdentifier;
+	}
+
+	/**
+	 * Sets an systemIdentifier for the institution.
+	 * @param systemIdentifier Unique systemIdentifier to be assigned to the institution.
+	 * @see #getIdentifier()
+	 * */
+	public void setSystemIdentifier(Long systemIdentifier)
+	{
+		this.systemIdentifier = systemIdentifier;
+	}
+
+	/**
+	 * Returns the name of the institution.
+	 * @hibernate.property name="name" type="string" 
+	 * column="NAME" length="50" not-null="true" unique="true"
+	 * @return Returns the name of the institution. 
+	 * @see #setName(String)
+	 */
+	public String getName()
+	{
+		return name;
+	}
+
+	/**
+	 * Sets the name of the institution.
+	 * @param name Name of the institution.
+	 * @see #getName()
+	 */
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	
+    /* (non-Javadoc)
+     * @see edu.wustl.catissuecore.domain.AbstractDomainObject#setAllValues(edu.wustl.catissuecore.actionForm.AbstractActionForm)
+     */
+    public void setAllValues(AbstractActionForm abstractForm)
+    {
+		InstitutionForm instituteForm = (InstitutionForm)abstractForm;
+		
+		this.name = instituteForm.getName().trim() ;
+    }
+}
