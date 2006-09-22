7489a4390eaf5514654469cc8fe54bb25305ddcc
santosh_chandak <santosh_chandak>
2006-09-22 09:09:12 +0000

    addded method String getValue(String theKey, List placeHolders)
    This method should be used when you want to customize error message with replacement parameters
    
    
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@157 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/global/ApplicationProperties.java
+++ src/edu/wustl/common/util/global/ApplicationProperties.java
@@ -2,8 +2,10 @@
  * Created on Jul 19, 2004
  *
  */
+
 package edu.wustl.common.util.global;
 
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -12,15 +14,36 @@ import java.util.ResourceBundle;
  */
 public class ApplicationProperties
 {
-    private static ResourceBundle bundle; 
-    
-    public static void initBundle(String baseName)
-    {
-    	bundle = ResourceBundle.getBundle(baseName);
-    }
-	
+
+	private static ResourceBundle bundle;
+
+	public static void initBundle(String baseName)
+	{
+		bundle = ResourceBundle.getBundle(baseName);
+	}
+
 	public static String getValue(String theKey)
 	{
 		return bundle.getString(theKey);
 	}
+
+/**
+ * This method should be used when you want to customize error message with replacement parameters
+ * 
+ * @param theKey - error key
+ * @param placeHolders - replacement Strings
+ * @return - complete error message
+ */	
+	public static String getValue(String theKey, List placeHolders)
+	{
+		String msg = bundle.getString(theKey);
+		StringBuffer message = new StringBuffer(msg);
+
+		for (int i = 0; i < placeHolders.size(); i++)
+		{
+			message.replace(message.indexOf("{"), message.indexOf("}") + 1, (String) placeHolders.get(i));
+		}
+		return message.toString();
+	}
+
 }
