7326ebbec0ae545f90d71d23211cafe26caefa53
ramya_nagraj <ramya_nagraj>
2006-10-19 13:06:56 +0000

    Implemented this class to have common funcitonalities of MouseInputListener
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@229 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/treeApplet/AppletTreeListener.java
@@ -0,0 +1,45 @@
+package edu.wustl.common.treeApplet;
+
+import java.awt.event.MouseEvent;
+
+import javax.swing.event.MouseInputListener;
+
+public class AppletTreeListener implements MouseInputListener 
+{
+
+	public void mouseClicked(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mousePressed(MouseEvent arg0) 
+	{
+	
+	}
+
+	public void mouseReleased(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mouseEntered(MouseEvent arg0) 
+	{
+	
+	}
+
+	public void mouseExited(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mouseDragged(MouseEvent arg0) 
+	{
+		
+	}
+
+	public void mouseMoved(MouseEvent arg0) 
+	{
+		
+	}
+
+}
