0631c7cdd67999e1956ebf821ba4aca09e53e177
santosh_chandak <santosh_chandak>
2006-11-24 04:59:16 +0000

    added constant for pageofaliquot
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@342 38c78225-d16a-144f-a689-2cd41fac0795

--- trunk/src/edu/wustl/common/util/global/Constants.java
+++ trunk/src/edu/wustl/common/util/global/Constants.java
@@ -472,4 +472,5 @@ public class Constants
 	public static final String SPECIMEN_COLLECTION_GROUP ="SpecimenCollectionGroup";
 	public static final String ALL = "All";
 	public static final int[] RESULT_PERPAGE_OPTIONS = {10,50,100,500,Integer.MAX_VALUE};
+	public static final String PAGEOF_ALIQUOT = "pageOfAliquot";
 }
