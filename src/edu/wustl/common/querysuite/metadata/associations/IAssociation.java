d1f1cfb5312ac1d17a1a77079b9f37c3e03fcb99
srinath_k <srinath_k>
2006-12-26 05:34:39 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/Common_Package_18_Oct_06_Branch@409 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/metadata/associations/IAssociation.java
+++ src/edu/wustl/common/querysuite/metadata/associations/IAssociation.java
@@ -4,13 +4,29 @@
 
 package edu.wustl.common.querysuite.metadata.associations;
 
-import edu.wustl.common.querysuite.queryobject.IBaseQueryObject;
+import java.io.Serializable;
+
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
 
 /**
  * @author prafull_kadam
  * Interface to represent Association between two classes.
  */
-public interface IAssociation extends IBaseQueryObject
+public interface IAssociation extends Serializable
 {
 
+	/**
+	 * @return true - if association is bidirectional, false otherwise.
+	 */
+	boolean isBidirectional();
+
+	/**
+	 * @return the source entity.
+	 */
+	EntityInterface getSourceEntity();
+
+	/**
+	 * @return the target entity.
+	 */
+	EntityInterface getTargetEntity();
 }
