354b6d9ffb56ff6412bc0e4836a74275e9ab52e8
Gautam <Gautam>
2007-01-04 10:00:35 +0000

    Written the XDoclets.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@436 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/Common_Package_18_Oct_06_Branch/src/edu/wustl/common/querysuite/metadata/category/CategorialAttribute.java
+++ branches/Common_Package_18_Oct_06_Branch/src/edu/wustl/common/querysuite/metadata/category/CategorialAttribute.java
@@ -1,3 +1,4 @@
+
 package edu.wustl.common.querysuite.metadata.category;
 
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
@@ -5,28 +6,112 @@ import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 /**
  * @version 1.0
  * @created 28-Dec-2006 2:01:13 AM
+ * @hibernate.class table="CATEGORIAL_ATTRIBUTE"
  */
-public class CategorialAttribute {
+public class CategorialAttribute
+{
 
-	private long id;
-	private long deCategoryAttributeId;
-	private long deSourceClassAttributeId;
-	public CategorialClass categorialEntity;
+    private Long id;
+    private AttributeInterface deCategoryAttribute;
+    private AttributeInterface deSourceClassAttribute;
+    private CategorialClass categorialClass;
 
-	public CategorialAttribute(){
+    /**
+     * @return Returns the categorialClass.
+     * @hibernate.many-to-one column="CATEGORIAL_CLASS_ID"
+     * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
+     */
+    public CategorialClass getCategorialClass()
+    {
+        return categorialClass;
+    }
 
-	}
+    /**
+     * @param categorialClass The categorialClass to set.
+     */
+    public void setCategorialClass(CategorialClass categorialEntity)
+    {
+        this.categorialClass = categorialEntity;
+    }
 
-	public void finalize() throws Throwable {
+    /**
+     * @return Returns the deCategoryAttribute.
+     * @hibernate.many-to-one column="DE_CATEGORY_ATTRIBUTE_ID"
+     * class="edu.common.dynamicextensions.domain.Attribute" constrained="true"
+     */
+    public AttributeInterface getDeCategoryAttribute()
+    {
+        return deCategoryAttribute;
+    }
 
-	}
+    /**
+     * @param deCategoryAttribute The deCategoryAttribute to set.
+     */
+    public void setDeCategoryAttribute(AttributeInterface deCategoryAttribute)
+    {
+        this.deCategoryAttribute = deCategoryAttribute;
+    }
 
-	public AttributeInterface getCategoryAttribute(){
-		return null;
-	}
+    /**
+     * @return Returns the deSourceClassAttribute.
+     * @hibernate.many-to-one column="DE_SOURCE_CLASS_ATTRIBUTE_ID"
+     * class="edu.common.dynamicextensions.domain.Attribute" constrained="true"
+     */
+    public AttributeInterface getDeSourceClassAttribute()
+    {
+        return deSourceClassAttribute;
+    }
 
-	public AttributeInterface getSourceClassAttribute(){
-		return null;
-	}
+    /**
+     * @param deSourceClassAttribute The deSourceClassAttribute to set.
+     */
+    public void setDeSourceClassAttribute(
+            AttributeInterface deSourceClassAttribute)
+    {
+        this.deSourceClassAttribute = deSourceClassAttribute;
+    }
+
+    /**
+     * @return Returns the id.
+     * @hibernate.id name="id" column="ID" type="long"
+     * length="30" unsaved-value="null" generator-class="native"
+     * @hibernate.generator-param name="sequence" value="CATEGORIAL_ATTRIBUTE_SEQ"
+     */
+    public Long getId()
+    {
+        return id;
+    }
+
+    /**
+     * @param id The id to set.
+     */
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    /**
+     * Default Constructor.
+     * Required for hibernate.
+     */
+    public CategorialAttribute()
+    {
+
+    }
+
+    public void finalize() throws Throwable
+    {
+
+    }
+
+    public AttributeInterface getCategoryAttribute()
+    {
+        return null;
+    }
+
+    public AttributeInterface getSourceClassAttribute()
+    {
+        return null;
+    }
 
 }
--- branches/Common_Package_18_Oct_06_Branch/src/edu/wustl/common/querysuite/metadata/category/Category.java
+++ branches/Common_Package_18_Oct_06_Branch/src/edu/wustl/common/querysuite/metadata/category/Category.java
@@ -1,45 +1,143 @@
+
 package edu.wustl.common.querysuite.metadata.category;
 
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.common.dynamicextensions.domain.Entity;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 
 /**
  * @version 1.0
  * @created 28-Dec-2006 2:01:13 AM
+ * @hibernate.class table="CATEGORY"
  */
-public class Category {
+public class Category
+{
 
-	private long id;
-	private long deEntityId;
-	public CategorialClass rootClass;
-	public Category subCategories;
+    private Long id;
+    private Entity deEntity;
+    private CategorialClass rootClass;
+    private Collection<Category> subCategories = new HashSet<Category>();
+    private Category parentCategory;
 
-	
-	public CategorialClass getRootClass()
-	{
-		return rootClass;
-	}
+    /**
+     * @hibernate.many-to-one column="DE_ENTITY_ID" class="edu.common.dynamicextensions.domain.Entity"
+     * constrained="true"
+     * @return Returns the deEntity.
+     */
+    public Entity getDeEntity()
+    {
+        return deEntity;
+    }
 
-	
-	public Category getSubCategories()
-	{
-		return subCategories;
-	}
+    /**
+     * @param deEntity The deEntity to set.
+     */
+    public void setDeEntity(Entity deEntity)
+    {
+        this.deEntity = deEntity;
+    }
 
-	public Category(){
+    /**
+     * @return Returns the id.
+     * @hibernate.id name="id" column="ID" type="long"
+     * length="30" unsaved-value="null" generator-class="native"
+     * @hibernate.generator-param name="sequence" value="CATEGORY_SEQ"
+     */
+    public Long getId()
+    {
+        return id;
+    }
 
-	}
+    /**
+     * @param id The id to set.
+     */
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
 
-	public void finalize() throws Throwable {
+    /**
+     * @return Returns the parentCategory.
+     * @hibernate.many-to-one column="PARENT_CATEGORY_ID" class="edu.wustl.common.querysuite.metadata.category.Category"
+     * constrained="true"
+     */
+    public Category getParentCategory()
+    {
+        return parentCategory;
+    }
 
-	}
+    /**
+     * @param parentCategory The parentCategory to set.
+     */
+    public void setParentCategory(Category parentCategory)
+    {
+        this.parentCategory = parentCategory;
+    }
 
-	public EntityInterface getCategory(){
-		return null;
-	}
-	
-	public Category clone() {
-		// TODO by Chandu
-		return null;
-	}
+    /**
+     * @param rootClass The rootClass to set.
+     */
+    public void setRootClass(CategorialClass rootClass)
+    {
+        this.rootClass = rootClass;
+    }
+
+    /**
+     * @param subCategories The subCategories to set.
+     */
+    public void setSubCategories(Set<Category> subCategories)
+    {
+        this.subCategories = subCategories;
+    }
+
+    /**
+     * Default constructor.
+     * Required for hibernate.
+     */
+    public Category()
+    {
+
+    }
+    
+    /**
+     * @return
+     * @hibernate.many-to-one column="ROOT_CATEGORIAL_CLASS_ID" 
+     * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
+     */
+    public CategorialClass getRootClass()
+    {
+        return rootClass;
+    }
+
+    /**
+     * @return
+     * @hibernate.set name="subCategories" table="CATEGORY"
+     * cascade="none" inverse="false" lazy="false"
+     * @hibernate.collection-key column="PARENT_CATEGORY_ID"
+     * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.Category"
+     */
+    public Collection<Category> getSubCategories()
+    {
+        return subCategories;
+    }
+
+    public void finalize() throws Throwable
+    {
+
+    }
+
+    public EntityInterface getCategory()
+    {
+        return null;
+    }
+
+    public Category clone()
+    {
+        // TODO by Chandu
+        return null;
+    }
 
 }
