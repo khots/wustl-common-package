ee999979c972231fec60b031f8a332c3fd2984f9
prafull_kadam <prafull_kadam>
2007-01-05 09:24:47 +0000

    Modified code to generate report of the query execution.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@442 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/Common_Package_18_Oct_06_Branch/test/edu/wustl/common/querysuite/ExecuteQueries.java
+++ branches/Common_Package_18_Oct_06_Branch/test/edu/wustl/common/querysuite/ExecuteQueries.java
@@ -1,11 +1,10 @@
-/**
- * 
- */
 
 package edu.wustl.common.querysuite;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -15,11 +14,13 @@ import java.sql.Statement;
 
 /**
  * @author prafull_kadam
+ * Class to execute queries for performance testing. 
  *
  */
 public class ExecuteQueries
 {
-
+	// To print all info on console set this attribute as true.
+	private static boolean DEBUG = true; 
 	private final static String DATABASE_MYSQL = "mysql";
 	private final static String DATABASE_ORACLE = "oracle";
 
@@ -32,7 +33,14 @@ public class ExecuteQueries
 
 	private static String sqlFile;
 	private static String tables[];
+	
+	private static BufferedWriter writer;
 
+	/**
+	 * To get the Connection object reference.
+	 * @return The reference to Connection object.
+	 * @throws Exception
+	 */
 	public static Connection getConnection() throws Exception
 	{
 		String driver = null;
@@ -85,6 +93,7 @@ public class ExecuteQueries
 
 			try
 			{
+				writer = new BufferedWriter(new FileWriter("report.txt"));
 				printSummary(stmt);
 				BufferedReader in = new BufferedReader(new FileReader(sqlFile));
 				String str;
@@ -93,6 +102,7 @@ public class ExecuteQueries
 					executeQuery(str, stmt);
 				}
 				in.close();
+				writer.close();
 			}
 			catch (IOException e)
 			{
@@ -120,35 +130,51 @@ public class ExecuteQueries
 		}
 	}
 
-	private static void printSummary(Statement stmt) throws SQLException
+	private static void printSummary(Statement stmt) throws SQLException, IOException
 	{
-		System.out.println("****************************************");
-		System.out.println("Database:" + database);
+		println("****************************************");
+		println("Database:" + database);
 		displayRecordCounts(stmt);
-		System.out.println("****************************************");
+		println("****************************************");
 	}
 
-	private static void displayRecordCounts(Statement stmt) throws SQLException
+	private static void displayRecordCounts(Statement stmt) throws SQLException, IOException
 	{
 		for (int i = 0; i < tables.length; i++)
 		{
 			ResultSet rs = stmt.executeQuery("select count(*) From " + tables[i]);
 			rs.next();
 			int noOfRecords = rs.getInt(1);
-			System.out.println(tables[i] + ":" + noOfRecords);
+			println(tables[i] + ":" + noOfRecords);
 			rs.close();
 		}
 	}
 
-	private static void executeQuery(String query, Statement stmt) throws SQLException
+	private static void executeQuery(String query, Statement stmt) throws SQLException, IOException
 	{
+		println("Executing Query:" + query);
 		long start = System.currentTimeMillis();
 		// execute query and return result as a ResultSet
+//		ResultSet rs = stmt.executeQuery("select count(*) from ("+query + ") aa");
 		ResultSet rs = stmt.executeQuery(query);
 		long end = System.currentTimeMillis();
-		System.out.println("Query:" + query);
-		System.out.println("Time in seconds:" + (end - start) / 1000.0);
-		System.out.println("--------------------------------------------------------");
+		println("Time in seconds:" + (end - start) / 1000.0);
+//		rs.next();
+//		int noOfRecords = rs.getInt(1);
+//		println("Total records fetched by Query:" + noOfRecords);
+		println("--------------------------------------------------------");
 		rs.close();
 	}
+	/**
+	 * To write message in logger & To print message on console.
+	 * @param message
+	 * @throws IOException 
+	 */
+	private static void println(String message) throws IOException
+	{
+		if (DEBUG)
+			System.out.println(message);
+		writer.write(message+"\n");
+	}
+	
 }
