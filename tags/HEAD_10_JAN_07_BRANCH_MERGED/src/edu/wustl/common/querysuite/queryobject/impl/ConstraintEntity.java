a13f913e6acd1b3389bce808cc93c4f2c3a95697
cvs2svn <cvs2svn>
2007-01-10 07:51:59 +0000

    This commit was manufactured by cvs2svn to create tag 'HEAD_10_JAN_07_BRANCH_MERGED'.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@467 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ tags/HEAD_10_JAN_07_BRANCH_MERGED/src/edu/wustl/common/querysuite/queryobject/impl/ConstraintEntity.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+
+package edu.wustl.common.querysuite.queryobject.impl;
+
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+import edu.wustl.common.querysuite.queryobject.IConstraintEntity;
+import edu.wustl.common.util.global.Constants;
+
+/**
+ * @author prafull_kadam
+ * Class implementation for IConstraintEntity.  
+ */
+public class ConstraintEntity extends QueryEntity implements IConstraintEntity
+{
+
+	private static final long serialVersionUID = 3442788139972273766L;
+
+	/**
+	 * To instanciate object of ConstraintEntity.
+	 * @param entityInterface The Dynamic Extension entity reference associated with this object. 
+	 */
+	public ConstraintEntity(EntityInterface entityInterface)
+	{
+		super(entityInterface);
+	}
+
+	/**
+	 * To check whether two objects are equal.
+	 * @param obj reference to the object to be checked for equality.
+	 * @return true if centityInterface of object is equal.
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == this)
+		{
+			return true;
+		}
+		if (obj != null && this.getClass() == obj.getClass())
+		{
+			return super.equals(obj);
+		}
+		return false;
+	}
+
+	/**
+	 * To get the HashCode for the object. It will be calculated based on entityInterface.
+	 * @return The hash code value for the object.
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode()
+	{
+		int hash = 1;
+		hash = hash * Constants.HASH_PRIME;
+		hash = hash * Constants.HASH_PRIME + super.hashCode();
+		return hash;
+	}
+
+	/**
+	 * @return String representation of this object
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString()
+	{
+		return "[ConstrainedEntity: " + super.toString() + "]";
+	}
+
+}
