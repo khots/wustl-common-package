6194923cf97fcc4dfd8a937e49c401449b0be48b
prafull_kadam <prafull_kadam>
2007-02-07 09:18:55 +0000

    Modified code so that: - the SQL generator wil create output tree structuraly equivalent to Constraints - it wil generate column name alias in the select part of the query. - Returns the Column name map. - updated corresponding testcases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@506 38c78225-d16a-144f-a689-2cd41fac0795

--- trunk/src/edu/wustl/common/querysuite/queryengine/ISqlGenerator.java
+++ trunk/src/edu/wustl/common/querysuite/queryengine/ISqlGenerator.java
@@ -4,6 +4,8 @@
 
 package edu.wustl.common.querysuite.queryengine;
 
+import java.util.Map;
+import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 import edu.wustl.common.querysuite.exceptions.MultipleRootsException;
 import edu.wustl.common.querysuite.exceptions.SqlException;
 import edu.wustl.common.querysuite.queryobject.IQuery;
@@ -24,4 +26,11 @@ public interface ISqlGenerator
 	 * @throws SqlException When there is error in the passed IQuery object.
 	 */
 	String generateSQL(IQuery query) throws MultipleRootsException, SqlException;
+	
+	/**
+	 * This method will return map of DE attributes verses & their column names present in the select part of the SQL. 
+	 * These DE attributes will be attributes of the each node present in the Output tree.
+	 * @return map of DE attributes verses & their column names present in the select part of the SQL.
+	 */
+	Map<Long, Map<AttributeInterface,String>> getColumnMap();
 }
