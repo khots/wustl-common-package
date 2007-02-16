4cf9a093953026f22da3aba4bea9457ec935d8f8
Aarti <Aarti>
2007-02-16 06:55:33 +0000

    Changes including latest caB2B jar and Resolve Ambiguity integration
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@511 38c78225-d16a-144f-a689-2cd41fac0795

--- trunk/src/edu/wustl/common/querysuite/queryengine/impl/CommonPathFinder.java
+++ trunk/src/edu/wustl/common/querysuite/queryengine/impl/CommonPathFinder.java
@@ -7,10 +7,10 @@ import java.util.Map;
 
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 import edu.wustl.cab2b.client.ui.query.IPathFinder;
-import edu.wustl.cab2b.common.path.PathInterface;
 import edu.wustl.cab2b.server.path.PathFinder;
 import edu.wustl.common.dao.DAOFactory;
 import edu.wustl.common.dao.JDBCDAO;
+import edu.wustl.common.querysuite.metadata.path.IPath;
 import edu.wustl.common.util.dbManager.DAOException;
 import edu.wustl.common.util.global.Constants;
 
@@ -21,19 +21,21 @@ import edu.wustl.common.util.global.Constants;
  */
 public class CommonPathFinder implements IPathFinder
 {
+	
 	/**
 	 * This method gets all the possible paths between two entities.
 	 */
 	public Map getAllPossiblePaths(List<EntityInterface> srcEntity, EntityInterface destEntity)
 	{
-		Map<EntityInterface, List<? extends PathInterface>> pathsMap = null;
+		Map<EntityInterface, List<IPath>> pathsMap = null;
 		JDBCDAO dao = (JDBCDAO) DAOFactory.getInstance().getDAO(Constants.JDBC_DAO);
 		Connection connection = null;
 		try
 		{
 			dao.openSession(null);
 			connection = dao.getConnection();
-			pathsMap = PathFinder.getAllPossiblePaths(srcEntity, destEntity, connection);
+			PathFinder pathFinder = (PathFinder) PathFinder.getInstance();
+			pathsMap = pathFinder.getAllPossiblePaths(srcEntity, destEntity, connection);
 		}
 		catch (DAOException e)
 		{
