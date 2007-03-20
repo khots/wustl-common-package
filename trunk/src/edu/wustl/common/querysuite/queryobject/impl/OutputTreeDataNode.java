d9752b0d6fdcacfbb9a6f6d9e2ab54fe48794b9d
sachin_lale <sachin_lale>
2007-03-20 12:28:53 +0000

    changes in return statement
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@551 38c78225-d16a-144f-a689-2cd41fac0795

--- trunk/src/edu/wustl/common/querysuite/queryobject/impl/OutputTreeDataNode.java
+++ trunk/src/edu/wustl/common/querysuite/queryobject/impl/OutputTreeDataNode.java
@@ -174,6 +174,6 @@ public class OutputTreeDataNode
 	public String toString()
 	{
 		return "[" + id + ":" + outputEntity.toString() + "("
-				+ (parentNode == null ? "-" : parentNode.getId()) + ")" + "]";
+				+ (parentNode == null ? "-" : parentNode.getId()+"") + ")" + "]";
 	}
 }
