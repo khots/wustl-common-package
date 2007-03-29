efd2729e977f102008221c801e6f1ca3372a211f
srinath_k <srinath_k>
2007-03-29 06:39:36 +0000

    *** empty log message ***
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@556 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/metadata/path/ICuratedPath.java
+++ src/edu/wustl/common/querysuite/metadata/path/ICuratedPath.java
@@ -1,5 +1,7 @@
+
 package edu.wustl.common.querysuite.metadata.path;
 
+import java.io.Serializable;
 import java.util.Set;
 
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
@@ -7,21 +9,26 @@ import edu.common.dynamicextensions.domaininterface.EntityInterface;
 /**
  * @author Chandrakant Talele
  */
-public interface ICuratedPath {
-    /**
-     * @return Returns the curatedPathId.
-     */
-    public long getCuratedPathId();
-    /**
-     * @return Returns the entitySet.
-     */
-    public Set<EntityInterface> getEntitySet();
-    /**
-     * @return Returns the isSelected.
-     */
-    public boolean isSelected();
-    /**
-     * @return Returns the paths.
-     */
-    public Set<Path> getPaths();
+public interface ICuratedPath extends Serializable
+{
+
+	/**
+	 * @return Returns the curatedPathId.
+	 */
+	public long getCuratedPathId();
+
+	/**
+	 * @return Returns the entitySet.
+	 */
+	public Set<EntityInterface> getEntitySet();
+
+	/**
+	 * @return Returns the isSelected.
+	 */
+	public boolean isSelected();
+
+	/**
+	 * @return Returns the paths.
+	 */
+	public Set<IPath> getPaths();
 }
