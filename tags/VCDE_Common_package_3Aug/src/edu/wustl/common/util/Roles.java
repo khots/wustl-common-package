e7d0a14cc6b351d01d38eed3bab1fba41f7ac68f
cvs2svn <cvs2svn>
2007-04-04 07:45:04 +0000

    This commit was manufactured by cvs2svn to create tag 'VCDE_Common_package_3Aug'.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@566 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ tags/VCDE_Common_package_3Aug/src/edu/wustl/common/util/Roles.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package edu.wustl.common.util;
+
+/**
+ * @author Srinivasarao_Vassadi
+ *
+ */
+public interface Roles {
+	
+	public static final String ADMINISTRATOR = "ADMINISTRATOR";
+	public static final String READ_ONLY = "READ_ONLY";
+    public static final String USE_ONLY = "USE_ONLY";
+    public static final String UPDATE_ONLY = "UPDATE_ONLY";
+
+}
