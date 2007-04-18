a96bb1d5e9ac5ca51a63d04d95b0b1c1dd2a7ff1
juberahamad_patel <juberahamad_patel>
2007-04-18 07:13:30 +0000

    if unique match is found, it is opened directly in edit mode
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@665 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/action/TitliSearchAction.java
+++ src/edu/wustl/common/action/TitliSearchAction.java
@@ -12,10 +12,12 @@ import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
 
 import titli.controller.interfaces.MatchListInterface;
+import titli.controller.interfaces.SortedResultMapInterface;
 import titli.controller.interfaces.TitliInterface;
 import titli.model.Titli;
 import titli.model.TitliException;
 import edu.wustl.common.actionForm.TitliSearchForm;
+import edu.wustl.common.util.TitliTableMapper;
 import edu.wustl.common.util.global.Constants;
 import edu.wustl.common.util.logger.Logger;
 
@@ -36,36 +38,67 @@ public class TitliSearchAction extends Action
 	 * @return action forward
 	 * 
 	 */
-	public ActionForward execute(ActionMapping mapping, ActionForm form,
-			HttpServletRequest request, HttpServletResponse response)
+	public ActionForward execute(ActionMapping mapping, ActionForm form,	HttpServletRequest request, HttpServletResponse response)
 	{
 
 		TitliSearchForm titliSearchForm = (TitliSearchForm) form;
-		System.out.println("Search string entered is :"
-				+ titliSearchForm.getSearchString());
+		System.out.println("Search string entered is :"+ titliSearchForm.getSearchString());
 
 		try
 		{
 
 			TitliInterface titli = Titli.getInstance();
-			MatchListInterface matchList = titli.search(titliSearchForm
-					.getSearchString().trim());
+			MatchListInterface matchList = titli.search(titliSearchForm.getSearchString().trim());
+			
+			SortedResultMapInterface sortedResultMap = matchList.getSortedResultMap();
 
 			// set the result in the action form
-			titliSearchForm.setSortedResultMap(matchList.getSortedResultMap());
-			request.getSession().setAttribute(Constants.TITLI_SORTED_RESULT_MAP,
-					matchList.getSortedResultMap());
+			titliSearchForm.setSortedResultMap(sortedResultMap);
+			request.getSession().setAttribute(Constants.TITLI_SORTED_RESULT_MAP,sortedResultMap);
 
 			titliSearchForm.setTimeTaken(matchList.getTimeTaken());
 			titliSearchForm.setNumberOfMatches(matchList.getNumberOfMatches());
+			
+			//if matches are from just one table, go directly to TitliFetchAction, skip TitliResultUpdatable.jsp 
+			if(sortedResultMap.size()==1)
+			{
+				try
+				{
+					String tableName = sortedResultMap.keySet().toArray(new String[0])[0];
+					String label = TitliTableMapper.getInstance().getLabel(tableName);
+					
+					//set the selectedLabel to the label of the only table
+					//setting the selectedLabel is necessary for getSelectedGroup() to work properly
+					titliSearchForm.setSelectedLabel(label);
+					
+				}
+				catch (Exception e)
+				{
+					Logger.out.error("Exception in TitliFetchAction : "	+ e.getMessage(), e);
+				}
+				
+				String path =Constants.TITLI_FETCH_ACTION;
+				return getActionForward(Constants.TITLI_SINGLE_RESULT, path);
+				
+			}
+		
 		}
 		catch (TitliException e)
 		{
-			Logger.out.error("TitliException in TitliSearchAction : "
-					+ e.getMessage(), e);
+			Logger.out.error("TitliException in TitliSearchAction : "+ e.getMessage(), e);
 		}
 
 		return mapping.findForward(Constants.SUCCESS);
 	}
+	
+	private ActionForward getActionForward(String name, String path)
+	{
+		ActionForward actionForward = new ActionForward();
+		actionForward.setName(name);
+		actionForward.setPath(path);
+
+		return actionForward;
+	}
+
 
 }
