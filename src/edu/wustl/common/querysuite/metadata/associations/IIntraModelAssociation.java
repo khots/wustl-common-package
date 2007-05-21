3ebee4c97a30b64bfa74c4688a2895e6807c94ff
srinath_k <srinath_k>
2007-05-21 09:57:07 +0000

    corrected return type of reverse() methods
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@701 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/metadata/associations/IIntraModelAssociation.java
+++ src/edu/wustl/common/querysuite/metadata/associations/IIntraModelAssociation.java
@@ -5,7 +5,6 @@
 package edu.wustl.common.querysuite.metadata.associations;
 
 import edu.common.dynamicextensions.domaininterface.AssociationInterface;
-import edu.wustl.common.querysuite.metadata.associations.impl.IntraModelAssociation;
 
 /**
  * @author prafull_kadam
@@ -24,5 +23,5 @@ public interface IIntraModelAssociation extends IAssociation
 	 * @return intramodel association wrapping de association that is reverse of this association.
 	 * @throws java.lang.IllegalArgumentException if this association is not bidirectional.
 	 */
-	public IntraModelAssociation reverse();
+	public IIntraModelAssociation reverse();
 }
