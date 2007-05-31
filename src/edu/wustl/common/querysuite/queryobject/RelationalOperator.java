4d057fa34921e1349dcbc62521843a2866242fe6
prafull_kadam <prafull_kadam>
2007-05-31 04:06:02 +0000

    modified for resolving checkstyle issues, done source formatting.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/trunk@713 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/queryobject/RelationalOperator.java
+++ src/edu/wustl/common/querysuite/queryobject/RelationalOperator.java
@@ -1,5 +1,5 @@
-package edu.wustl.common.querysuite.queryobject;
 
+package edu.wustl.common.querysuite.queryobject;
 
 /**
  * Enum:
@@ -15,25 +15,27 @@ package edu.wustl.common.querysuite.queryobject;
  * @updated 11-Oct-2006 02:57:23 PM
  */
 public enum RelationalOperator {
-	Equals(1,"Equals"), NotEquals(1,"Not Equals"), Between(2,"Between"), IsNull(0,"Is Null"), IsNotNull(0,"Is Not Null"), LessThan(1,"Less than"), LessThanOrEquals(1,"Less than or Equal to"), GreaterThan(
-			1,"Greater than"), GreaterThanOrEquals(1,"Greater than or Equal to"), In(Integer.MAX_VALUE,"In"), Contains(1,"Contains"), StartsWith(1,"Starts With"), EndsWith(
-			1,"Ends With"), NotIn(Integer.MAX_VALUE,"Not In");
+	Equals(1, "Equals"), NotEquals(1, "Not Equals"), Between(2, "Between"), IsNull(0, "Is Null"), IsNotNull(
+			0, "Is Not Null"), LessThan(1, "Less than"), LessThanOrEquals(1,
+			"Less than or Equal to"), GreaterThan(1, "Greater than"), GreaterThanOrEquals(1,
+			"Greater than or Equal to"), In(Integer.MAX_VALUE, "In"), Contains(1, "Contains"), StartsWith(
+			1, "Starts With"), EndsWith(1, "Ends With"), NotIn(Integer.MAX_VALUE, "Not In");
 
 	private int numberOfValuesRequired;
-    /**
-     * String Representation of the operator
-     */
-    private String stringRepresentation;
+	/**
+	 * String Representation of the operator
+	 */
+	private String stringRepresentation;
 
 	/**
 	 * 
 	 * @param numberOfValuesRequired numberOfValuesRequired to set.
 	 * @param stringRepresentation String Representation of the operator
 	 */
-	RelationalOperator(int numberOfValuesRequired,String stringRepresentation)
+	RelationalOperator(int numberOfValuesRequired, String stringRepresentation)
 	{
 		this.numberOfValuesRequired = numberOfValuesRequired;
-        this.stringRepresentation = stringRepresentation;
+		this.stringRepresentation = stringRepresentation;
 	}
 
 	/**
@@ -95,23 +97,29 @@ public enum RelationalOperator {
 		}
 		return sql;
 	}
-    /**
-     * @return The String Representation of the enumeration.
-     */
-    public String getStringRepresentation() {
-        return stringRepresentation;
-    }
-    /**
-     * @param stringRepresentation String Representation
-     * @return Returns the RelationalOperator which has given string representation
-     */
-    public static RelationalOperator getOperatorForStringRepresentation(String stringRepresentation) {
-        RelationalOperator relationalOpr = null;
-        for (RelationalOperator relationalOperator : RelationalOperator.values()) {
-            if (relationalOperator.getStringRepresentation().equals(stringRepresentation)) {
-                relationalOpr =  relationalOperator;
-            }
-        }
-        return relationalOpr;
-    }
+
+	/**
+	 * @return The String Representation of the enumeration.
+	 */
+	public String getStringRepresentation()
+	{
+		return stringRepresentation;
+	}
+
+	/**
+	 * @param stringRepresentation String Representation
+	 * @return Returns the RelationalOperator which has given string representation
+	 */
+	public static RelationalOperator getOperatorForStringRepresentation(String stringRepresentation)
+	{
+		RelationalOperator relationalOpr = null;
+		for (RelationalOperator relationalOperator : RelationalOperator.values())
+		{
+			if (relationalOperator.getStringRepresentation().equals(stringRepresentation))
+			{
+				relationalOpr = relationalOperator;
+			}
+		}
+		return relationalOpr;
+	}
 }
