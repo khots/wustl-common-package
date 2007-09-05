9014865ba9ece81731e0d8cf149c9b8f06afa809
chetan_patil <chetan_patil>
2007-09-05 13:21:37 +0000

    Corrected hibernate annotation to match manually edited hbm
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@818 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/metadata/category/CategorialClass.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/metadata/category/CategorialClass.java
@@ -1,4 +1,3 @@
-
 package edu.wustl.common.querysuite.metadata.category;
 
 import java.io.Serializable;
@@ -14,221 +13,194 @@ import edu.wustl.common.querysuite.metadata.path.IPath;
  * @created 28-Dec-2006 2:01:13 AM
  * @hibernate.class table="CATEGORIAL_CLASS"
  */
-public class CategorialClass implements Serializable
-{
+public class CategorialClass implements Serializable {
+    private static final long serialVersionUID = -136227480173654340L;
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -136227480173654340L;
-	private Long id;
-	private Long deEntityId;
-	private Category category;
-	private Set<CategorialAttribute> categorialAttributeCollection = new HashSet<CategorialAttribute>();
-	private Set<CategorialClass> children = new HashSet<CategorialClass>();
-	private CategorialClass parent;
-	private Long pathFromParentId;
-	private EntityInterface categorialClassEntity;
-	private IPath pathFromParent;
+    private Long id;
 
-	/**
-	 * @return the pathFromParent.
-	 */
-	public IPath getPathFromParent()
-	{
-		return pathFromParent;
-	}
+    private Long deEntityId;
 
-	/**
-	 * @param pathFromParent the pathFromParent to set.
-	 */
-	public void setPathFromParent(IPath pathFromParent)
-	{
-		this.pathFromParent = pathFromParent;
-	}
+    private Category category;
 
-	/**
-	 * @return Returns the id.
-	 * @hibernate.id name="id" column="ID" type="long"
-	 * length="30" unsaved-value="null" generator-class="native"
-	 * @hibernate.generator-param name="sequence" value="CATEGORIAL_CLASS_SEQ"
-	 */
-	public Long getId()
-	{
-		return id;
-	}
+    private Set<CategorialAttribute> categorialAttributeCollection = new HashSet<CategorialAttribute>();
 
-	/**
-	 * @return Returns the category.
-	 * @hibernate.many-to-one column="CATEGORY_ID" class="edu.wustl.common.querysuite.metadata.category.Category"
-	 * constrained="true"
-	 */
-	public Category getCategory()
-	{
-		return category;
-	}
+    private Set<CategorialClass> children = new HashSet<CategorialClass>();
 
-	/**
-	 * @param category The category to set.
-	 */
-	public void setCategory(Category category)
-	{
-		this.category = category;
-	}
+    private CategorialClass parent;
 
-	/**
-	 * @return Returns the deEntity.
-	 * @hibernate.property name="deEntityId" type="long" column="DE_ENTITY_ID"
-	 */
-	public Long getDeEntityId()
-	{
-		return deEntityId;
-	}
+    private Long pathFromParentId;
 
-	/**
-	 * @param deEntityId The deEntity to set.
-	 */
-	public void setDeEntityId(Long deEntityId)
-	{
-		this.deEntityId = deEntityId;
-	}
+    private EntityInterface categorialClassEntity;
 
-	/**
-	 * @param id The id to set.
-	 */
-	public void setId(Long id)
-	{
-		this.id = id;
-	}
+    private IPath pathFromParent;
 
-	/**
-	 * @param categorialAttributeCollection The categorialAttributeCollection to set.
-	 */
-	public void setCategorialAttributeCollection(
-			Set<CategorialAttribute> categorialAttributeCollection)
-	{
-		this.categorialAttributeCollection = categorialAttributeCollection;
-	}
+    /**
+     * @return the pathFromParent.
+     */
+    public IPath getPathFromParent() {
+        return pathFromParent;
+    }
 
-	/**
-	 * @param children The children to set.
-	 */
-	public void setChildren(Set<CategorialClass> children)
-	{
-		this.children = children;
-	}
+    /**
+     * @param pathFromParent the pathFromParent to set.
+     */
+    public void setPathFromParent(IPath pathFromParent) {
+        this.pathFromParent = pathFromParent;
+    }
 
-	public CategorialClass()
-	{
+    /**
+     * @return Returns the id.
+     * @hibernate.id name="id" column="ID" type="long"
+     * length="30" unsaved-value="null" generator-class="native"
+     * @hibernate.generator-param name="sequence" value="CATEGORIAL_CLASS_SEQ"
+     */
+    public Long getId() {
+        return id;
+    }
 
-	}
+    /**
+     * @return Returns the category.
+     * @hibernate.one-to-one column="CATEGORY_ID" class="edu.wustl.common.querysuite.metadata.category.Category" property-ref="rootClass" constrained="true"
+     */
+    public Category getCategory() {
+        return category;
+    }
 
-	/**
-	 * @return
-	 * @hibernate.set name="categorialAttributeCollection" table="CATEGORIAL_ATTRIBUTE"
-	 * cascade="none" inverse="false" lazy="false"
-	 * @hibernate.collection-key column="CATEGORIAL_CLASS_ID"
-	 * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.CategorialAttribute"
-	 */
-	public Set<CategorialAttribute> getCategorialAttributeCollection()
-	{
-		return categorialAttributeCollection;
-	}
+    /**
+     * @param category The category to set.
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+    }
 
-	/**
-	 * @return
-	 * @hibernate.set name="children" table="CATEGORIAL_CLASS"
-	 * cascade="none" inverse="false" lazy="false"
-	 * @hibernate.collection-key column="PARENT_CATEGORIAL_CLASS_ID"
-	 * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.CategorialClass"
-	 */
-	public Set<CategorialClass> getChildren()
-	{
-		return children;
-	}
+    /**
+     * @return Returns the deEntity.
+     * @hibernate.property name="deEntityId" type="long" column="DE_ENTITY_ID"
+     */
+    public Long getDeEntityId() {
+        return deEntityId;
+    }
 
-	/**
-	 * @return
-	 * @hibernate.property name="pathFromParentId" type="long" column="PATH_FROM_PARENT_ID"
-	 */
-	public Long getPathFromParentId()
-	{
-		return pathFromParentId;
-	}
+    /**
+     * @param deEntityId The deEntity to set.
+     */
+    public void setDeEntityId(Long deEntityId) {
+        this.deEntityId = deEntityId;
+    }
 
-	/**
-	 * @hibernate.many-to-one column="PARENT_CATEGORIAL_CLASS_ID" 
-	 * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
-	 */
-	public CategorialClass getParent()
-	{
-		return parent;
-	}
+    /**
+     * @param id The id to set.
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public AttributeInterface findSourceAttribute(AttributeInterface categoryAttribute)
-	{
-		for (CategorialAttribute categorialAttribute : getCategorialAttributeCollection())
-		{
-			if (categoryAttribute.equals(categorialAttribute.getCategoryAttribute()))
-			{
-				return categorialAttribute.getSourceClassAttribute();
-			}
-		}
-		return null;
-	}
+    /**
+     * @param categorialAttributeCollection The categorialAttributeCollection to set.
+     */
+    public void setCategorialAttributeCollection(Set<CategorialAttribute> categorialAttributeCollection) {
+        this.categorialAttributeCollection = categorialAttributeCollection;
+    }
 
-	public void addChildCategorialClass(CategorialClass child, IPath pathToChild)
-	{
-		children.add(child);
-		child.setParent(this);
-		child.setPathFromParent(pathToChild);
-	}
+    /**
+     * @param children The children to set.
+     */
+    public void setChildren(Set<CategorialClass> children) {
+        this.children = children;
+    }
 
-	public void addChildCategorialClass(CategorialClass child)
-	{
-		children.add(child);
-		child.setParent(this);
-	}
+    public CategorialClass() {
 
-	// rets true if the child was found; false otherwise.
-	public boolean removeChildCategorialClass(CategorialClass child)
-	{
-		return children.remove(child);
-	}
+    }
 
-	public void setParent(CategorialClass parent)
-	{
-		this.parent = parent;
-	}
+    /**
+     * @return
+     * @hibernate.set name="categorialAttributeCollection" table="CATEGORIAL_ATTRIBUTE" cascade="save-update" inverse="false" lazy="false"
+     * @hibernate.collection-key column="CATEGORIAL_CLASS_ID"
+     * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.CategorialAttribute"
+     */
+    public Set<CategorialAttribute> getCategorialAttributeCollection() {
+        return categorialAttributeCollection;
+    }
 
-	public void setPathFromParentId(Long pathFromParentId)
-	{
-		this.pathFromParentId = pathFromParentId;
-	}
+    /**
+     * @return
+     * @hibernate.set name="children" table="CATEGORIAL_CLASS" cascade="save-update" inverse="false" lazy="false"
+     * @hibernate.collection-key column="PARENT_CATEGORIAL_CLASS_ID"
+     * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.CategorialClass"
+     */
+    public Set<CategorialClass> getChildren() {
+        return children;
+    }
 
-	public void addCategorialAttribute(CategorialAttribute categorialAttribute)
-	{
-		if (categorialAttributeCollection == null)
-		{
-			categorialAttributeCollection = new HashSet<CategorialAttribute>();
-		}
-		categorialAttributeCollection.add(categorialAttribute);
-		categorialAttribute.setCategorialClass(this);
-	}
+    /**
+     * @return
+     * @hibernate.property name="pathFromParentId" type="long" column="PATH_FROM_PARENT_ID"
+     */
+    public Long getPathFromParentId() {
+        return pathFromParentId;
+    }
 
-	/**
-	 * @return Returns the categorialClassEntity.
-	 */
-	public EntityInterface getCategorialClassEntity()
-	{
-		return categorialClassEntity;
-	}
+    /**
+     * @hibernate.many-to-one column="PARENT_CATEGORIAL_CLASS_ID" 
+     * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
+     */
+    public CategorialClass getParent() {
+        return parent;
+    }
 
-	/**
-	 * @param categorialClassEntity The categorialClassEntity to set.
-	 */
-	public void setCategorialClassEntity(EntityInterface categorialClassEntity)
-	{
-		this.categorialClassEntity = categorialClassEntity;
-	}
+    public AttributeInterface findSourceAttribute(AttributeInterface categoryAttribute) {
+        for (CategorialAttribute categorialAttribute : getCategorialAttributeCollection()) {
+            if (categoryAttribute.equals(categorialAttribute.getCategoryAttribute())) {
+                return categorialAttribute.getSourceClassAttribute();
+            }
+        }
+        return null;
+    }
+
+    public void addChildCategorialClass(CategorialClass child, IPath pathToChild) {
+        children.add(child);
+        child.setParent(this);
+        child.setPathFromParent(pathToChild);
+    }
+
+    public void addChildCategorialClass(CategorialClass child) {
+        children.add(child);
+        child.setParent(this);
+    }
+
+    // rets true if the child was found; false otherwise.
+    public boolean removeChildCategorialClass(CategorialClass child) {
+        return children.remove(child);
+    }
+
+    public void setParent(CategorialClass parent) {
+        this.parent = parent;
+    }
+
+    public void setPathFromParentId(Long pathFromParentId) {
+        this.pathFromParentId = pathFromParentId;
+    }
+
+    public void addCategorialAttribute(CategorialAttribute categorialAttribute) {
+        if (categorialAttributeCollection == null) {
+            categorialAttributeCollection = new HashSet<CategorialAttribute>();
+        }
+        categorialAttributeCollection.add(categorialAttribute);
+        categorialAttribute.setCategorialClass(this);
+    }
+
+    /**
+     * @return Returns the categorialClassEntity.
+     */
+    public EntityInterface getCategorialClassEntity() {
+        return categorialClassEntity;
+    }
+
+    /**
+     * @param categorialClassEntity The categorialClassEntity to set.
+     */
+    public void setCategorialClassEntity(EntityInterface categorialClassEntity) {
+        this.categorialClassEntity = categorialClassEntity;
+    }
 }
