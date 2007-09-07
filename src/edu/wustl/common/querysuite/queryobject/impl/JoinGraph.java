78bb79c443d32c1ef12376c7a69eef8bc791c746
prafull_kadam <prafull_kadam>
2007-09-07 10:00:58 +0000

    Added getIntermediateExpreesionIds() method, required for DAG Flex Interface.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@820 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/queryobject/impl/JoinGraph.java
+++ src/edu/wustl/common/querysuite/queryobject/impl/JoinGraph.java
@@ -216,6 +216,55 @@ public class JoinGraph extends BaseQueryObject implements IJoinGraph {
     }
 
     /**
+     * To the the List of intermediate ExpressionId between the given soure & target ExpressionIds with the given List of association.
+     * @param source The Source Expression Id.
+     * @param target The Target ExpressionId.
+     * @param associations The List of Associations.
+     * @return List of intermediate ExpressionIds for matching List of Associations.
+     * 			null, if there is no path between source & target or There is no matching path with the given associations. 
+     */
+    public List<IExpressionId> getIntermediateExpressions(IExpressionId source, IExpressionId target, List<IAssociation> associations) {
+    	//getting path between two vertices.
+    	List<List<IExpressionId>> reachablePaths = graph.getReachablePaths(source, target);
+
+        for(List<IExpressionId> rechablePath :reachablePaths)
+        {
+        	// compairing path size & the association list size. 
+        	// This path includes source & target ExprssionIds.
+        	// For maching path, if there are 2 associations in list, there there will be 3 expressions in obtained path.
+        	if (rechablePath.size() == associations.size()+1)
+        	{
+            	// check whether its matching ot not.
+            	boolean isMatching = true;
+        		for(int index=0; index < rechablePath.size()-1; index++)
+        		{
+            		IExpressionId fromExpId = rechablePath.get(index);
+        			IExpressionId toExpId = rechablePath.get(index+1);
+        			//check for association match
+        			if (associations.get(index).equals(graph.getEdge(fromExpId, toExpId)))
+        			{
+        				fromExpId = toExpId;
+        			}
+        			else
+        			{
+        				//association is not matching, so this path is not the required path.
+        				isMatching = false;
+        				break;	
+        			}
+        		}
+        		if (isMatching)
+        		{
+        			// matching path  found, so logic terminates here.....Their will be only one matching path.
+        			return rechablePath;
+        		}
+        	}
+        }
+        
+        // No matching path found !!!!
+		return null;
+    }
+    
+    /**
      * To get the first path of the  given ExpressionId from the root Expression.
      * @param expressionId regerence to ExpressionId 
      * @return the List of vertices representing path of the given ExpressionId from root. returns empty path list if there is no path.
