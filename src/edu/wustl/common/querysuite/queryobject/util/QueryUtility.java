3d914935ddf62db89b3e1e41b026716bf2147e22
chetan_patil <chetan_patil>
2007-09-21 13:30:56 +0000

    Corrected the method
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@851 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/queryobject/util/QueryUtility.java
+++ src/edu/wustl/common/querysuite/queryobject/util/QueryUtility.java
@@ -3,10 +3,10 @@
  */
 package edu.wustl.common.querysuite.queryobject.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
@@ -25,29 +25,35 @@ import edu.wustl.common.querysuite.queryobject.IRule;
  * @created Sep 20, 2007, 12:07:59 PM
  */
 public class QueryUtility {
+
     /**
      * This method returns all the selected Condition for a given query.  
      * @param query
      * @return Map of ExpressionId -> Collection of Condition
      */
-    public static Map<IExpressionId, Collection<ICondition>> getSelectedConditions(IQuery query) {
-        Map<IExpressionId, Collection<ICondition>> expressionIdConditionCollectionMap = null;
+    public static Map<IExpressionId, Map<AttributeInterface, ICondition>> getSelectedConditions(IQuery query) {
+        HashMap<IExpressionId, Map<AttributeInterface, ICondition>> expressionIdConditionCollectionMap = null;
         if (query != null) {
-            expressionIdConditionCollectionMap = new HashMap<IExpressionId, Collection<ICondition>>();
-            
+            expressionIdConditionCollectionMap = new HashMap<IExpressionId, Map<AttributeInterface, ICondition>>();
+
             IConstraints constraints = query.getConstraints();
             Enumeration<IExpressionId> expressionIds = constraints.getExpressionIds();
             while (expressionIds.hasMoreElements()) {
                 IExpressionId expressionId = expressionIds.nextElement();
-
                 IExpression expression = constraints.getExpression(expressionId);
+
                 for (int index = 0; index < expression.numberOfOperands(); index++) {
                     IExpressionOperand expressionOperand = expression.getOperand(index);
                     if (!expressionOperand.isSubExpressionOperand()) {
                         IRule rule = (IRule) expressionOperand;
-                        Collection<ICondition> conditionList = rule.getConditions();
 
-                        expressionIdConditionCollectionMap.put(expression.getExpressionId(), conditionList);
+                        Map<AttributeInterface, ICondition> attributeConditionMap = new HashMap<AttributeInterface, ICondition>();
+                        Collection<ICondition> conditionList = rule.getConditions();
+                        for (ICondition condition : conditionList) {
+                            attributeConditionMap.put(condition.getAttribute(), condition);
+                        }
+
+                        expressionIdConditionCollectionMap.put(expression.getExpressionId(), attributeConditionMap);
                     }
                 }
             }
@@ -65,7 +71,7 @@ public class QueryUtility {
         Map<IExpressionId, Collection<AttributeInterface>> expressionIdAttributeCollectionMap = null;
         if (query != null) {
             expressionIdAttributeCollectionMap = new HashMap<IExpressionId, Collection<AttributeInterface>>();
-            
+
             IConstraints constraints = query.getConstraints();
             Enumeration<IExpressionId> expressionIds = constraints.getExpressionIds();
             while (expressionIds.hasMoreElements()) {
@@ -75,7 +81,7 @@ public class QueryUtility {
                 IQueryEntity queryEntity = expression.getQueryEntity();
                 EntityInterface deEntity = queryEntity.getDynamicExtensionsEntity();
                 Collection<AttributeInterface> attributeCollection = deEntity.getAttributeCollection();
-                
+
                 expressionIdAttributeCollectionMap.put(expression.getExpressionId(), attributeCollection);
             }
         }
