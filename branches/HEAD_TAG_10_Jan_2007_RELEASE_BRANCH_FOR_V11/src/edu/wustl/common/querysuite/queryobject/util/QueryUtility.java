f4158e99824f4f50255a261e522a2e48832e90fa
chetan_patil <chetan_patil>
2007-10-09 12:28:02 +0000

    Added new method
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@890 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/queryobject/util/QueryUtility.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/queryobject/util/QueryUtility.java
@@ -3,6 +3,7 @@
  */
 package edu.wustl.common.querysuite.queryobject.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
 import java.util.HashMap;
@@ -15,6 +16,7 @@ import edu.wustl.common.querysuite.queryobject.IConstraints;
 import edu.wustl.common.querysuite.queryobject.IExpression;
 import edu.wustl.common.querysuite.queryobject.IExpressionId;
 import edu.wustl.common.querysuite.queryobject.IExpressionOperand;
+import edu.wustl.common.querysuite.queryobject.IParameterizedCondition;
 import edu.wustl.common.querysuite.queryobject.IQuery;
 import edu.wustl.common.querysuite.queryobject.IQueryEntity;
 import edu.wustl.common.querysuite.queryobject.IRule;
@@ -34,7 +36,7 @@ public class QueryUtility {
         Map<IExpressionId, Collection<ICondition>> expressionIdConditionCollectionMap = null;
         if (query != null) {
             expressionIdConditionCollectionMap = new HashMap<IExpressionId, Collection<ICondition>>();
-            
+
             IConstraints constraints = query.getConstraints();
             Enumeration<IExpressionId> expressionIds = constraints.getExpressionIds();
             while (expressionIds.hasMoreElements()) {
@@ -48,6 +50,48 @@ public class QueryUtility {
                         Collection<ICondition> conditionList = rule.getConditions();
 
                         expressionIdConditionCollectionMap.put(expression.getExpressionId(), conditionList);
+                    }
+                }
+            }
+        }
+
+        return expressionIdConditionCollectionMap;
+    }
+
+    /**
+     * This method returns all the selected Condition for a given query.  
+     * @param query
+     * @return Map of ExpressionId -> Collection of Condition
+     */
+    public static Map<IExpressionId, Collection<IParameterizedCondition>> getAllParameterizedConditions(
+                                                                                                        IQuery query) {
+        Map<IExpressionId, Collection<IParameterizedCondition>> expressionIdConditionCollectionMap = null;
+        if (query != null) {
+            expressionIdConditionCollectionMap = new HashMap<IExpressionId, Collection<IParameterizedCondition>>();
+
+            IConstraints constraints = query.getConstraints();
+            Enumeration<IExpressionId> expressionIds = constraints.getExpressionIds();
+            while (expressionIds.hasMoreElements()) {
+                IExpressionId expressionId = expressionIds.nextElement();
+
+                IExpression expression = constraints.getExpression(expressionId);
+                for (int index = 0; index < expression.numberOfOperands(); index++) {
+                    IExpressionOperand expressionOperand = expression.getOperand(index);
+                    if (!expressionOperand.isSubExpressionOperand()) {
+                        IRule rule = (IRule) expressionOperand;
+                        
+                        Collection<IParameterizedCondition> parameterizedConditions = new ArrayList<IParameterizedCondition>();
+
+                        Collection<ICondition> conditionList = rule.getConditions();
+                        for (ICondition condition : conditionList) {
+                            if (condition instanceof IParameterizedCondition) {
+                                parameterizedConditions.add((IParameterizedCondition) condition);
+                            }
+                        }
+                        if (!parameterizedConditions.isEmpty()) {
+                            expressionIdConditionCollectionMap.put(expression.getExpressionId(),
+                                                                   parameterizedConditions);
+                        }
                     }
                 }
             }
