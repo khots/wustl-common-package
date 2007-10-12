230a4613d2c131697e73754142a374e50cb495b3
Rahul_Ner <Rahul_Ner>
2007-10-12 06:37:39 +0000

    method added that provides the specific value
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@895 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/queryobject/DataType.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/queryobject/DataType.java
@@ -1,4 +1,3 @@
-
 package edu.wustl.common.querysuite.queryobject;
 
 /**
@@ -7,7 +6,59 @@ package edu.wustl.common.querysuite.queryobject;
  * @updated 11-Oct-2006 02:55:25 PM
  */
 public enum DataType {
-	String, Date, Integer, Long, Double, Boolean, Float;
 
-	// TODO add more
+    String(java.lang.String.class) {
+    },
+    Date(java.util.Date.class) {
+    },
+    Integer(java.lang.Integer.class) {
+        public Integer convertValue(String value) {
+            return java.lang.Integer.valueOf(value);
+        }
+    },
+    Long(java.lang.Long.class) {
+
+        public Long convertValue(String value) {
+            return java.lang.Long.valueOf(value);
+        }
+    },
+    Double(java.lang.Double.class) {
+
+        public Double convertValue(String value) {
+            return java.lang.Double.valueOf(value);
+        }
+    },
+    Boolean(java.lang.Boolean.class) {
+
+        public Boolean convertValue(String value) {
+            return java.lang.Boolean.valueOf(value);
+        }
+    },
+    Float(java.lang.Float.class) {
+
+        public Float convertValue(String value) {
+            return java.lang.Float.valueOf(value);
+        }
+    };
+    private Class javaType;
+
+    private DataType(Class javaType) {
+        this.javaType = javaType;
+    }
+
+    /**
+     * @return Returns the javaType.
+     */
+    public Class getJavaType() {
+        return javaType;
+    }
+
+    /**
+     * This method returns 
+     * @param value
+     * @return
+     */
+    public Object convertValue(String value) {
+        return value;
+    }
 }
