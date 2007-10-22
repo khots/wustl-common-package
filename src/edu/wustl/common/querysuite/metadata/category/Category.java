d0b97b29941453ebf84364475195e30620fb050d
atul_jawale <atul_jawale>
2007-10-22 05:55:27 +0000

    Copy() method added to create a copy of the existing object.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@903 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/metadata/category/Category.java
+++ src/edu/wustl/common/querysuite/metadata/category/Category.java
@@ -1,4 +1,3 @@
-
 package edu.wustl.common.querysuite.metadata.category;
 
 import java.io.Serializable;
@@ -12,117 +11,138 @@ import edu.common.dynamicextensions.domaininterface.EntityInterface;
  * @created 28-Dec-2006 2:01:13 AM
  * @hibernate.class table="CATEGORY"
  */
-public class Category implements Serializable
-{
+public class Category implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -4899537928159969695L;
-	private Long id;
-	private Long deEntityId;
-	private CategorialClass rootClass;
-	private Set<Category> subCategories = new HashSet<Category>();
-	private Category parentCategory;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -4899537928159969695L;
+
+    private Long id;
+
+    private Long deEntityId;
+
+    private CategorialClass rootClass;
+
+    private Set<Category> subCategories = new HashSet<Category>();
+
+    private Category parentCategory;
+
     private EntityInterface categoryEntity;
-	/**
-	 * @hibernate.property name="deEntityId" type="long" column="DE_ENTITY_ID"
-	 * @return Returns the deEntity.
-	 */
-	public Long getDeEntityId()
-	{
-		return deEntityId;
-	}
 
-	/**
-	 * @param deEntityId The deEntity to set.
-	 */
-	public void setDeEntityId(Long deEntityId)
-	{
-		this.deEntityId = deEntityId;
-	}
+    /**
+     * @hibernate.property name="deEntityId" type="long" column="DE_ENTITY_ID"
+     * @return Returns the deEntity.
+     */
+    public Long getDeEntityId() {
+        return deEntityId;
+    }
 
-	/**
-	 * @return Returns the id.
-	 * @hibernate.id name="id" column="ID" type="long"
-	 * length="30" unsaved-value="null" generator-class="native"
-	 * @hibernate.generator-param name="sequence" value="CATEGORY_SEQ"
-	 */
-	public Long getId()
-	{
-		return id;
-	}
+    /**
+     * @param deEntityId The deEntity to set.
+     */
+    public void setDeEntityId(Long deEntityId) {
+        this.deEntityId = deEntityId;
+    }
 
-	/**
-	 * @param id The id to set.
-	 */
-	public void setId(Long id)
-	{
-		this.id = id;
-	}
+    /**
+     * @return Returns the id.
+     * @hibernate.id name="id" column="ID" type="long"
+     * length="30" unsaved-value="null" generator-class="native"
+     * @hibernate.generator-param name="sequence" value="CATEGORY_SEQ"
+     */
+    public Long getId() {
+        return id;
+    }
 
-	/**
-	 * @return Returns the parentCategory.
-	 * @hibernate.many-to-one column="PARENT_CATEGORY_ID" class="edu.wustl.common.querysuite.metadata.category.Category"
-	 * constrained="true"
-	 */
-	public Category getParentCategory()
-	{
-		return parentCategory;
-	}
+    /**
+     * @param id The id to set.
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	/**
-	 * @param parentCategory The parentCategory to set.
-	 */
-	public void setParentCategory(Category parentCategory)
-	{
-		this.parentCategory = parentCategory;
-	}
+    /**
+     * @return Returns the parentCategory.
+     * @hibernate.many-to-one column="PARENT_CATEGORY_ID" class="edu.wustl.common.querysuite.metadata.category.Category"
+     * constrained="true"
+     */
+    public Category getParentCategory() {
+        return parentCategory;
+    }
 
-	/**
-	 * @param rootClass The rootClass to set.
-	 */
-	public void setRootClass(CategorialClass rootClass)
-	{
-		this.rootClass = rootClass;
-	}
+    /**
+     * @param parentCategory The parentCategory to set.
+     */
+    public void setParentCategory(Category parentCategory) {
+        this.parentCategory = parentCategory;
+    }
 
-	/**
-	 * @param subCategories The subCategories to set.
-	 */
-	public void setSubCategories(Set<Category> subCategories)
-	{
-		this.subCategories = subCategories;
-	}
+    /**
+     * @param rootClass The rootClass to set.
+     */
+    public void setRootClass(CategorialClass rootClass) {
+        this.rootClass = rootClass;
+    }
 
-	/**
-	 * Default constructor.
-	 * Required for hibernate.
-	 */
-	public Category()
-	{
+    /**
+     * @param subCategories The subCategories to set.
+     */
+    public void setSubCategories(Set<Category> subCategories) {
+        this.subCategories = subCategories;
+    }
 
-	}
+    /**
+     * 
+     * @return
+     */
+    public Category copy() {
+        Category newCategory = new Category();
+        newCategory.setDeEntityId(this.deEntityId);
+        CategorialClass newCategorialClass = this.rootClass.copy();
+        newCategorialClass.setCategory(newCategory);
+        newCategory.setRootClass(newCategorialClass);
+        newCategory.setCategoryEntity(this.categoryEntity);
+        /*     newCategory.setParentCategory(this.parentCategory.copy());
+         
+         Set<Category> newSubCategories = new HashSet<Category>();
+         for(Category category:subCategories){
+         Category categoryCopy = category.copy();
+         categoryCopy.setParentCategory(newCategory);
+         newSubCategories.add(categoryCopy);
+         }
+         newCategory.setSubCategories(newSubCategories);
+         */
+        return newCategory;
+    }
 
-	/**
-	 * @return
-	 * @hibernate.many-to-one column="ROOT_CATEGORIAL_CLASS_ID" cascade="save-update" unique="true" class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
-	 */
-	public CategorialClass getRootClass()
-	{
-		return rootClass;
-	}
+    /**
+     * Default constructor.
+     * Required for hibernate.
+     */
+    public Category() {
 
-	/**
-	 * @return
-	 * @hibernate.set name="subCategories" table="CATEGORY" inverse="false" lazy="false"
-	 * @hibernate.collection-key column="PARENT_CATEGORY_ID"
-	 * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.Category"
-	 */
-	public Set<Category> getSubCategories()
-	{
-		return subCategories;
-	}
+    }
+
+    /**
+     * @return
+     * @hibernate.many-to-one column="ROOT_CATEGORIAL_CLASS_ID" 
+     * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
+     */
+    public CategorialClass getRootClass() {
+        return rootClass;
+    }
+
+    /**
+     * @return
+     * @hibernate.set name="subCategories" table="CATEGORY"
+     * cascade="save-update" inverse="false" lazy="false"
+     * @hibernate.collection-key column="PARENT_CATEGORY_ID"
+     * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.Category"
+     */
+    public Set<Category> getSubCategories() {
+        return subCategories;
+    }
 
     /**
      * @return Returns the categoryEntity.
