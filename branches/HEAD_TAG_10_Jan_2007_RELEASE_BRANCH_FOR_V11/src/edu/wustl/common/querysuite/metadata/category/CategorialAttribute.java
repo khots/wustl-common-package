bad398a0a3a239b208e4b46409ecbe6a321f1c3a
chetan_patil <chetan_patil>
2008-01-03 14:50:41 +0000

    DataCategory related chanages.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1001 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/metadata/category/CategorialAttribute.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/metadata/category/CategorialAttribute.java
@@ -6,44 +6,38 @@ import java.io.Serializable;
 import edu.common.dynamicextensions.domaininterface.AttributeInterface;
 
 /**
- * @version 1.0
- * @created 28-Dec-2006 2:01:13 AM
- * @hibernate.class table="CATEGORIAL_ATTRIBUTE"
+ * @author deepak_shingan
+ *
+ * @hibernate.joined-subclass table="CATEGORIAL_ATTRIBUTE"
+ * @hibernate.joined-subclass-key column="ID" 
  */
-public class CategorialAttribute implements Serializable
+public class CategorialAttribute extends AbstractCategorialAttribute implements Serializable
 {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 3284285745433302355L;
-	private Long id;
 	private Long deCategoryAttributeId;
-	private Long deSourceClassAttributeId;
-	private CategorialClass categorialClass;
-    private AttributeInterface categoryAttribute;
-    private AttributeInterface sourceClassAttribute;
+
+	private AttributeInterface categoryAttribute;
+
 	/**
-	 * @return Returns the categorialClass.
-	 * @hibernate.many-to-one column="CATEGORIAL_CLASS_ID"
-	 * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
+	 * @return the categoryAttribute
 	 */
-	public CategorialClass getCategorialClass()
+	public AttributeInterface getCategoryAttribute()
 	{
-		return categorialClass;
+		return categoryAttribute;
 	}
 
 	/**
-	 * @param categorialClass The categorialClass to set.
+	 * @param categoryAttribute the categoryAttribute to set
 	 */
-	public void setCategorialClass(CategorialClass categorialEntity)
+	public void setCategoryAttribute(AttributeInterface categoryAttribute)
 	{
-		this.categorialClass = categorialEntity;
+		this.categoryAttribute = categoryAttribute;
 	}
 
 	/**
-	 * @return Returns the deCategoryAttribute.
-	 * @hibernate.property name="deCategoryAttributeId" type="long" column="DE_CATEGORY_ATTRIBUTE_ID"
+	 * @return the deCategoryAttributeId
+	 * 
+	 * @hibernate.property name="deCategoryAttributeId" type="long" length="30" column="DE_CATEGORY_ATTRIBUTE_ID"
 	 */
 	public Long getDeCategoryAttributeId()
 	{
@@ -51,98 +45,11 @@ public class CategorialAttribute implements Serializable
 	}
 
 	/**
-	 * @param deCategoryAttributeId The deCategoryAttribute to set.
+	 * @param deCategoryAttributeId the deCategoryAttributeId to set
 	 */
 	public void setDeCategoryAttributeId(Long deCategoryAttributeId)
 	{
 		this.deCategoryAttributeId = deCategoryAttributeId;
 	}
-
-	/**
-	 * @return Returns the deSourceClassAttribute.
-	 * @hibernate.property name="deSourceClassAttributeId" type="long" column="DE_SOURCE_CLASS_ATTRIBUTE_ID"
-	 */
-	public Long getDeSourceClassAttributeId()
-	{
-		return deSourceClassAttributeId;
-	}
-
-	/**
-	 * @param deSourceClassAttributeId The deSourceClassAttribute to set.
-	 */
-	public void setDeSourceClassAttributeId(Long deSourceClassAttributeId)
-	{
-		this.deSourceClassAttributeId = deSourceClassAttributeId;
-	}
-
-	/**
-	 * @return Returns the id.
-	 * @hibernate.id name="id" column="ID" type="long"
-	 * length="30" unsaved-value="null" generator-class="native"
-	 * @hibernate.generator-param name="sequence" value="CATEGORIAL_ATTRIBUTE_SEQ"
-	 */
-	public Long getId()
-	{
-		return id;
-	}
-
-	/**
-	 * @param id The id to set.
-	 */
-	public void setId(Long id)
-	{
-		this.id = id;
-	}
-
-    /**
-     * 
-     * @return CategorialAttribute
-     */
-    public CategorialAttribute  copy()  
-    {
-        CategorialAttribute newCategorialAttribute = new CategorialAttribute();  
-        newCategorialAttribute.setDeCategoryAttributeId(this.deCategoryAttributeId);
-        newCategorialAttribute.setDeSourceClassAttributeId(this.deSourceClassAttributeId);
-        newCategorialAttribute.setCategoryAttribute(this.categoryAttribute);
-        newCategorialAttribute.setSourceClassAttribute(this.sourceClassAttribute);
-        return newCategorialAttribute;
-       
-    }
-	/**
-	 * Default Constructor.
-	 * Required for hibernate.
-	 */
-	public CategorialAttribute()
-	{
-
-	}
-
-    /**
-     * @return Returns the categoryAttribute.
-     */
-    public AttributeInterface getCategoryAttribute() {
-        return categoryAttribute;
-    }
-
-    /**
-     * @return Returns the sourceClassAttribute.
-     */
-    public AttributeInterface getSourceClassAttribute() {
-        return sourceClassAttribute;
-    }
-
-    /**
-     * @param categoryAttribute The categoryAttribute to set.
-     */
-    public void setCategoryAttribute(AttributeInterface categoryAttribute) {
-        this.categoryAttribute = categoryAttribute;
-    }
-
-    /**
-     * @param sourceClassAttribute The sourceClassAttribute to set.
-     */
-    public void setSourceClassAttribute(AttributeInterface sourceClassAttribute) {
-        this.sourceClassAttribute = sourceClassAttribute;
-    }
 
 }
