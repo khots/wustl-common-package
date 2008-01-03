e09bcdf507e02154ba1678f360c33a6f8a015bca
chetan_patil <chetan_patil>
2008-01-03 14:50:41 +0000

    DataCategory related chanages.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@1001 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/querysuite/metadata/category/Category.java
+++ src/edu/wustl/common/querysuite/metadata/category/Category.java
@@ -1,160 +1,78 @@
+
 package edu.wustl.common.querysuite.metadata.category;
 
 import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
 
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 
 /**
  * @version 1.0
  * @created 28-Dec-2006 2:01:13 AM
- * @hibernate.class table="CATEGORY"
+ * 
+ * @hibernate.joined-subclass table="CATEGORY"
+ * @hibernate.joined-subclass-key column="ID" 
  */
-public class Category implements Serializable {
+public class Category extends AbstractCategory<CategorialClass, Category> implements Serializable
+{
 
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -4899537928159969695L;
+	private static final long serialVersionUID = -4899537928159969695L;
 
-    private Long id;
+	private Long deEntityId;
 
-    private Long deEntityId;
+	private EntityInterface categoryEntity;
 
-    private CategorialClass rootClass;
+	/**
+	 * Default constructor. Required for hibernate.
+	 */
+	public Category()
+	{
+	}
 
-    private Set<Category> subCategories = new HashSet<Category>();
+	/**
+	 * @return the categoryEntity
+	 */
+	public EntityInterface getCategoryEntity()
+	{
+		return categoryEntity;
+	}
 
-    private Category parentCategory;
+	/**
+	 * @param categoryEntity
+	 *            the categoryEntity to set
+	 */
+	public void setCategoryEntity(EntityInterface categoryEntity)
+	{
+		this.categoryEntity = categoryEntity;
+	}
 
-    private EntityInterface categoryEntity;
+	/**
+	 * @return the deEntityId
+	 * 
+	 * @hibernate.property name="deEntityId" type="long" length="30" column="DE_ENTITY_ID"
+	 */
+	public Long getDeEntityId()
+	{
+		return deEntityId;
+	}
 
-    /**
-     * @hibernate.property name="deEntityId" type="long" column="DE_ENTITY_ID"
-     * @return Returns the deEntity.
-     */
-    public Long getDeEntityId() {
-        return deEntityId;
-    }
+	/**
+	 * @param deEntityId
+	 *            the deEntityId to set
+	 */
+	public void setDeEntityId(Long deEntityId)
+	{
+		this.deEntityId = deEntityId;
+	}
+	
+	/**
+	 * @return the rootClass
+	 * 
+	 * @hibernate.many-to-one column="ROOT_CLASS_ID" class="edu.wustl.common.querysuite.metadata.category.CategorialClass" unique="true" cascade="all" lazy="false"
+	 */
+	public CategorialClass getRootClass()
+	{
+		return rootClass;
+		
+	}
 
-    /**
-     * @param deEntityId The deEntity to set.
-     */
-    public void setDeEntityId(Long deEntityId) {
-        this.deEntityId = deEntityId;
-    }
-
-    /**
-     * @return Returns the id.
-     * @hibernate.id name="id" column="ID" type="long"
-     * length="30" unsaved-value="null" generator-class="native"
-     * @hibernate.generator-param name="sequence" value="CATEGORY_SEQ"
-     */
-    public Long getId() {
-        return id;
-    }
-
-    /**
-     * @param id The id to set.
-     */
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    /**
-     * @return Returns the parentCategory.
-     * @hibernate.many-to-one column="PARENT_CATEGORY_ID" class="edu.wustl.common.querysuite.metadata.category.Category"
-     * constrained="true"
-     */
-    public Category getParentCategory() {
-        return parentCategory;
-    }
-
-    /**
-     * @param parentCategory The parentCategory to set.
-     */
-    public void setParentCategory(Category parentCategory) {
-        this.parentCategory = parentCategory;
-    }
-
-    /**
-     * @param rootClass The rootClass to set.
-     */
-    public void setRootClass(CategorialClass rootClass) {
-        this.rootClass = rootClass;
-    }
-
-    /**
-     * @param subCategories The subCategories to set.
-     */
-    public void setSubCategories(Set<Category> subCategories) {
-        this.subCategories = subCategories;
-    }
-
-    /**
-     * 
-     * @return
-     */
-    public Category copy() {
-        Category newCategory = new Category();
-        newCategory.setDeEntityId(this.deEntityId);
-        CategorialClass newCategorialClass = this.rootClass.copy();
-        newCategorialClass.setCategory(newCategory);
-        newCategory.setRootClass(newCategorialClass);
-        newCategory.setCategoryEntity(this.categoryEntity);
-        /*     newCategory.setParentCategory(this.parentCategory.copy());
-         
-         Set<Category> newSubCategories = new HashSet<Category>();
-         for(Category category:subCategories){
-         Category categoryCopy = category.copy();
-         categoryCopy.setParentCategory(newCategory);
-         newSubCategories.add(categoryCopy);
-         }
-         newCategory.setSubCategories(newSubCategories);
-         */
-        return newCategory;
-    }
-
-    /**
-     * Default constructor.
-     * Required for hibernate.
-     */
-    public Category() {
-
-    }
-
-    /**
-     * @return
-     * @hibernate.many-to-one column="ROOT_CATEGORIAL_CLASS_ID" 
-     * class="edu.wustl.common.querysuite.metadata.category.CategorialClass" constrained="true"
-     */
-    public CategorialClass getRootClass() {
-        return rootClass;
-    }
-
-    /**
-     * @return
-     * @hibernate.set name="subCategories" table="CATEGORY"
-     * cascade="save-update" inverse="false" lazy="false"
-     * @hibernate.collection-key column="PARENT_CATEGORY_ID"
-     * @hibernate.collection-one-to-many class="edu.wustl.common.querysuite.metadata.category.Category"
-     */
-    public Set<Category> getSubCategories() {
-        return subCategories;
-    }
-
-    /**
-     * @return Returns the categoryEntity.
-     */
-    public EntityInterface getCategoryEntity() {
-        return categoryEntity;
-    }
-
-    /**
-     * @param categoryEntity The categoryEntity to set.
-     */
-    public void setCategoryEntity(EntityInterface categoryEntity) {
-        this.categoryEntity = categoryEntity;
-    }
 }
