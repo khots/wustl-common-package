a9816a39335e7101c0a7236960573d54386ae5d1
abhijit_naik <abhijit_naik>
2008-01-22 04:50:38 +0000

    unwanted exception commented
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@1025 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/security/SecurityManager.java
+++ src/edu/wustl/common/security/SecurityManager.java
@@ -1134,6 +1134,7 @@ public class SecurityManager implements Permissions {
 			Logger.out.warn(
 					ex.getMessage() + "Error occured while creating Potection Element "
 							+ protectionElement.getProtectionElementName());
+//			throw new CSException (ex.getMessage(),ex);
 		}
 
 	}
