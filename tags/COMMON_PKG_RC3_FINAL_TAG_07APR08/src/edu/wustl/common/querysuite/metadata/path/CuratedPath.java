b8bfd47181fddf8b59ef2afdbb4bb6f4ad3e8833
cvs2svn <cvs2svn>
2008-04-05 13:15:56 +0000

    This commit was manufactured by cvs2svn to create tag 'COMMON_PKG_RC3_FINAL_TAG_07APR08'.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1058 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ tags/COMMON_PKG_RC3_FINAL_TAG_07APR08/src/edu/wustl/common/querysuite/metadata/path/CuratedPath.java
@@ -0,0 +1,111 @@
+
+package edu.wustl.common.querysuite.metadata.path;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import edu.common.dynamicextensions.domaininterface.EntityInterface;
+
+/**
+ * @author Chandrakant Talele
+ */
+public class CuratedPath implements ICuratedPath
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4723486424420197283L;
+	private long curatedPathId;
+	private Set<IPath> paths;
+	private Set<EntityInterface> entitySet;
+	private boolean selected;
+
+	/**
+	 * @param curatedPathId
+	 * @param paths
+	 * @param entitySet
+	 * @param isSelected
+	 */
+	public CuratedPath(long curatedPathId, Set<EntityInterface> entitySet, boolean selected)
+	{
+		this.curatedPathId = curatedPathId;
+		this.selected = selected;
+		if (entitySet == null)
+		{
+			this.entitySet = new HashSet<EntityInterface>();
+		}
+		this.entitySet = entitySet;
+		this.paths = new HashSet<IPath>();
+	}
+
+	public void addPath(Path path)
+	{
+		paths.add(path);
+	}
+
+	/**
+	 * @return Returns the curatedPathId.
+	 */
+	public long getCuratedPathId()
+	{
+		return curatedPathId;
+	}
+
+	/**
+	 * @return Returns the entitySet.
+	 */
+	public Set<EntityInterface> getEntitySet()
+	{
+		return entitySet;
+	}
+
+	/**
+	 * @return Returns the isSelected.
+	 */
+	public boolean isSelected()
+	{
+		return selected;
+	}
+
+	/**
+	 * @return Returns the paths.
+	 */
+	public Set<IPath> getPaths()
+	{
+		return paths;
+	}
+
+	/**
+	 * @param curatedPathId The curatedPathId to set.
+	 */
+	public void setCuratedPathId(long curatedPathId)
+	{
+		this.curatedPathId = curatedPathId;
+	}
+
+	/**
+	 * @param entitySet The entitySet to set.
+	 */
+	public void setEntitySet(Set<EntityInterface> entitySet)
+	{
+		this.entitySet = entitySet;
+	}
+
+	/**
+	 * @param isSelected The isSelected to set.
+	 */
+	public void setSelected(boolean selected)
+	{
+		this.selected = selected;
+	}
+
+	/**
+	 * @param paths The paths to set.
+	 */
+	public void setPaths(Set<IPath> paths)
+	{
+		this.paths = paths;
+	}
+
+}
