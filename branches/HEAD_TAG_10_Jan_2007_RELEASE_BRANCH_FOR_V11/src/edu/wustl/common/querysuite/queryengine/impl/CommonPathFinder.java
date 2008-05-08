5f7f68e17454f870bbb054f2e12e6f5e753b9ac9
chetan_patil <chetan_patil>
2008-05-08 13:09:19 +0000

    Changes to get DS from properties file.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1089 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/queryengine/impl/CommonPathFinder.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/querysuite/queryengine/impl/CommonPathFinder.java
@@ -1,19 +1,18 @@
 
 package edu.wustl.common.querysuite.queryengine.impl;
 
+import java.io.IOException;
+import java.net.URL;
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.HashSet;
 import java.util.List;
+import java.util.Properties;
 import java.util.Set;
-import java.util.Vector;
 
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import javax.sql.DataSource;
 
-import edu.common.dynamicextensions.domaininterface.AssociationInterface;
 import edu.common.dynamicextensions.domaininterface.EntityInterface;
 import edu.wustl.cab2b.client.ui.query.IPathFinder;
 import edu.wustl.cab2b.server.path.PathFinder;
@@ -26,80 +25,113 @@ import edu.wustl.common.util.logger.Logger;
 /**
  * This class is used to find all the possible paths between two entities.
  * @author deepti_shelar
+ * @author lalit_chand
  *
  */
 public class CommonPathFinder implements IPathFinder
 {
-	
-	private PathFinder getPathFinderInstance()
+
+	private static PathFinder pathFinder = null;
+
+	public CommonPathFinder()
 	{
-		PathFinder pathFinder=null;
-		Connection connection = null;
-		try
+		if (pathFinder == null)
 		{
-			InitialContext context = new InitialContext();
- 			DataSource dataSource =  (DataSource) context.lookup("java:/catissuecore");
- 			connection = dataSource.getConnection();
- 			pathFinder = PathFinder.getInstance(connection);
-		}
-		catch (NamingException e)
-		{
-			Logger.out.error("CommonPathFinder:",e);
-		}
-		catch (SQLException e)
-		{
-			Logger.out.error("CommonPathFinder:",e);
-		}
-		finally
-		{
+			Connection connection = null;
 			try
 			{
-				if (connection!=null)
-					connection.close();
+				InitialContext context = new InitialContext();
+				DataSource dataSource = (DataSource) context.lookup(getDataSource());
+				connection = dataSource.getConnection();
+				pathFinder = PathFinder.getInstance(connection);
+
+			}
+			catch (NamingException e)
+			{
+				Logger.out.error("CommonPathFinder:", e);
+				//TODO need to see how to handle exception
 			}
 			catch (SQLException e)
 			{
-				Logger.out.error("CommonPathFinder:",e);
+				Logger.out.error("CommonPathFinder:", e);
+				//TODO need to see how to handle exception
+			}
+			finally
+			{
+				try
+				{
+					if (connection != null)
+						connection.close();
+				}
+				catch (SQLException e)
+				{
+					Logger.out.error("CommonPathFinder:", e);
+				}
 			}
 		}
-		return pathFinder;
 	}
 	
+	/*
+	 * This method is for testing only. Its is solely meant to be used in CommonPathFinderTest class
+	 */
+	String getDS() {
+		return getDataSource();
+	}
+
+	//  Name of the property file
+	private String getDataSource()
+	{
+		String propertyfile = "commonpackage.properties";
+		ClassLoader classLoader = CommonPathFinder.class.getClassLoader();
+		URL url = classLoader.getResource(propertyfile);
+		Properties properties = new Properties();
+		try
+		{
+			properties.load(url.openStream());
+		}
+		catch (IOException e)
+		{
+			Logger.out.error((new StringBuilder()).append("Unable to load properties from : ")
+					.append(propertyfile).toString());
+			e.printStackTrace();
+		}
+		return (new StringBuilder()).append("java:/").append(
+				properties.getProperty("datasource.name")).toString();
+	}
+
 	/**
 	 * This method gets all the possible paths between two entities.
 	 */
 	public List<IPath> getAllPossiblePaths(EntityInterface srcEntity, EntityInterface destEntity)
 	{
-		 PathFinder pathFinder= getPathFinderInstance();
-		 return pathFinder.getAllPossiblePaths(srcEntity, destEntity);
+		//PathFinder pathFinder= getPathFinderInstance();
+		return pathFinder.getAllPossiblePaths(srcEntity, destEntity);
 	}
 
-	public IPath getPathForAssociations(List<IIntraModelAssociation> intraModelAssociationList) 
+	public IPath getPathForAssociations(List<IIntraModelAssociation> intraModelAssociationList)
 	{
-		PathFinder pathFinder = getPathFinderInstance();
-		return  pathFinder.getPathForAssociations(intraModelAssociationList);
+		//PathFinder pathFinder = getPathFinderInstance();
+		return pathFinder.getPathForAssociations(intraModelAssociationList);
 	}
-	
-	public Set<ICuratedPath> autoConnect(Set<EntityInterface> arg0)
+
+	public Set<ICuratedPath> autoConnect(Set<EntityInterface> entitySet)
 	{
-		return new HashSet<ICuratedPath>();
+		return pathFinder.autoConnect(entitySet);
 	}
 
-
-	public Set<ICuratedPath> getCuratedPaths(EntityInterface arg0, EntityInterface arg1)
+	public Set<ICuratedPath> getCuratedPaths(EntityInterface srcEntity, EntityInterface destEntity)
 	{
-		return new HashSet<ICuratedPath>();
+		return pathFinder.getCuratedPaths(srcEntity, destEntity);
 	}
 
-
-	public Collection<AssociationInterface> getIncomingIntramodelAssociations(Long arg0)
-	{
-		return new Vector<AssociationInterface>();
-	}
-
+	//
+	//	public Collection<AssociationInterface> getIncomingIntramodelAssociations(Long arg0)
+	//	{
+	//		return  pathFinder.getIncomingIntramodelAssociations(arg0);
+	//	}
 
 	public List<IInterModelAssociation> getInterModelAssociations(Long arg0)
 	{
-		return new Vector<IInterModelAssociation>();
+		return pathFinder.getInterModelAssociations(arg0);
 	}
 }
