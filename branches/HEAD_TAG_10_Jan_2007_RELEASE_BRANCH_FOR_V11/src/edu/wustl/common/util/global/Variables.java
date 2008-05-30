ed64fc99b6643d0e2ff016e71ca659169839c55c
rukhsana_sameer <rukhsana_sameer>
2008-05-30 06:45:11 +0000

    mainProtocol and read denied objects set to these variables after reading from properties file.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1125 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/util/global/Variables.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/util/global/Variables.java
@@ -6,7 +6,9 @@
  */
 package edu.wustl.common.util.global;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,22 +24,24 @@ import java.util.Map;
 
 public class Variables 
 {
-	public static String applicationName = new String();
-	public static String applicationVersion = new String();
-	
-	/* To hold path of applicatio installation directory */
-	public static String applicationHome = new String();
-	
-	public static String databaseName=new String();
-	public static String propertiesDirPath = new String();
-	
-	public static String datePattern = new String();
-	public static String timePattern = new String();
-	public static String timeFormatFunction = new String();
-	public static String dateFormatFunction = new String();
-	public static String strTodateFunction = new String();
-	public static String dateTostrFunction = new String();
-	public static String catissueURL=new String();
-	
-	 public static Map<String,String> entityCPSqlMap = new HashMap<String, String>();
+    public static String applicationName = new String();
+    public static String applicationVersion = new String();
+    
+    /* To hold path of applicatio installation directory */
+    public static String applicationHome = new String();
+    
+    public static String databaseName=new String();
+    public static String propertiesDirPath = new String();
+    
+    public static String datePattern = new String();
+    public static String timePattern = new String();
+    public static String timeFormatFunction = new String();
+    public static String dateFormatFunction = new String();
+    public static String strTodateFunction = new String();
+    public static String dateTostrFunction = new String();
+    public static String catissueURL=new String();
+    
+     public static Map<String,String> entityCPSqlMap = new HashMap<String, String>();
+     public static String mainProtocolObject  = new String();
+     public static List<String> queryReadDeniedObjectList = new ArrayList<String>();
 }
