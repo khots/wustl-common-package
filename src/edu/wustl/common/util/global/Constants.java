4287089ca2440351911e77c7c943146cf2ee5b9b
pooja_deshpande <pooja_deshpande>
2008-05-30 10:49:50 +0000

    Constants for hashed out string
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@1129 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/global/Constants.java
+++ src/edu/wustl/common/util/global/Constants.java
@@ -504,7 +504,7 @@ public class Constants
 	
 	public static final String 	CP_CLASS_NAME = "edu.wustl.catissuecore.domain.CollectionProtocol";
 	public static final String EXPORT_FILE_NAME_START = "Report_Content_";
-    
+       
     public static final String SECURITY_MANAGER_PROP_FILE = "SecurityManager.properties";
     public static final String APPLN_CONTEXT_NAME = "application.context.name";
     public static final String SECURITY_MANAGER_CLASSNAME = "class.name";
@@ -527,6 +527,7 @@ public class Constants
 	public static final String PUBLIC_GROUP_ID = "PUBLIC_GROUP_ID";
 	public static final String APPLICATION_CLINPORTAL = "clinportal";
 
+public static final String hashedOut = "##";
 }
 
 
