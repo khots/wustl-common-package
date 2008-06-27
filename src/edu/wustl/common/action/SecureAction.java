b28f2023cb85a9318373e4722d89976e177a98d5
supriya <supriya>
2008-06-27 13:47:38 +0000

    Made changes for MSR functionality.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11@1176 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/action/SecureAction.java
+++ src/edu/wustl/common/action/SecureAction.java
@@ -148,7 +148,9 @@ public abstract class SecureAction extends BaseAction
 			return isAuthorizedToExecute(request);
 		}
 		else if (Variables.privilegeDetailsMap.get(objectId) != null && Variables.privilegeDetailsMap.get(objectId).equals(
-				Permissions.GENERAL_ADMINISTRATION))
+				Permissions.GENERAL_ADMINISTRATION) ||Variables.privilegeDetailsMap.get(objectId).equals(
+						Permissions.REPOSITORY_ADMINISTRATION)||Variables.privilegeDetailsMap.get(objectId).equals(
+								Permissions.PROTOCOL_ADMINISTRATION))
 		{
 			SessionDataBean sessionDataBean = (SessionDataBean) request.getSession().getAttribute(
 					Constants.SESSION_DATA);
