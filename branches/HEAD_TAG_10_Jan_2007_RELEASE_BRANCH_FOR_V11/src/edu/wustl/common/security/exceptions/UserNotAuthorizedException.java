a2d2512ae2b944260e2fa5b2cc99dc860f169337
vishvesh_mulay <vishvesh_mulay>
2008-08-09 12:19:26 +0000

    added few fields related to MSR
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1258 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/security/exceptions/UserNotAuthorizedException.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/security/exceptions/UserNotAuthorizedException.java
@@ -15,8 +15,66 @@ package edu.wustl.common.security.exceptions;
  */
 public class UserNotAuthorizedException extends SMException
 {
+    
+    private String privilegeName;
+    
+    private String baseObject;
+    
+    private String baseObjectIdentifier;
 	
-	public UserNotAuthorizedException() {
+	
+    
+    /**
+     * @return Returns the baseObject.
+     */
+    public String getBaseObject()
+    {
+        return baseObject;
+    }
+
+    
+    /**
+     * @param baseObject The baseObject to set.
+     */
+    public void setBaseObject(String baseObject)
+    {
+        this.baseObject = baseObject;
+    }
+
+    
+    /**
+     * @return Returns the baseObjectIdentifier.
+     */
+    public String getBaseObjectIdentifier()
+    {
+        return baseObjectIdentifier;
+    }
+
+    
+    /**
+     * @param baseObjectIdentifier The baseObjectIdentifier to set.
+     */
+    public void setBaseObjectIdentifier(String baseObjectIdentifier)
+    {
+        this.baseObjectIdentifier = baseObjectIdentifier;
+    }
+
+    /**
+     * @return Returns the privilegeName.
+     */
+    public String getPrivilegeName()
+    {
+        return privilegeName;
+    }
+    
+    /**
+     * @param privilegeName The privilegeName to set.
+     */
+    public void setPrivilegeName(String privilegeName)
+    {
+        this.privilegeName = privilegeName;
+    }
+    public UserNotAuthorizedException() {
 		super();
 	}
 	/**
