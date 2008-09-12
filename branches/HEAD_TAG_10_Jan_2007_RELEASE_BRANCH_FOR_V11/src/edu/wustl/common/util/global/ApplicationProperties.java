381678308f1f01dc7a16a353a6e365ba43539e5c
abhijit_naik <abhijit_naik>
2008-09-12 13:08:58 +0000

    handled nullpointer exception
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1306 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/util/global/ApplicationProperties.java
+++ branches/HEAD_TAG_10_Jan_2007_RELEASE_BRANCH_FOR_V11/src/edu/wustl/common/util/global/ApplicationProperties.java
@@ -9,6 +9,11 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import org.apache.log4j.Logger;
+
+import sun.security.action.GetLongAction;
+import sun.util.logging.resources.logging;
+
 /**
  * This class is used to retrieve values of keys from the ApplicationResources.properties file.
  * @author kapil_kaveeshwar
@@ -17,15 +22,26 @@ public class ApplicationProperties
 {
 
 	private static ResourceBundle bundle;
-
+	private static Logger logger = edu.wustl.common.util.logger.Logger.getLogger(ApplicationProperties.class);
 	public static void initBundle(String baseName)
 	{
 		bundle = ResourceBundle.getBundle(baseName);
+	
 	}
 
 	public static String getValue(String theKey)
 	{
-		return bundle.getString(theKey);
+		String val="";
+		if(bundle == null)
+		{
+			logger.fatal("resource bundle is null cannot return value for key " + theKey);
+			
+		}
+		else
+		{
+			val= bundle.getString(theKey);
+		}
+		return val;
 	}
 
 /**
