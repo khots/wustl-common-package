22dcf6fc0bdca6a07d8d8be57ba94887d8418361
ravi_kumar <ravi_kumar>
2008-09-22 09:32:22 +0000

    Added new file for constants.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1335 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
@@ -0,0 +1,11 @@
+package edu.wustl.common.util.global;
+
+
+public class TextConstants
+{
+
+	public static final String LINE_SEPARATOR = System.getProperty(("line.separator"));
+	public static final String TWO_LINE_BREAK = "\n\n";
+	public static final String LINE_BREAK = "\n";
+	public static final String TAB = "\t";
+}
