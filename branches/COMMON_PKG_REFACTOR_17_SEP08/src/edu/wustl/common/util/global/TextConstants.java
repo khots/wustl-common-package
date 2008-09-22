222204edb44bf4c8b32638134d389161850cfbc7
ravi_kumar <ravi_kumar>
2008-09-22 13:16:53 +0000

    Added Constant for empty string.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1342 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
@@ -8,4 +8,5 @@ public class TextConstants
 	public static final String TWO_LINE_BREAK = "\n\n";
 	public static final String LINE_BREAK = "\n";
 	public static final String TAB = "\t";
+	public static final String EMPTY_STRING = "";
 }
