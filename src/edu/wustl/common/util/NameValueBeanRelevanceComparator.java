38cc931e4f8de704e99fe6789dd466ad46b8c4e4
ravi_kumar <ravi_kumar>
2008-09-23 11:18:26 +0000

    Code refactoring.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1351 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/NameValueBeanRelevanceComparator.java
+++ src/edu/wustl/common/util/NameValueBeanRelevanceComparator.java
@@ -6,7 +6,7 @@ package edu.wustl.common.util;
  */
 import edu.wustl.common.beans.NameValueBean;
 
-public class NameValueBeanRelevanceComparator implements java.util.Comparator
+public class NameValueBeanRelevanceComparator implements java.util.Comparator<Object>
 {
 
 	/* (non-Javadoc)
@@ -14,29 +14,46 @@ public class NameValueBeanRelevanceComparator implements java.util.Comparator
 	 */
 	public int compare(Object arg0, Object arg1)
 	{
+		int retValue=0;
 		if (arg0 instanceof NameValueBean && arg1 instanceof NameValueBean)
 		{
 			NameValueBean nvb1 = (NameValueBean) arg0;
 			NameValueBean nvb2 = (NameValueBean) arg1;
-			//Compare according to relevance counter
+
 			if (nvb1.getRelevanceCounter() != null && nvb2.getRelevanceCounter() != null)
 			{
-				int cmp = nvb1.getRelevanceCounter().compareTo(nvb2.getRelevanceCounter());
-				//If relevance counter are equal then compare on basis of name.
-				if (cmp == 0)
-				{
-					if (nvb1.getName() != null && nvb2.getName() != null)
-					{
-						return nvb1.getName().toString().toLowerCase().compareTo(
-								nvb2.getName().toString().toLowerCase());
-					}
-				}
-				else
-				{
-					return cmp;
-				}
+				retValue = getDiffOfNameValueBeans(nvb1, nvb2);
 			}
 		}
-		return 0;
+		return retValue;
+	}
+
+	/**
+	 * This method returns difference of two NameValueBean
+	 * @param retValue
+	 * @param nvb1 NameValueBean
+	 * @param nvb2 NameValueBean
+	 * @return difference of two NameValueBean.
+	 */
+	private int getDiffOfNameValueBeans(NameValueBean nvb1, NameValueBean nvb2)
+	{
+		int retValue=0;
+
+		//Compare according to relevance counter
+		int cmp = nvb1.getRelevanceCounter().compareTo(nvb2.getRelevanceCounter());
+		//If relevance counter are equal then compare on basis of name.
+		if (cmp == 0)
+		{
+			if (nvb1.getName() != null && nvb2.getName() != null)
+			{
+				retValue= nvb1.getName().toString().toLowerCase().compareTo(
+						nvb2.getName().toString().toLowerCase());
+			}
+		}
+		else
+		{
+			retValue=cmp;
+		}
+		return retValue;
 	}
 }
