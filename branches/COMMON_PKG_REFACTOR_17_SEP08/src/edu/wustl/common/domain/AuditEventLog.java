e3d7313c9ea89bd13f21c3aa2aa2d52c09cdf51a
dharitri_pradhan <dharitri_pradhan>
2008-09-24 09:35:04 +0000

    java-doc has been edited.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1373 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/domain/AuditEventLog.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/domain/AuditEventLog.java
@@ -11,15 +11,34 @@ public class AuditEventLog implements java.io.Serializable
 {
 
 	private static final long serialVersionUID = 1234567890L;
-
+	
+	/**
+	 * System generated unique id.
+	 */
 	private Long id;
+	/**
+	 * identifier that identify the event object.
+	 */
 	private Long objectIdentifier;
+	/**
+	 * Name of the event object.
+	 */
 	private String objectName;
+	/**
+	 * Type of the event.
+	 */
 	private String eventType;
+	/**
+	 * object of audit event.
+	 */
 	private AuditEvent auditEvent;
-	private Collection<AuditEventDetails> auditEventDetailsCollcetion = new HashSet<AuditEventDetails>();
+	/**
+	 * collection that contains details of event audit.
+	 */
+	private Collection<AuditEventDetails> auditEventDetailsCollection = new HashSet<AuditEventDetails>();
 
 	/**
+	 * get the id.
 	 * @hibernate.id name="id" column="IDENTIFIER" type="long" length="30"
 	 * unsaved-value="null" generator-class="native" 
 	 * @hibernate.generator-param name="sequence" value="CATISSUE_AUDIT_EVENT_LOG_SEQ"
@@ -28,13 +47,16 @@ public class AuditEventLog implements java.io.Serializable
 	{
 		return id;
 	}
-
+	/**
+	 * set the id.
+	 */
 	public void setId(Long id)
 	{
 		this.id = id;
 	}
 
 	/**
+	 * get the identifier of the object.
 	 * @hibernate.property name="objectIdentifier" type="long" 
 	 * column="OBJECT_IDENTIFIER" length="50"
 	 */
@@ -42,13 +64,16 @@ public class AuditEventLog implements java.io.Serializable
 	{
 		return objectIdentifier;
 	}
-
+	/**
+	 * set the object Identifier.
+	 */
 	public void setObjectIdentifier(Long objectIdentifier)
 	{
 		this.objectIdentifier = objectIdentifier;
 	}
 
 	/**
+	 * get the name of object.
 	 * @hibernate.property name="ObjectName" type="string" 
 	 * column="OBJECT_NAME" length="50"
 	 */
@@ -56,13 +81,16 @@ public class AuditEventLog implements java.io.Serializable
 	{
 		return objectName;
 	}
-
+	/**
+	 * set the name of the object.
+	 */
 	public void setObjectName(String objectName)
 	{
 		this.objectName = objectName;
 	}
 
 	/**
+	 * get the type of the event.
 	 * @hibernate.property name="eventType" type="string" 
 	 * column="EVENT_TYPE" length="50"
 	 */
@@ -70,13 +98,16 @@ public class AuditEventLog implements java.io.Serializable
 	{
 		return eventType;
 	}
-
+	/**
+	 * set the type of the event.
+	 */
 	public void setEventType(String eventType)
 	{
 		this.eventType = eventType;
 	}
 
 	/**
+	 * get the audit event object.
 	 * @hibernate.many-to-one column="AUDIT_EVENT_ID"  class="edu.wustl.common.domain.AuditEvent" constrained="true"
 	 * @see #setParticipant(Site)
 	 */
@@ -84,13 +115,16 @@ public class AuditEventLog implements java.io.Serializable
 	{
 		return auditEvent;
 	}
-
+	/**
+	 * set the audit event object.
+	 */
 	public void setAuditEvent(AuditEvent auditEvent)
 	{
 		this.auditEvent = auditEvent;
 	}
 
 	/**
+	 * get the collection.
 	 * @hibernate.set name="auditEventDetailsCollcetion" table="CATISSUE_AUDIT_EVENT_DETAILS"
 	 * @hibernate.collection-key column="AUDIT_EVENT_LOG_ID"
 	 * @hibernate.collection-one-to-many class="edu.wustl.common.domain.AuditEventDetails"
@@ -99,7 +133,9 @@ public class AuditEventLog implements java.io.Serializable
 	{
 		return auditEventDetailsCollcetion;
 	}
-
+	/**
+	 * set the collection with audit event details.
+	 */
 	public void setAuditEventDetailsCollcetion(
 			Collection<AuditEventDetails> auditEventDetailsCollcetion)
 	{
