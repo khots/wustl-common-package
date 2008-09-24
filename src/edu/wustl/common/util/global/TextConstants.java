5ce7f2ea38f2422ad7f4e67f5913af14742a3b75
ravi_kumar <ravi_kumar>
2008-09-24 10:42:21 +0000

    added constant TITLI_TABLE_MAPPING_FILE
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1376 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/global/TextConstants.java
+++ src/edu/wustl/common/util/global/TextConstants.java
@@ -9,4 +9,5 @@ public class TextConstants
 	public static final String LINE_BREAK = "\n";
 	public static final String TAB = "\t";
 	public static final String EMPTY_STRING = "";
+	public static final String TITLI_TABLE_MAPPING_FILE="titli-table-mapping.xml";
 }
