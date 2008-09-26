412fa3eddf1b1a5ed7ba478d399d0220b6c5b18d
ravi_kumar <ravi_kumar>
2008-09-26 13:31:43 +0000

    Added constant for PermissionMapDetails.xml
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1408 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
@@ -10,4 +10,5 @@ public class TextConstants
 	public static final String TAB = "\t";
 	public static final String EMPTY_STRING = "";
 	public static final String TITLI_TABLE_MAPPING_FILE="titli-table-mapping.xml";
+	public static final String PERMSN_MAP_DET_FILE="PermissionMapDetails.xml";
 }
