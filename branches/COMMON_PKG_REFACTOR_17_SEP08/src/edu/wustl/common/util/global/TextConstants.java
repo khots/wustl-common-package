f1feff2b3accb0c1a1e98b2624b2d4c8aebc358a
ravi_kumar <ravi_kumar>
2008-09-29 11:28:34 +0000

    Added const for 0
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1424 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/TextConstants.java
@@ -9,6 +9,7 @@ public class TextConstants
 	public static final String LINE_BREAK = "\n";
 	public static final String TAB = "\t";
 	public static final String EMPTY_STRING = "";
+	public static final String STR_ZERO = "0";
 	public static final String TITLI_TABLE_MAPPING_FILE="titli-table-mapping.xml";
 	public static final String PERMSN_MAP_DET_FILE="PermissionMapDetails.xml";
 }
