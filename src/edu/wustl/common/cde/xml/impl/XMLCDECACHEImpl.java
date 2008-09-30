3083d7de6e2ac352b68f553951d30f85c0982543
ravi_kumar <ravi_kumar>
2008-09-30 07:36:47 +0000

    code refactoring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1442 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/cde/xml/impl/XMLCDECACHEImpl.java
+++ src/edu/wustl/common/cde/xml/impl/XMLCDECACHEImpl.java
@@ -221,23 +221,23 @@ public class XMLCDECACHEImpl extends edu.wustl.common.cde.xml.impl.XMLCDECacheTy
 		public void leaveElement(java.lang.String ___uri, java.lang.String ___local,
 				java.lang.String ___qname) throws org.xml.sax.SAXException
 		{
-			int attIdx;
+			int attIndx;
 			outer : while (true)
 			{
 				switch (state)
 				{
 					case 1 :
-						attIdx = context.getAttribute("", "lazyLoading");
-						if (attIdx >= 0)
+						attIndx = context.getAttribute("", "lazyLoading");
+						if (attIndx >= 0)
 						{
-							context.consumeAttribute(attIdx);
+							context.consumeAttribute(attIndx);
 							context.getCurrentHandler().leaveElement(___uri, ___local, ___qname);
 							return;
 						}
-						attIdx = context.getAttribute("", "refreshTime");
-						if (attIdx >= 0)
+						attIndx = context.getAttribute("", "refreshTime");
+						if (attIndx >= 0)
 						{
-							context.consumeAttribute(attIdx);
+							context.consumeAttribute(attIndx);
 							context.getCurrentHandler().leaveElement(___uri, ___local, ___qname);
 							return;
 						}
