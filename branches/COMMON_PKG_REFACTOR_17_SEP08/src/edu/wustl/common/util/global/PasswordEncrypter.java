3e13fdf425c7d08dbcd51884aa59414787084ec9
ravi_kumar <ravi_kumar>
2008-10-15 14:09:58 +0000

    code refactoring.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1566 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/PasswordEncrypter.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/PasswordEncrypter.java
@@ -7,6 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import edu.wustl.common.security.exceptions.PasswordEncryptionException;
+import edu.wustl.common.util.logger.Logger;
 import gov.nih.nci.security.util.StringUtilities;
 
 /**
@@ -22,45 +23,62 @@ import gov.nih.nci.security.util.StringUtilities;
 public class PasswordEncrypter
 {
 	/**
+	 * logger Logger - Generic logger.
+	 */
+	private static org.apache.log4j.Logger logger = Logger.getLogger(PasswordEncrypter.class);
+
+	/**
 	 * dbUtility object is used to store the argument values and getting database connection.
 	 */
 	private static DatabaseUtility dbUtility= new DatabaseUtility();
 
-	// The name of the csm table whose password field is to be encrypted.
-	static String CSM_DATABASE_TABLE_NAME = "csm_user";
-	// The name of the catissue table whose password field is to be encrypted.
-	static String CATISSUE_DATABASE_TABLE_NAME = "catissue_password";
-	// The name of the field whose row values have to be encrypted.
-	static String DATABASE_TABLE_FIELD_NAME = "password";
+	/**
+	 * Minimum number of arguments required.
+	 */
+	private static final int MIN_NO_ARGS=7;
+	/**
+	 * The name of the csm table whose password field is to be encrypted.
+	 */
+	private static final String CSM_DATABASE_TABLE_NAME = "csm_user";
 
+	/**
+	 * The name of the catissue table whose password field is to be encrypted.
+	 */
+	private static final String CATISSUE_DATABASE_TABLE_NAME = "catissue_password";
+
+	/**
+	 * The name of the field whose row values have to be encrypted.
+	 */
+	private static final String DATABASE_TABLE_FIELD_NAME = "password";
+	/**
+	 * Main method.
+	 * @param args arguments to main methods.
+	 */
 	public static void main(String[] args)
 	{
 
 		configureDBConnection(args);
-
 		try
 		{
 			// Create an updatable result set
 			Connection connection = dbUtility.getConnection();
 
 			//Encrypting password for csm_user table
-
 			String sql = "SELECT " + CSM_DATABASE_TABLE_NAME + ".* FROM " + CSM_DATABASE_TABLE_NAME;
 			updatePasswords(connection, sql);
 
 			//Encrypting password for catissue_password table
-
 			sql = "SELECT " + CATISSUE_DATABASE_TABLE_NAME + ".* FROM "
 					+ CATISSUE_DATABASE_TABLE_NAME;
 			updatePasswords(connection, sql);
 		}
-		catch (ClassNotFoundException e)
+		catch (ClassNotFoundException exception)
 		{
-			e.printStackTrace();
+			logger.fatal("Not abel to load database driver class", exception);
 		}
-		catch (SQLException e)
+		catch (SQLException exception)
 		{
-			e.printStackTrace();
+			logger.fatal("Not able to update the passwords.", exception);
 		}
 	}
 
@@ -68,7 +86,7 @@ public class PasswordEncrypter
 	 * This method will update password field with new encrypted password in the database.
 	 * @param connection Database connection object
 	 * @param sql Query String
-	 * @throws SQLException
+	 * @throws SQLException generic SQL Exception
 	 */
 	private static void updatePasswords(Connection connection, String sql) throws SQLException
 	{
@@ -76,37 +94,32 @@ public class PasswordEncrypter
 				ResultSet.CONCUR_UPDATABLE);
 		ResultSet resultSet = stmt.executeQuery(sql);
 
-		boolean first = false;
+		resultSet.first();
+		try
+		{
 		while (resultSet.next())
 		{
-			if (!first)
-			{
-				resultSet.first();
-				first = true;
-			}
 			String userPassword = resultSet.getString(DATABASE_TABLE_FIELD_NAME);
 			if (!StringUtilities.isBlank(userPassword))
 			{
-				try
-				{
 					//Encrypting the password and updating the database.
 					String encryptedPasswordWithNewEncryption = PasswordManager
 							.encrypt(PasswordManager.decode(userPassword));
-					if (!StringUtilities.isBlank(encryptedPasswordWithNewEncryption))
-					{
-						resultSet.updateString(DATABASE_TABLE_FIELD_NAME,
-								encryptedPasswordWithNewEncryption);
-						resultSet.updateRow();
-					}
-				}
-				catch (PasswordEncryptionException e)
-				{
-					e.printStackTrace();
+					resultSet.updateString(DATABASE_TABLE_FIELD_NAME,
+							encryptedPasswordWithNewEncryption);
+					resultSet.updateRow();
 				}
 			}
 		}
+		catch (PasswordEncryptionException exception)
+		{
+			logger.fatal("Not able to encrypt the passwords.", exception);
+		}
+		finally
+		{
 		resultSet.close();
 		stmt.close();
+		}
 	}
 
 	/**
@@ -115,15 +128,9 @@ public class PasswordEncrypter
 	 */
 	private static void configureDBConnection(String[] args)
 	{
-		if (args.length == 7)
+		if (args.length == MIN_NO_ARGS)
 		{
-			dbUtility.setDbServerNname(args[0]);
-			dbUtility.setDbServerPortNumber(args[1]);
-			dbUtility.setDbType(args[2]);
-			dbUtility.setDbName(args[3]);
-			dbUtility.setDbUserName(args[4]);
-			dbUtility.setDbPassword(args[5]);
-			dbUtility.setDbDriver(args[6]);
+			dbUtility.setDbParams(args);
 		}
 		else
 		{
