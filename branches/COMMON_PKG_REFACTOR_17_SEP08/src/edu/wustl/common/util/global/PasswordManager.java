2b84addde380564caccd33dc418939e17e2f1c9b
prashant_bandal <prashant_bandal>
2008-10-17 09:50:59 +0000

    code refactor
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1593 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/PasswordManager.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/PasswordManager.java
@@ -42,55 +42,80 @@ public class PasswordManager
 	 * logger Logger - Generic logger.
 	 */
 	private static org.apache.log4j.Logger logger = Logger.getLogger(PasswordManager.class);
-	
+	/**
+	 * specify Success.
+	 */
 	public static final int SUCCESS = 0;
+	/**
+	 * specify fail password length.
+	 */
 	public static final int FAIL_LENGTH = 1;
+	/**
+	 * specify fail password same as old.
+	 */
 	public static final int FAIL_SAME_AS_OLD = 2;
+	/**
+	 * specify fail password same as username.
+	 */
 	public static final int FAIL_SAME_AS_USERNAME = 3;
+	/**
+	 * specify fail password in pattern.
+	 */
 	public static final int FAIL_IN_PATTERN = 4;
+	/**
+	 * specify fail same session.
+	 */
 	public static final int FAIL_SAME_SESSION = 5;
+	/**
+	 * specify fail wrong old password.
+	 */
 	public static final int FAIL_WRONG_OLD_PASSWORD = 6;
+	/**
+	 *  specify fail invalid session.
+	 */
 	public static final int FAIL_INVALID_SESSION = 7;
 
 	/**
 	 * Generate random alpha numeric password.
-	 * @param loginName the loginName of a user.
 	 * @return Returns the generated password.
 	 */
 	public static String generatePassword()
 	{
-		//Define a Constants alpha-numeric String 
-		final String UPPER_CHAR_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-		final String LOWER_CHAR_STRING = "abcdefghijklmnopqrstuvwxyz";
-		final String DIGITS_STRING = "0123456789";
+		//Define a Constants alpha-numeric String
+		final String upperCharString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		final String lowerCharString = "abcdefghijklmnopqrstuvwxyz";
+		final String digitString = "0123456789";
 
 		// Generate password of length 6
 		//final int PASSWORD_LENGTH = 6;
 
 		Random random = new Random();
 		StringBuffer passwordBuff = new StringBuffer();
-		//This password must satisfy the following criteria:New Password must include at least one 
+		//This password must satisfy the following criteria:New Password must include at least one
 		//Upper Case, Lower Case letter and a Number. It must not include Space.
 		for (int i = 0; i < 2; i++)
 		{
 			//Generate a random number from 0(inclusive) to lenght of CHAR_STRING(exclusive).
 			//Get the character corrosponding to random number and append it to password buffer.
-			int randomVal = random.nextInt(UPPER_CHAR_STRING.length());
-			passwordBuff.append(UPPER_CHAR_STRING.charAt(randomVal));
-			randomVal = random.nextInt(LOWER_CHAR_STRING.length());
-			passwordBuff.append(LOWER_CHAR_STRING.charAt(randomVal));
-			randomVal = random.nextInt(DIGITS_STRING.length());
-			passwordBuff.append(DIGITS_STRING.charAt(randomVal));
+			int randomVal = random.nextInt(upperCharString.length());
+			passwordBuff.append(upperCharString.charAt(randomVal));
+			randomVal = random.nextInt(lowerCharString.length());
+			passwordBuff.append(lowerCharString.charAt(randomVal));
+			randomVal = random.nextInt(digitString.length());
+			passwordBuff.append(digitString.charAt(randomVal));
 		}
 		return passwordBuff.toString();
 	}
 
+	/**
+	 * specify stringEncrypter.
+	 */
 	private static StringEncrypter stringEncrypter = null;
 
 	/**
 	 * TO get the instance of StringEncrypter class.
 	 * @return The Object reference of StringEncrypter class.
-	 * @throws EncryptionException
+	 * @throws EncryptionException generic EncryptionException
 	 */
 	private static StringEncrypter getEncrypter() throws EncryptionException
 	{
@@ -105,7 +130,7 @@ public class PasswordManager
 	 * TO get Encrypted password for the given password.
 	 * @param password The password to be encrypted.
 	 * @return The Encrypted password for the given password.
-	 * @throws PasswordEncryptionException 
+	 * @throws PasswordEncryptionException generic PasswordEncryptionException
 	 */
 	public static String encrypt(String password) throws PasswordEncryptionException
 	{
@@ -123,7 +148,7 @@ public class PasswordManager
 	 * TO get Decrypted password for the given password.
 	 * @param password The password to be Decrypted.
 	 * @return The Decrypted password for the given password.
-	 * @throws PasswordEncryptionException 
+	 * @throws PasswordEncryptionException generic PasswordEncryptionException
 	 */
 	public static String decrypt(String password) throws PasswordEncryptionException
 	{
@@ -137,19 +162,24 @@ public class PasswordManager
 		}
 	}
 
+	/**
+	 * This method encode input string.
+	 * @param input string
+	 * @return string
+	 */
 	@Deprecated
 	public static String encode(String input)
 	{
 
-		char char_O = 'O';
-		char char_F = 'f';
+		char charO = 'O';
+		char charF = 'f';
 		char[] digits = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e',
 				'f'};
 		char ch1 = 'A';
 		String key = new String("" + ch1);
-		key += "WelcomeTocaTISSUECORE" + char_O;
+		key += "WelcomeTocaTISSUECORE" + charO;
 		String in = "";
-		key += char_F;
+		key += charF;
 		key += "ThisIsTheFirstReleaseOfcaTISSUECOREDevelopedByWashUAtPersistentSystemsPrivateLimited";
 		for (int i = 0; i < input.length(); i++)
 		{
@@ -179,25 +209,38 @@ public class PasswordManager
 		return null;
 	}
 
+	/**
+	 * This method decode string.
+	 * @param decodeString string to decode
+	 * @return  string
+	 */
 	@Deprecated
-	public static String decode(String s)
+	public static String decode(String decodeString)
 	{
 		try
 		{
-			int len = s.length();
+			int len = decodeString.length();
 			byte[] r = new byte[len / 2];
 			for (int i = 0; i < r.length; i++)
 			{
-				int digit1 = s.charAt(i * 2);
-				int digit2 = s.charAt(i * 2 + 1);
+				int digit1 = decodeString.charAt(i * 2);
+				int digit2 = decodeString.charAt(i * 2 + 1);
 				if ((digit1 >= '0') && (digit1 <= '9'))
+				{
 					digit1 -= '0';
+				}
 				else if ((digit1 >= 'a') && (digit1 <= 'f'))
+				{
 					digit1 -= 'a' - 10;
+				}
 				if ((digit2 >= '0') && (digit2 <= '9'))
+				{
 					digit2 -= '0';
+				}
 				else if ((digit2 >= 'a') && (digit2 <= 'f'))
+				{
 					digit2 -= 'a' - 10;
+				}
 				r[i] = (byte) ((digit1 << 4) + digit2);
 			}
 			String sin = new String(r);
@@ -217,40 +260,39 @@ public class PasswordManager
 	}
 
 	/**
-	 * This method returns the validation results on Form Bean. This method should be called 
-	 * from validate method of form bean
+	 * This method returns the validation results on Form Bean. This method should be called
+	 * from validate method of form bean.
 	 * @param newPassword New Password value
 	 * @param oldPassword Old Password value
 	 * @param httpSession HttpSession object
-	 * @param abstractForm UserForm object  
-	 * @return SUCCESS if all condition passed 
-	 *   else return respective error code (constant int) value  
+	 * @return SUCCESS if all condition passed
+	 *   else return respective error code (constant int) value
 	 */
 	public static int validatePasswordOnFormBean(String newPassword, String oldPassword,
 			HttpSession httpSession)
 	{
 
 		// to check whether password change in same session
-		// get attribute (Boolean) from session object stored when password is changed successfully 
+		// get attribute (Boolean) from session object stored when password is changed successfully
 		Boolean passwordChangedInsameSession = (Boolean) httpSession
 				.getAttribute(Constants.PASSWORD_CHANGE_IN_SESSION);
 		if (passwordChangedInsameSession != null
 				&& passwordChangedInsameSession.booleanValue() == true)
 		{
-			// return error code if attribute (Boolean) is in session   
+			// return error code if attribute (Boolean) is in session
 			logger.debug("Attempt to change Password in same session Returning FAIL_SAME_SESSION");
 			return FAIL_SAME_SESSION;
 		}
 		int minimumPasswordLength = Integer.parseInt(XMLPropertyHandler
 				.getValue(Constants.MINIMUM_PASSWORD_LENGTH));
-		// to Check length of password,if not valid return FAIL_LENGTH 
+		// to Check length of password,if not valid return FAIL_LENGTH
 		if (newPassword.length() < minimumPasswordLength)
 		{
 			logger.debug("Password is not valid returning FAIL_LENGHT");
 			return FAIL_LENGTH;
 		}
 
-		// to Check new password is different as old password ,if bot are same return FAIL_SAME_AS_OLD    	
+		// to Check new password is different as old password ,if bot are same return FAIL_SAME_AS_OLD
 		if (newPassword.equals(oldPassword))
 		{
 			logger.debug("Password is not valid returning FAIL_SAME_AS_OLD");
@@ -261,7 +303,7 @@ public class PasswordManager
 		 * following code checks pattern i.e password must include atleast one UCase,LCASE and Number
 		 * and must not contain space charecter.
 		 */
-		char dest[] = new char[newPassword.length()];
+		char [] dest = new char[newPassword.length()];
 		// get char array where values get stores in dest[]
 		newPassword.getChars(0, newPassword.length(), dest, 0);
 		boolean foundUCase = false; // boolean to check UCase character found in string
@@ -277,13 +319,13 @@ public class PasswordManager
 				foundSpace = true;
 				break;
 			}
-			// to check whether char is Upper Case. 
+			// to check whether char is Upper Case.
 			if (foundUCase == false && Character.isUpperCase(dest[i]) == true)
 			{
 				foundUCase = true;
 			}
 
-			// to check whether char is Lower Case 
+			// to check whether char is Lower Case
 			if (foundLCase == false && Character.isLowerCase(dest[i]) == true)
 			{
 				foundLCase = true;
@@ -307,22 +349,19 @@ public class PasswordManager
 	}
 
 	/**
-	 * 
+	 * TODO Remove this method.
+	 * This method combines UI validation and business rules validation which is incorrect.
+	 * Call validatePasswordOnFormBean for Form bean validations.
+	 * Write your own methods for business validations.
 	 * @param newPassword New Password value
 	 * @param oldPassword Old Password value
 	 * @param httpSession HttpSession object
-	 * @param abstractForm UserForm object  
-	 * @return SUCCESS (constant int 0) if all condition passed 
-	 *   else return respective error code (constant int) value  
-	 */
-
-	/**
-	 * TODO Remove this method. This method combines UI validation and business rules validation which is incorrect.
-	 * Call validatePasswordOnFormBean for Form bean validations. Write your own methods for business validations.
+	 * @return SUCCESS (constant int 0) if all condition passed
+	 *   else return respective error code (constant int) value
 	 */
 	public static int validate(String newPassword, String oldPassword, HttpSession httpSession)
 	{
-		// get SessionDataBean objet from session 
+		// get SessionDataBean objet from session
 		Object obj = httpSession.getAttribute(Constants.SESSION_DATA);
 		SessionDataBean sessionData = null;
 		String userName = "";
@@ -356,7 +395,7 @@ public class PasswordManager
 				password = (String) userList.get(0);
 			}
 
-			// compare password stored in database with value of old password currently entered by 
+			// compare password stored in database with value of old password currently entered by
 			// user for Change Password operation
 			if (!oldPassword.equals(PasswordManager.decode(password)))
 			{
@@ -371,13 +410,13 @@ public class PasswordManager
 		}
 
 		// to check whether password change in same session
-		// get attribute (Boolean) from session object stored when password is changed successfully 
+		// get attribute (Boolean) from session object stored when password is changed successfully
 		Boolean b = null;
 		b = (Boolean) httpSession.getAttribute(Constants.PASSWORD_CHANGE_IN_SESSION);
 		logger.debug("b---" + b);
 		if (b != null && b.booleanValue() == true)
 		{
-			// return error code if attribute (Boolean) is in session   
+			// return error code if attribute (Boolean) is in session
 			logger.debug("Attempt to change Password in same session Returning FAIL_SAME_SESSION");
 			return FAIL_SAME_SESSION; // return int value 5
 		}
@@ -390,7 +429,7 @@ public class PasswordManager
 			return FAIL_LENGTH; // return int value 1
 		}
 
-		// to Check new password is different as old password ,if bot are same return FAIL_SAME_AS_OLD = 3    	
+		// to Check new password is different as old password ,if bot are same return FAIL_SAME_AS_OLD = 3
 		if (newPassword.equals(oldPassword))
 		{
 			logger.debug("Password is not valid returning FAIL_SAME_AS_OLD");
@@ -400,7 +439,7 @@ public class PasswordManager
 		// to check password is differnt than user name if same return FAIL_SAME_AS_USERNAME =4
 		// eg. username=abc@abc.com newpassword=abc is not valid
 		int usernameBeforeMailaddress = userName.indexOf('@');
-		// get substring of username before '@' character    
+		// get substring of username before '@' character
 		String name = userName.substring(0, usernameBeforeMailaddress);
 		logger.debug("usernameBeforeMailaddress---" + name);
 		if (name != null && newPassword.equals(name))
@@ -419,7 +458,7 @@ public class PasswordManager
 		// following is to checks pattern i.e password must include atleast one UCase,LCASE and Number
 		// and must not contain space charecter.
 		// define get char array whose length is equal to length of new password string.
-		char dest[] = new char[newPassword.length()];
+		char []dest = new char[newPassword.length()];
 		// get char array where values get stores in dest[]
 		newPassword.getChars(0, newPassword.length(), dest, 0);
 		boolean foundUCase = false; // boolean to check UCase character found in string
@@ -436,18 +475,20 @@ public class PasswordManager
 				logger.debug("Found Space in Password");
 				break;
 			}
-			// to check whether char is Upper Case. 
+			// to check whether char is Upper Case.
 			if (foundUCase == false && Character.isUpperCase(dest[i]) == true)
 			{
-				//foundUCase=true if char is Upper Case and Upper Case is not found in previous char.
+				//foundUCase=true if char is Upper Case
+				//and Upper Case is not found in previous char.
 				foundUCase = true;
 				logger.debug("Found UCase in Password");
 			}
 
-			// to check whether char is Lower Case 
+			// to check whether char is Lower Case
 			if (foundLCase == false && Character.isLowerCase(dest[i]) == true)
 			{
-				//foundLCase=true if char is Lower Case and Lower Case is not found in previous char.
+				//foundLCase=true if char is Lower Case
+				//and Lower Case is not found in previous char.
 				foundLCase = true;
 				logger.debug("Found LCase in Password");
 			}
@@ -472,9 +513,9 @@ public class PasswordManager
 	}
 
 	/**
-	 * 
+	 * Error message.
 	 * @param errorCode int value return by validate() method
-	 * @return String error message with respect to error code 
+	 * @return String error message with respect to error code
 	 */
 	public static String getErrorMessage(int errorCode)
 	{
@@ -514,7 +555,11 @@ public class PasswordManager
 		return errMsg;
 
 	}
-
+	/**
+	 *
+	 * @param args filename,password.
+	 * @throws PasswordEncryptionException generic PasswordEncryptionException
+	 */
 	public static void main(String[] args) throws PasswordEncryptionException
 	{
 		String pwd = "admin";
@@ -530,19 +575,19 @@ public class PasswordManager
 			String password = args[1];
 			encodedPWD = encrypt(password);
 			//System.out.println("Filename : " + filename + " : password : " + password
-				//	+ " : encoded" + encodedPWD);
+			//	+ " : encoded" + encodedPWD);
 			writeToFile(filename, encodedPWD);
 		}
 	}
 
 	/**
 	 * This method writes the encoded password to the file.
-	 * @param filename File to be written. 
+	 * @param filename File to be written.
 	 * @param encodedPassword Encoded password.
 	 */
 	private static void writeToFile(String filename, String encodedPassword)
 	{
-		//Mandar: 781 10-May-06: 
+		//Mandar: 781 10-May-06:
 		try
 		{
 			File fileObject = new File(filename);
