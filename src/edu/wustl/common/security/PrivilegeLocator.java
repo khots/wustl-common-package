da88aa169aef782270b2b951971fdbe03a4d7959
ravi_kumar <ravi_kumar>
2008-10-17 10:23:20 +0000

    code refactoring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1597 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/security/PrivilegeLocator.java
+++ src/edu/wustl/common/security/PrivilegeLocator.java
@@ -1,20 +1,14 @@
 package edu.wustl.common.security;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
+import edu.wustl.common.util.global.XMLParserUtility;
 import edu.wustl.common.util.logger.Logger;
 
 /**
@@ -88,7 +82,7 @@ public class PrivilegeLocator
 	 */
 	public void init()
 	{
-		Document doc =getDocument(PRIV_CONF_FILE);
+		Document doc =XMLParserUtility.getDocument(PRIV_CONF_FILE);
 		NodeList privNodeLst = doc.getElementsByTagName(ELE_PRIVILEGE);
 		populateMaps(privNodeLst);
 	}
@@ -122,56 +116,10 @@ public class PrivilegeLocator
 		Privilege privilege;
 		Element privElmnt = (Element) privNode;
 	    privName=privElmnt.getAttribute(ATTR_NAME);
-	    bitNumber = Integer.parseInt(getElementValue(privElmnt,ELE_BIT_NUM));
-	    roleName = getElementValue(privElmnt,ELE_ROLE_NAME);
+	    bitNumber = Integer.parseInt(XMLParserUtility.getElementValue(privElmnt,ELE_BIT_NUM));
+	    roleName = XMLParserUtility.getElementValue(privElmnt,ELE_ROLE_NAME);
 	    privilege= new Privilege(privName,bitNumber,roleName);
 	    namePrivMap.put(privName, privilege);
 	    bitPrivMap.put(bitNumber,privilege);
-	}
-
-	/**
-	 * This method return role name from xml file.
-	 * @param privElmnt String- privilege Element
-	 * @param elementName -Element name for which value has to be return
-	 * @return String Role name
-	 */
-	private String getElementValue(Element privElmnt,String elementName)
-	{
-		String roleName;
-		NodeList roleNmElmntLst = privElmnt.getElementsByTagName(elementName);
-		Element roleNmElmnt = (Element) roleNmElmntLst.item(0);
-		NodeList roleNm = roleNmElmnt.getChildNodes();
-		roleName=((Node) roleNm.item(0)).getNodeValue();
-		return roleName;
-	}
-
-	/**
-	 * This method returns the Document object for xml file.
-	 * @param fileName File name.
-	 * @return Document xml document.
-	 */
-	private Document getDocument(String fileName)
-	{
-		Document doc=null;
-		try
-		{
-			File file = new File(fileName);
-			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			DocumentBuilder documentBuilder = dbf.newDocumentBuilder();
-			doc=documentBuilder.parse(file);
-		}
-		catch (ParserConfigurationException exception)
-		{
-			logger.error("DocumentBuilder cannot be created",exception);
-		}
-		catch (SAXException exception)
-		{
-			logger.error("Can not parse the xml file:"+fileName,exception);
-		}
-		catch (IOException exception)
-		{
-			logger.error("Can not parse the xml file:"+fileName,exception);
-		}
-		return doc;
 	}
 }
