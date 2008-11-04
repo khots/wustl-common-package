d827122eb50295fda655b2b7f4d443d13df8496b
ravi_kumar <ravi_kumar>
2008-11-04 08:14:27 +0000

    code refactoring.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1633 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/global/TextConstants.java
+++ src/edu/wustl/common/util/global/TextConstants.java
@@ -1,15 +1,42 @@
 package edu.wustl.common.util.global;
 
-
+/**
+ * This file is to store all Text constants.
+ * @author ravi_kumar
+ *
+ */
 public class TextConstants
 {
-
+	/**
+	 * constants for line seperator.
+	 */
 	public static final String LINE_SEPARATOR = System.getProperty(("line.separator"));
+	/**
+	 * constants for two consecutive new line character.
+	 */
 	public static final String TWO_LINE_BREAK = "\n\n";
+	/**
+	 * Constant for new line character.
+	 */
 	public static final String LINE_BREAK = "\n";
+	/**
+	 * Constant for tab character.
+	 */
 	public static final String TAB = "\t";
+	/**
+	 * Constant for Empty String.
+	 */
 	public static final String EMPTY_STRING = "";
+	/**
+	 * Constant for zero.
+	 */
 	public static final String STR_ZERO = "0";
+	/**
+	 * XML file name to initialize title table mappting.
+	 */
 	public static final String TITLI_TABLE_MAPPING_FILE="titli-table-mapping.xml";
+	/**
+	 * XML file name to initialize Privileges Map.
+	 */
 	public static final String PERMSN_MAP_DET_FILE="PermissionMapDetails.xml";
 }
