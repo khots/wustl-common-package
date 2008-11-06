e72e22418c2a42d08ef1da9887d98e6186be2ca8
Kalpana_Thakur <Kalpana_Thakur>
2008-11-06 12:37:26 +0000

    DAO Refractoring (PHASE I)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1658 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/dao/MySQLDAOImpl.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/dao/MySQLDAOImpl.java
@@ -1,7 +1,22 @@
 package edu.wustl.common.dao;
 
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
+import edu.wustl.common.util.Utility;
 import edu.wustl.common.util.dbmanager.DAOException;
 import edu.wustl.common.util.logger.Logger;
 
@@ -59,6 +74,237 @@ public class MySQLDAOImpl extends JDBCDAOImpl
 		return timeFormatFunction;
 	}
 	
+	
+	/* (non-Javadoc)
+	 * @see edu.wustl.common.dao.JDBCDAO#insert(java.lang.String, java.util.List)
+	 */
+	public void insert(String tableName, List columnValues) throws DAOException, SQLException
+	{
+		insert(tableName, columnValues, null);
+	}
+	
+	/**
+	 * @param tableName
+	 * @param columnValues
+	 * @param columnNames
+	 * @throws DAOException
+	 * @throws SQLException
+	 */
+	public void insert(String tableName, List columnValues, List<String>... columnNames) throws DAOException, SQLException
+	{
+		List dateColumns = new ArrayList();
+		List numberColumns = new ArrayList();
+		List tinyIntColumns = new ArrayList();
+		List columnNames_t = getColumns(tableName, columnValues,
+				dateColumns,numberColumns,tinyIntColumns,columnNames);
+
+		String insertQuery = createInsertQuery(tableName,columnNames_t,columnValues);
+		
+		PreparedStatement stmt = null;
+		try
+		{
+			stmt = getPreparedStatement(insertQuery);
+			for (int i = 0; i < columnValues.size(); i++)
+			{
+				Object obj = columnValues.get(i);
+				/**
+				 * For Number -1 is used as MarkUp data For Date 1-1-9999 is used as markUp data.
+				 * Please refer bug 3576
+				 */
+
+				if (obj != null && dateColumns.contains(new Integer(i + 1)) && obj.toString().equals("##"))
+				{
+					java.util.Date date = null;
+					try
+					{
+						date = Utility.parseDate("1-1-9999", "mm-dd-yyyy");
+					}
+					catch (ParseException e)
+					{
+						e.printStackTrace();
+					}
+					Date sqlDate = new Date(date.getTime());
+					stmt.setDate(i + 1, sqlDate);
+				}
+				/** Name : Aarti Sharma
+				* Reviewer:  Prafull Kadam
+				* Bug ID: 4126
+				* Patch ID: 4126_2
+				* See also: 4126_1
+				* Desciption: If the value of the column is true set 1 in the statement else set 0.
+				* This is necessary for MySQL since all boolean values in MySQL are stored in tinyint.
+				* If this is not done then all values will be set as 0
+				* irrespective of whether the value is true or false.
+				*/
+				else if (tinyIntColumns.contains(new Integer(i + 1)))
+				{
+					if (obj != null && (obj.equals("true") || obj.equals("TRUE") || obj.equals("1")))
+					{
+						stmt.setObject(i + 1, 1);
+					}
+					else
+					{
+						stmt.setObject(i + 1, 0);
+					}
+				}
+				else
+				{
+					Timestamp date = isColumnValueDate(obj);
+					if (date != null)
+					{
+						stmt.setObject(i + 1, date);
+						//Logger.out.debug("t.toString(): " + "---" + date);
+					}
+					else
+					{
+						if (obj != null && numberColumns.contains(new Integer(i + 1)) && obj.toString().equals("##"))
+						{
+							stmt.setObject(i + 1, new Integer(-1));
+						}
+						else
+						{
+							stmt.setObject(i + 1, obj);
+						}
+					}
+				}
+			}
+			stmt.executeUpdate();
+		}
+		catch (SQLException sqlExp)
+		{
+			sqlExp.printStackTrace();
+			throw new DAOException(sqlExp.getMessage(), sqlExp);
+		}
+		finally
+		{
+			try
+			{
+				if (stmt != null)
+					stmt.close();
+			}
+			catch (SQLException ex)
+			{
+				throw new DAOException(ex.getMessage(), ex);
+			}
+		}
+	}
+	
+	private List getColumns(String tableName, List columnValues, List dateColumns,
+			List numberColumns,List tinyIntColumns,List<String>... columnNames) throws SQLException
+	{
+		StringBuffer sql = new StringBuffer("Select ");
+		Statement statement = getConnectionStmt();
+		ResultSet resultSet = null;
+		ResultSetMetaData metaData = null;
+		
+		List<String> columnNames_t;
+		if (columnNames != null && columnNames.length > 0)
+		{
+			columnNames_t = columnNames[0];
+			for (int i = 0; i < columnNames_t.size(); i++)
+			{
+				sql.append(columnNames_t.get(i));
+				if (i != columnNames_t.size() - 1)
+				{
+					sql.append(",");
+				}
+			}
+			sql.append(" from " + tableName + " where 1!=1");
+			resultSet = statement.executeQuery(sql.toString());
+			metaData = resultSet.getMetaData();
+
+		}
+		else
+		{
+			sql.append("* from " + tableName + " where 1!=1");
+			resultSet = statement.executeQuery(sql.toString());
+			metaData = resultSet.getMetaData();
+
+			columnNames_t = new ArrayList<String>();
+			for (int i = 1; i <= metaData.getColumnCount(); i++)
+			{
+				columnNames_t.add(metaData.getColumnName(i));
+			}
+		}
+
+		for (int i = 1; i <= metaData.getColumnCount(); i++)
+		{
+			String type = metaData.getColumnTypeName(i);
+			if (type.equals("DATE"))
+				dateColumns.add(new Integer(i));
+			if (type.equals("NUMBER"))
+				numberColumns.add(new Integer(i));
+			if (type.equals("TINYINT"))
+				tinyIntColumns.add(new Integer(i));
+
+		}
+
+		resultSet.close();
+		statement.close();
+		return columnNames_t;
+	}
+	
+	private String createInsertQuery(String tableName,List columnNames_t,List columnValues)
+	{
+		StringBuffer query = new StringBuffer("INSERT INTO " + tableName + "(");
+
+		Iterator<String> columnIterator = columnNames_t.iterator();
+		while (columnIterator.hasNext())
+		{
+			query.append(columnIterator.next());
+			if (columnIterator.hasNext())
+			{
+				query.append(",");
+			}
+			else
+			{
+				query.append(") values(");
+			}
+		}
+
+		//StringBuffer query = new StringBuffer("INSERT INTO " + tableName + " values(");
+		//Changed implementation with column names
+
+		
+		Iterator it = columnValues.iterator();
+		while (it.hasNext())
+		{
+			it.next();
+			query.append("?");
+
+			if (it.hasNext())
+				query.append(",");
+			else
+				query.append(")");
+		}
+		return query.toString();
+	}
+
+	
+	private Timestamp isColumnValueDate(Object value)
+	{
+		//Logger.out.debug("Column value: " + value);
+		try
+		{
+			DateFormat formatter = new SimpleDateFormat("MM-dd-yyyy");
+			formatter.setLenient(false);
+			java.util.Date date = formatter.parse((String) value);
+			Timestamp t = new Timestamp(date.getTime());
+			// Date sqlDate = new Date(date.getTime());
+
+			//Logger.out.debug("Column date value: " + date);
+			if (value != null && value.toString().equals("") == false)
+			{
+				//Logger.out.debug("Return true: " + value);
+				return t;
+			}
+		}
+		catch (Exception e)
+		{
+
+		}
+		return null;
+	}
 	
 
 	public String getActivityStatus(String sourceObjectName, Long indetifier) throws DAOException
