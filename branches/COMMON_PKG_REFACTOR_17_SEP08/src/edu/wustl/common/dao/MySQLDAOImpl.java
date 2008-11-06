9b3b9dfe1664a9dcd735ca95a3422f2ff40dd71f
Kalpana_Thakur <Kalpana_Thakur>
2008-11-06 07:54:02 +0000

    DAO Refractoring (PHASE I)
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1653 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/dao/MySQLDAOImpl.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/dao/MySQLDAOImpl.java
@@ -1,120 +1,16 @@
 package edu.wustl.common.dao;
 
-import java.sql.Connection;
-import java.sql.SQLException;
-
-import edu.wustl.common.audit.AuditManager;
 import edu.wustl.common.beans.SessionDataBean;
-import edu.wustl.common.security.exceptions.SMException;
+import edu.wustl.common.security.exceptions.UserNotAuthorizedException;
 import edu.wustl.common.util.dbmanager.DAOException;
-import edu.wustl.common.util.dbmanager.DBUtil;
-import edu.wustl.common.util.global.Constants;
 import edu.wustl.common.util.logger.Logger;
 
 
 public class MySQLDAOImpl extends JDBCDAOImpl
 {
 	
-	private Connection connection = null;
-	protected AuditManager auditManager;
 	private static org.apache.log4j.Logger logger = Logger.getLogger(MySQLDAOImpl.class);
-
-	/**
-	 * This method will be used to establish the session with the database.
-	 * Declared in AbstractDAO class.
-	 * 
-	 * @throws DAOException
-	 */
-	public void openSession(SessionDataBean sessionDataBean) throws DAOException
-	{
-		auditManager = new AuditManager();
-		if (sessionDataBean != null)
-		{
-			auditManager.setUserId(sessionDataBean.getUserId());
-			auditManager.setIpAddress(sessionDataBean.getIpAddress());
-		}
-		else
-		{
-			auditManager.setUserId(null);
-		}
-
-		try
-		{
-			//Creates a connection.
-			connection = DBUtil.getConnection();// getConnection(database, loginName, password);
-			connection.setAutoCommit(false);
-		}
-		catch (Exception sqlExp)
-		{
-			//throw new DAOException(sqlExp.getMessage(),sqlExp);
-			logger.error(sqlExp.getMessage(), sqlExp);
-			throw new DAOException(Constants.GENERIC_DATABASE_ERROR, sqlExp);
-		}
-	}
-	
-	/**
-	 * This method will be used to close the session with the database.
-	 * Declared in AbstractDAO class.
-	 * @throws DAOException
-	 */
-	public void closeSession() throws DAOException
-	{
-		try
-		{
-			auditManager = null;
-			DBUtil.closeConnection();
-			//        	if (connection != null && !connection.isClosed())
-			//        	    connection.close();
-		}
-		catch (Exception sqlExp)
-		{
-			//            new DAOException(sqlExp.getMessage(),sqlExp);
-			logger.error(sqlExp.getMessage(), sqlExp);
-			throw new DAOException(Constants.GENERIC_DATABASE_ERROR, sqlExp);
-
-		}
-	}
-
-	/**
-	 * Commit the database level changes.
-	 * Declared in AbstractDAO class.
-	 * @throws DAOException
-	 * @throws SMException
-	 */
-	public void commit() throws DAOException
-	{
-		try
-		{
-			auditManager.insert(this);
-
-			if (connection != null)
-				connection.commit();
-		}
-		catch (SQLException dbex)
-		{
-			logger.error(dbex.getMessage(), dbex);
-			throw new DAOException(Constants.GENERIC_DATABASE_ERROR, dbex);
-		}
-	}
-
-	/**
-	 * Rollback all the changes after last commit. 
-	 * Declared in AbstractDAO class. 
-	 * @throws DAOException
-	 */
-	public void rollback()
-	{
-		try
-		{
-			if (connection != null)
-				connection.rollback();
-		}
-		catch (SQLException dbex)
-		{
-			logger.error(dbex.getMessage(), dbex);
-		}
-	}
-	
+		
 	/**
 	 * Deletes the specified table
 	 * @param tableName
@@ -126,7 +22,95 @@ public class MySQLDAOImpl extends JDBCDAOImpl
 		query = new StringBuffer("DROP TABLE IF EXISTS " + tableName);
 			executeUpdate(query.toString());
 	}
+			
 
 	
+	public String getDatePattern()
+	{
+		String datePattern = "%m-%d-%Y";
+		return datePattern;
+	}
 	
+	public String getTimePattern()
+	{
+		String timePattern = "%H:%i:%s";
+		return timePattern;
+	}
+	public String getDateFormatFunction()
+	{
+		String dateFormatFunction = "DATE_FORMAT";
+		return dateFormatFunction;
+	}
+	public String getTimeFormatFunction()
+	{
+		String timeFormatFunction = "TIME_FORMAT";
+		return timeFormatFunction;
+	}
+	
+	public String getDateTostrFunction()
+	{
+		String timeFormatFunction = "TO_CHAR";
+		return timeFormatFunction;
+	}
+	
+	public String getStrTodateFunction()
+	{
+		String timeFormatFunction = "STR_TO_DATE";
+		return timeFormatFunction;
+	}
+	
+	
+
+	public String getActivityStatus(String sourceObjectName, Long indetifier) throws DAOException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public void audit(Object obj, Object oldObj, SessionDataBean sessionDataBean, boolean isAuditable) throws DAOException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void delete(Object obj) throws DAOException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void disableRelatedObjects(String tableName, String whereColumnName, Long[] whereColumnValues) throws DAOException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void insert(Object obj, SessionDataBean sessionDataBean, boolean isAuditable, boolean isSecureInsert) throws DAOException, UserNotAuthorizedException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public Object retrieveAttribute(String sourceObjectName, Long identifier, String attributeName) throws DAOException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	public void update(Object obj, SessionDataBean sessionDataBean, boolean isAuditable, boolean isSecureUpdate, boolean hasObjectLevelPrivilege) throws DAOException, UserNotAuthorizedException
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	
+		
 }
