6fee6872a96d3ba12db4b68060fa4ae574f7f150
ravi_kumar <ravi_kumar>
2008-11-07 12:38:49 +0000

    created new file instead of Variables.java
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1664 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/util/global/CommonServiceLocator.java
@@ -0,0 +1,204 @@
+package edu.wustl.common.util.global;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Properties;
+
+import edu.wustl.common.util.XMLPropertyHandler;
+import edu.wustl.common.util.logger.Logger;
+
+
+/**
+ *
+ * @author ravi_kumar
+ *
+ */
+public final class CommonServiceLocator
+{
+	/**
+	 * logger Logger - Generic logger.
+	 */
+	private static org.apache.log4j.Logger logger = Logger.getLogger(CommonServiceLocator.class);
+
+	/**
+	 * object of class CommonServiceLocator.
+	 */
+	private static CommonServiceLocator commonServLocator= new CommonServiceLocator();
+	/**
+	 * Application Name.
+	 */
+	private static String appName;
+	/**
+	 * Application Version.
+	 */
+	private static String appVersion;
+	/**
+	 * Application Home.
+	 */
+	private static String appHome;
+	/**
+	 * Directory path of properties file.
+	 */
+	private static String propDirPath;
+	/**
+	 * Application URL.
+	 */
+	private static String appURL;
+
+	/**
+	 *No argument constructor.
+	 *Here all the properties are set
+	 */
+	private CommonServiceLocator()
+	{	Properties props=initProps();
+		setAppName(props);
+		setAppVersion(props);
+		setAppHome(props);
+		setPropDirPath();
+	}
+
+	/**
+	 * This method return object of the class CommonServiceLocator.
+	 * @return object of the class CommonServiceLocator.
+	 */
+	public static CommonServiceLocator getInstance()
+	{
+		return commonServLocator;
+	}
+	/**
+	 * This method loads properties file.
+	 * @return Properties object.
+	 */
+	private Properties initProps()
+	{
+		Properties props= new Properties();
+		URL url = ClassLoader.getSystemResource("ApplicationResources.properties");
+	    try
+		{
+			props.load(url.openStream());
+		}
+		catch (IOException exception)
+		{
+			logger.fatal("Not able to load properties file",exception);
+		}
+	    return props;
+	}
+	/**
+	 * @return the application name.
+	 */
+	public String getAppName()
+	{
+		return appName;
+	}
+
+
+	/**
+	 * Set the application name.
+	 * @param props Object of Properties
+	 */
+	private void setAppName(Properties props)
+	{
+		appName=props.getProperty("app.name");
+	}
+
+
+	/**
+	 * @return the application version.
+	 */
+	public String getAppVersion()
+	{
+		return appVersion;
+	}
+
+
+	/**
+	 * Set the application version.
+	 * @param props Object of Properties
+	 */
+	private void setAppVersion(Properties props)
+	{
+		appVersion=props.getProperty("app.version");
+	}
+
+
+	/**
+	 * @return the application home directory where application is running.
+	 */
+	public String getAppHome()
+	{
+		return appHome;
+	}
+
+
+	/**
+	 * Set application home directory where application is running.
+	 * @param props Object of Properties
+	 */
+	private void setAppHome(Properties props)
+	{
+		appHome=props.getProperty("app.home.dir");
+	}
+
+
+	/**
+	 * @return the path of directory of property files.
+	 */
+	public String getPropDirPath()
+	{
+		return propDirPath;
+	}
+
+
+	/**
+	 * Set path of directory of property files.
+	 */
+	private void setPropDirPath()
+	{
+		String path = System.getProperty("app.propertiesFile");
+		try
+		{
+			XMLPropertyHandler.init(path);
+		}
+		catch(Exception exception)
+		{
+			logger.error("Not able to initialize directory path of property files.");
+		}
+    	File propetiesDirPath = new File(path);
+    	propDirPath = propetiesDirPath.getParent();
+	}
+
+
+	/**
+	 * @return the Application URL.
+	 */
+	public String getAppURL()
+	{
+		return appURL;
+	}
+
+
+	/**
+	 * This method Set Application URL.
+	 * @param requestURL request URL.
+	 */
+	public void setAppURL(String requestURL)
+	{
+		if (appURL == null || TextConstants.EMPTY_STRING.equals(appURL.trim()))
+		{
+			String tempUrl=TextConstants.EMPTY_STRING;
+			try
+			{
+				URL url = new URL(requestURL);
+				tempUrl = url.getProtocol() + "://" + url.getAuthority() + url.getPath();
+				appURL = tempUrl.substring(0, tempUrl.lastIndexOf('/'));
+				logger.debug("Application URL set: " + appURL);
+			}
+			catch (MalformedURLException urlExp)
+			{
+				logger.error(urlExp.getMessage(), urlExp);
+			}
+		}
+	}
+}
