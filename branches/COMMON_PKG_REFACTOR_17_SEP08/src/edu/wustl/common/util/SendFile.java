beebb12ce657e41082619d740c32eb9389ac953b
ravi_kumar <ravi_kumar>
2008-11-11 09:10:36 +0000

    code refactoring.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1674 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/SendFile.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/SendFile.java
@@ -26,6 +26,13 @@ public class SendFile
 	 */
 	private static org.apache.log4j.Logger logger = Logger.getLogger(SendFile.class);
 
+	/**
+	 *
+	 * @param response HttpServletResponse object.
+	 * @param filePath Path of file.
+	 * @param fileName name of file.
+	 * @param contentType Content type of attachment.
+	 */
 	public static void sendFileToClient(HttpServletResponse response, String filePath,
 			String fileName, String contentType)
 	{
@@ -38,34 +45,44 @@ public class SendFile
 				response.setHeader("Content-Disposition", "attachment;filename=\""
 						+ fileName+ "\";");
 				response.setContentLength((int) file.length());
-				BufferedInputStream bis=null;
-				try
-				{
-					OutputStream opstream = response.getOutputStream();
-					bis = new BufferedInputStream(new FileInputStream(file));
-					int count;
-					byte[] buf = new byte[Constants.FOUR_KILO_BYTES];
-					while ((count = bis.read(buf)) > -1)
-					{
-						opstream.write(buf, 0, count);
-					}
-					opstream.flush();
-					bis.close();
-					file.delete();
-				}
-				catch (FileNotFoundException ex)
-				{
-					logger.error("Exception in method sendFileToClient:"+ex.getMessage(), ex);
-				}
-				catch (IOException ex)
-				{
-					logger.error("Exception in method sendFileToClient:"+ex.getMessage(), ex);
-				}
+				writeToStream(response, file);
 			}
 			else
 			{
 				logger.error("Sorry Cannot Download as fileName is null");
 			}
+		}
+	}
+
+	/**
+	 * This method write data into OutputStream.
+	 * @param response HttpServletResponse
+	 * @param file File object to write.
+	 */
+	private static void writeToStream(HttpServletResponse response, File file)
+	{
+		BufferedInputStream bis=null;
+		try
+		{
+			OutputStream opstream = response.getOutputStream();
+			bis = new BufferedInputStream(new FileInputStream(file));
+			int count;
+			byte[] buf = new byte[Constants.FOUR_KILO_BYTES];
+			while ((count = bis.read(buf)) > -1)
+			{
+				opstream.write(buf, 0, count);
+			}
+			opstream.flush();
+			bis.close();
+			file.delete();
+		}
+		catch (FileNotFoundException ex)
+		{
+			logger.error("Exception in method sendFileToClient:"+ex.getMessage(), ex);
+		}
+		catch (IOException ex)
+		{
+			logger.error("Exception in method sendFileToClient:"+ex.getMessage(), ex);
 		}
 	}
 }
