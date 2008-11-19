ce81928d6aeeb1cff7bd68769741f3220c3b87f6
prashant_bandal <prashant_bandal>
2008-11-19 04:34:34 +0000

    code refactoring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1713 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/actionForm/TimeStampTimeValidator.java
@@ -0,0 +1,42 @@
+/**
+ *
+ */
+package edu.wustl.common.actionForm;
+
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+
+import edu.wustl.common.util.global.Constants;
+import edu.wustl.common.util.global.Validator;
+
+
+/**
+ * @author prashant_bandal
+ *
+ */
+public class TimeStampTimeValidator implements ValidatorDataTypeInterface
+{
+
+	/* (non-Javadoc)
+	 * @see edu.wustl.common.actionForm.ValidatorDataTypeInterface#
+	 * validate(java.lang.String, java.lang.String, org.apache.struts.action.ActionErrors)
+	 */
+	/**
+	 * This method validate TimeStampTime values.
+	 * @param enteredValue entered Value.
+	 * @param errors errors.
+	 * @return conditionError boolean value.
+	 */
+	public boolean validate(String enteredValue, ActionErrors errors)
+	{
+		Validator validator = new Validator();
+		boolean conditionError = false;
+		if (!(validator.isValidTime(enteredValue, Constants.TIME_PATTERN_HH_MM_SS)))
+		{
+			errors.add(ActionErrors.GLOBAL_ERROR, new ActionError("simpleQuery.time.format"));
+			conditionError = true;
+		}
+		return conditionError;
+	}
+
+}
