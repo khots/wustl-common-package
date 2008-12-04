7eabfade18d95c14e63d9666d4bb5e0cb2563fb2
prashant_bandal <prashant_bandal>
2008-12-04 11:13:48 +0000

    code refactoring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1842 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/exceptionformatter/ExceptionFormatter.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/exceptionformatter/ExceptionFormatter.java
@@ -2,12 +2,17 @@
 package edu.wustl.common.exceptionformatter;
 
 /**
- * 
  * @author sachin_lale
- * Description: Interface defines method for formatting the database specific Exception message  
+ * Description: Interface defines method for formatting the database specific Exception message
  */
 public interface ExceptionFormatter
 {
 
-	public String formatMessage(Exception objExcp, Object args[]);
+	/**
+	 * This method format Message.
+	 * @param objExcp Exception.
+	 * @param args arguments.
+	 * @return formated Message.
+	 */
+	String formatMessage(Exception objExcp, Object []args);
 }
