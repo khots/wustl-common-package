87d39299d76be950ed522dc8f4630d6bb88eeda9
prashant_bandal <prashant_bandal>
2008-12-09 10:41:37 +0000

    code refactoring
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1873 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/util/global/CDEConstants.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+
+package edu.wustl.common.util.global;
+
+/**
+ * @author prashant_bandal
+ *
+ */
+public class CDEConstants
+{
+	/**
+	 * Constant for CDEConfig file.
+	 */
+	public static final String CDE_CONF_FILE = "CDEConfig.xml";
+	/**
+	 * Constant for CDE_XML_PACKAGE_PATH.
+	 */
+	public static final String CDE_XML_PACKAGE_PATH = "edu.wustl.common.cde.xml";
+	/**
+	 * Constant for CDE_NAME.
+	 */
+	public static final String CDE_NAME = "cdeName";
+}
