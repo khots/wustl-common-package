31d38ef108968e28be8392633677a4f1924beed2
ravi_kumar <ravi_kumar>
2008-12-11 08:45:26 +0000

    added more test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1905 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/bizlogic/DefaultBizLogicTestCase.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/bizlogic/DefaultBizLogicTestCase.java
@@ -1,11 +1,15 @@
 package edu.wustl.common.bizlogic;
 
+import java.util.List;
+
 import edu.wustl.common.CommonBaseTestCase;
+import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.util.logger.Logger;
 import edu.wustl.dao.DAO;
 import edu.wustl.dao.MyDAOImpl;
 import edu.wustl.dao.daofactory.DAOConfigFactory;
 import edu.wustl.dao.daofactory.IDAOFactory;
+import edu.wustl.dao.exception.DAOException;
 
 /**
  * Test cases for DefaultBizLogic.
@@ -18,76 +22,156 @@ public class DefaultBizLogicTestCase extends CommonBaseTestCase
 	 * Generic Logger.
 	 */
 	private static org.apache.log4j.Logger logger = Logger.getLogger(DefaultBizLogicTestCase.class);
-	
+
+	/**
+	 * Application name for DAO configuration.This must be same as in file ApplicationDAOProperties.xml.
+	 */
+	private static final String APP_NAME="commonpackagetest";
+	/**
+	 * DAO factory.
+	 */
+	private static final  IDAOFactory DAO_FACTORY = DAOConfigFactory.getInstance().getDAOFactory(APP_NAME);
+
+	/**
+	 * Positive test for
+	 * List retrieve(String sourceObjectName, String[] selectColumnName,
+			String[] whereColumnName, String[] whereColumnCondition, Object[] whereColumnValue,
+			String joinCondition).
+	 */
+	public void testRetrive1()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=false;
+			String []selectColumnName={"selectColumnName"};
+			String []whereColumnName={"whereColumnName"};
+			String []whereColumnCondition={"="};
+			String []whereColumnValue={"whereColumnValue"};
+			String joinCondition="joinCondition";
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
+			List<Object> list=defaultBizLogic.retrieve("SourceObjName",selectColumnName,
+					whereColumnName,whereColumnCondition,whereColumnValue,joinCondition);
+			assertEquals(MyDAOImpl.list.size(), list.size());
+		}
+		catch (BizLogicException exception)
+		{
+			fail("Not able to retrieve data.");
+			logger.fatal(exception.getMessage());
+		}
+	}
+	/**
+	 * Negative test for
+	 * List retrieve(String sourceObjectName, String[] selectColumnName,
+			String[] whereColumnName, String[] whereColumnCondition, Object[] whereColumnValue,
+			String joinCondition).
+	 */
+	public void testFailRetrive1()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=true;
+			String []selectColumnName={"selectColumnName"};
+			String []whereColumnName={"whereColumnName"};
+			String []whereColumnCondition={"="};
+			String []whereColumnValue={"whereColumnValue"};
+			String joinCondition="joinCondition";
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
+			List<Object> list=defaultBizLogic.retrieve("SourceObjName",selectColumnName,
+					whereColumnName,whereColumnCondition,whereColumnValue,joinCondition);
+			fail("Negative test case: Should throw BizLogicException");
+		}
+		catch (BizLogicException exception)
+		{
+			assertTrue("Negative test case:Thrown BizLogicException",true);
+			logger.fatal(exception.getMessage());
+		}
+	}
+
+	/**
+	 * Positive test case for insert(Object,DAO,SessionDataBean).
+	 */
 	public void testInsertWithSessionDataBean()
 	{
-		IDAOFactory daoFactory = DAOConfigFactory.getInstance().getDAOFactory("commonpackagetest");
 		try
 		{
 			MyDAOImpl.isTestForFail=false;
-			DAO myJdbcDao = daoFactory.getDAO();
+			DAO myJdbcDao = DAO_FACTORY.getDAO();
 			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
 			defaultBizLogic.insert(null,myJdbcDao,null);
 			assertTrue("Data inserted successfully.",true);
 		}
-		catch (Exception exception)	
+		catch (Exception exception)
 		{
 			fail("Not able to insert data.");
 			logger.fatal(exception.getMessage());
 		}
 	}
-	
+
+	/**
+	 * Negative test case for insert(Object,DAO,SessionDataBean).
+	 */
 	public void testFailInsertWithSessionDataBean()
 	{
-		IDAOFactory daoFactory = DAOConfigFactory.getInstance().getDAOFactory("commonpackagetest");
 		try
 		{
 			MyDAOImpl.isTestForFail=true;
-			DAO myJdbcDao = daoFactory.getDAO();
-			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();			
+			DAO myJdbcDao = DAO_FACTORY.getDAO();
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
 			defaultBizLogic.insert(null,myJdbcDao,null);
 			fail("Negative test case: should not inserted data.");
 		}
+		catch (BizLogicException exception)
+		{
+			assertTrue("Negative test case:Thrown BizLogicException",true);
+			logger.fatal(exception.getMessage(),exception);
+		}
 		catch (Exception exception)
 		{
-			assertTrue("Error:exception should be thrown",true);
-			logger.fatal(exception.getMessage());
+			fail("Negative test case: should throw only BizLogicException");
+			logger.fatal(exception.getMessage(),exception);
 		}
 	}
-	
+	/**
+	 * Positive test case for insert(Object,DAO).
+	 */
 	public void testInsert()
 	{
-		IDAOFactory daoFactory = DAOConfigFactory.getInstance().getDAOFactory("commonpackagetest");
 		try
 		{
 			MyDAOImpl.isTestForFail=false;
-			DAO myJdbcDao = daoFactory.getDAO();
+			DAO myJdbcDao = DAO_FACTORY.getDAO();
 			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
 			defaultBizLogic.insert(null,myJdbcDao);
 			assertTrue("Data inserted successfully.",true);
 		}
-		catch (Exception exception)	
+		catch (Exception exception)
 		{
 			fail("Not able to insert data.");
 			logger.fatal(exception.getMessage());
 		}
 	}
+	/**
+	 * Negative test case for insert(Object,DAO).
+	 */
 	public void testFailInsert()
 	{
-		IDAOFactory daoFactory = DAOConfigFactory.getInstance().getDAOFactory("commonpackagetest");
-
 		try
 		{
 			MyDAOImpl.isTestForFail=true;
-			DAO myJdbcDao = daoFactory.getDAO();
-			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();			
+			DAO myJdbcDao = DAO_FACTORY.getDAO();
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
 			defaultBizLogic.insert(null,myJdbcDao);
 			fail("Negative test case: should not inserted data.");
 		}
+		catch (BizLogicException exception)
+		{
+			assertTrue("Negative test case:Thrown BizLogicException",true);
+			logger.fatal(exception.getMessage(),exception);
+		}
 		catch (Exception exception)
 		{
-			assertTrue("Error:exception should be thrown",true);
-			logger.fatal(exception.getMessage());
+			fail("Negative test case: should throw only BizLogicException");
+			logger.fatal(exception.getMessage(),exception);
 		}
 	}
 }
