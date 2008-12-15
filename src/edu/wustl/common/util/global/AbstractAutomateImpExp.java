5c1bf9250d0d217da233c4d15ab80f690f56e20d
ravi_kumar <ravi_kumar>
2008-12-15 09:35:59 +0000

    exception handling.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@1943 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/global/AbstractAutomateImpExp.java
+++ src/edu/wustl/common/util/global/AbstractAutomateImpExp.java
@@ -93,7 +93,7 @@ public abstract class AbstractAutomateImpExp implements IAutomateImpExp
 		}
 		catch(IOException exception)
 		{
-			ErrorKey errorKey=ErrorKey.getErrorKey("");
+			ErrorKey errorKey=ErrorKey.getErrorKey("impexp.tablenamelist.error");
 			throw new ApplicationException(errorKey,null,"INot able to get import class");
 		}
 		return tableNamesList;
