39ee47142a52aa3ee2654b90045bc6e9a3f3ff49
ravi_kumar <ravi_kumar>
2008-12-15 08:50:40 +0000

    refactoring of AutomateImport class
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@1939 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/AutomateImport.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/AutomateImport.java
@@ -1,21 +1,11 @@
 
 package edu.wustl.common.util.global;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.sql.Types;
-import java.util.ArrayList;
-import java.util.List;
+
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.exception.ErrorKey;
+
 
 /**
  * This class is for import/export data to database.
@@ -25,40 +15,14 @@ import java.util.List;
 public class AutomateImport
 {
 	/**
-	 * dbUtility object is used to store the argument values and getting database connection.
-	 */
-	private static DatabaseUtility dbUtility= new DatabaseUtility();
-
-	/**
 	 * Minimum number of arguments required.
 	 */
 	private static final int MIN_NO_OF_ARGS=10;
 
 	/**
-	 * oracle tns name.
-	 */
-	private static String oracleTnsName;
-
-	/**
 	 * Index for CA model operation like import or export.
 	 */
 	private static final int  INDX_FOR_OPERATION=7;
-	/**
-	 * Index for file which contains all table names.
-	 */
-	private static final int  INDX_FOR_TABLE_NAMES_FILE=8;
-	/**
-	 * Index for csv file name.
-	 */
-	private static final int  INDX_FOR_CSV_FILE_NAME=9;
-	/**
-	 * Index for CTL file name.
-	 */
-	private static final int  INDX_FOR_CTL_FILE_PATH=10;
-	/**
-	 * Index for oracle tns name.
-	 */
-	private static final int  INDX_FOR_TNS_NAME=11;
 
 	/**
 	 *
@@ -76,323 +40,48 @@ public class AutomateImport
 	 * 			- folder path for CAModelCSVs files
 	 * 			- folder path for CAModelCTLs files required in case of oracle
 	 * 			- oracle.tns.name required in case of oracle
-	 * @throws Exception -Generic exception.
+	 * @throws ApplicationException -Generic Application exception.
 	 */
-	public static void main(String[] args) throws Exception
+	public static void main(String[] args) throws ApplicationException
 	{
-		Connection connection = null;
-		try
+		AutomateImport automateImport = new AutomateImport();
+		DatabaseUtility dbUtility= new DatabaseUtility();
+		automateImport.configureDBConnection(args,dbUtility);
+		IAutomateImpExp impExpObj=dbUtility.getAutomatImpExpObj();
+		impExpObj.init(dbUtility);
+		if(args[INDX_FOR_OPERATION].equalsIgnoreCase("import"))
 		{
-			AutomateImport automateImport = new AutomateImport();
-			automateImport.configureDBConnection(args);
-			connection = dbUtility.getConnection();
-			List<String> tableNamesList = automateImport
-							.getTableNamesList(args[INDX_FOR_TABLE_NAMES_FILE]);
-			int size = tableNamesList.size();
-			String filePath = args[INDX_FOR_CSV_FILE_NAME].replaceAll("\\\\", "//");
-			if (DatabaseUtility.ORACLE_DATABASE.equals(dbUtility.getDbType().toUpperCase()))
-			{
-				oracleTnsName = args[INDX_FOR_TNS_NAME];
-				String filePathCTL = args[INDX_FOR_CTL_FILE_PATH].replaceAll("\\\\", "//");
-				if (args[INDX_FOR_OPERATION].equalsIgnoreCase("import"))
-				{
-					for (int i = 0; i < size; i++)
-					{
-						String ctlFilePath = filePathCTL + tableNamesList.get(i) + ".ctl";
-						if (!new File(ctlFilePath).exists())
-						{
-							String csvFilePath = filePath
-								+ tableNamesList.get(i) + ".csv";
-							automateImport.createCTLFiles(connection,
-									csvFilePath,ctlFilePath,
-									tableNamesList.get(i));
-						}
-						automateImport.importDataOracle(ctlFilePath);
-					}
-				}
-				else
-				{
-					for (int i = 0; i < size; i++)
-					{
-						String ctlFilePath = filePathCTL + tableNamesList.get(i) + ".ctl";
-						String csvFilePath = filePath + tableNamesList.get(i) + ".csv";
-						automateImport.createCTLFiles(connection, csvFilePath, ctlFilePath,
-								tableNamesList.get(i));
-					}
-				}
-			}
-			else
-			{
-				if (args[INDX_FOR_OPERATION].equalsIgnoreCase("import"))
-				{
-					Statement stmt = connection.createStatement();
-					stmt.execute("SET FOREIGN_KEY_CHECKS=0;");
-					for (int i = 0; i < size; i++)
-					{
-						String dumpFilePath = filePath + tableNamesList.get(i) + ".csv";
-						automateImport.importDataMySQL(connection,
-								dumpFilePath, tableNamesList.get(i));
-					}
-					stmt.execute("SET FOREIGN_KEY_CHECKS=1;");
-				}
-				else
-				{
-					for (int i = 0; i < size; i++)
-					{
-						String dumpFilePath = filePath + tableNamesList.get(i) + ".csv";
-						automateImport.exportDataMySQL(connection,
-								dumpFilePath, tableNamesList.get(i));
-					}
-				}
-			}
+			impExpObj.executeImport(args);
 		}
-		finally
+		else
 		{
-			connection.close();
+			impExpObj.executeExport(args);
 		}
 	}
 
 	/**
 	 * assigning database parameters.
 	 * @param args String[] of configuration info
-	 * @throws Exception  Generic exception
+	 * @param dbUtility DatabaseUtility
+	 * @throws ApplicationException  Generic Application Exception
 	 */
-	private void configureDBConnection(String[] args) throws Exception
+	private void configureDBConnection(String[] args,DatabaseUtility dbUtility) throws ApplicationException
 	{
 		if (args.length < MIN_NO_OF_ARGS)
 		{
-			throw new Exception("In sufficient number of arguments");
+			try
+			{
+				ErrorKey.init("-");
+			}
+			catch (IOException exception)
+			{
+				ErrorKey errorKey=ErrorKey.getErrorKey("");
+				throw new ApplicationException(errorKey,exception,"Properties file not found.");
+			}
+			ErrorKey errorKey=ErrorKey.getErrorKey("");
+			throw new ApplicationException(errorKey,null,"Insufficient number of arguments");
 		}
 		dbUtility.setDbParams(args);
 	}
-
-	/**
-	 *  This method will insert the data to database.
-	 * @param conn Connection object
-	 * @param filename File Name
-	 * @param tableName Table Name
-	 * @throws SQLException generic exception
-	 */
-	private void importDataMySQL(Connection conn, String filename, String tableName)
-			throws SQLException
-	{
-		Statement stmt=null;
-		try
-		{
-			stmt = conn.createStatement
-				(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			StringBuffer query= new StringBuffer("LOAD DATA LOCAL INFILE '")
-			.append(filename).append("' INTO TABLE ").append(tableName)
-			.append(" FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n';");
-			stmt.execute(query.toString());
-		}
-		finally
-		{
-			stmt.close();
-		}
-	}
-
-	/**
-	 *  This method will export the data to database.
-	 * @param conn Connection object
-	 * @param fileName File Name
-	 * @param tableName Table Name
-	 * @throws SQLException generic exception
-	 */
-	private void exportDataMySQL(Connection conn, String fileName, String tableName)
-			throws SQLException
-	{
-		Statement stmt=null;
-		try
-		{
-			File file = new File(fileName);
-			if (file.exists())
-			{
-				file.delete();
-			}
-			stmt = conn.createStatement
-			(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			StringBuffer query=new StringBuffer("SELECT * INTO OUTFILE '")
-			.append(fileName)
-			.append("' FIELDS TERMINATED BY ',' ENCLOSED BY '\"' LINES TERMINATED BY '\\n' FROM ")
-			.append(tableName).append(";");
-			stmt.execute(query.toString());
-		}
-		finally
-		{
-			stmt.close();
-		}
-	}
-
-	/**
-	 * This method will read the table list file.
-	 * @param fileName file name
-	 * @return table list
-	 * @throws IOException Generic IO exception
-	 */
-	private List<String> getTableNamesList(String fileName) throws IOException
-	{
-		ArrayList<String> tableNamesList = new ArrayList<String>();
-		BufferedReader reader = new BufferedReader(new FileReader(new File(fileName)));
-		String linereader = reader.readLine();
-		while (linereader != null)
-		{
-			tableNamesList.add(linereader);
-			linereader = reader.readLine();
-		}
-		return tableNamesList;
-	}
-
-	/**
-	 * This method will insert the data to database.
-	 * @param fileName File Name
-	 * @throws Exception generic exception
-	 */
-	private void importDataOracle(String fileName) throws Exception
-	{
-		StringBuffer cmd = new StringBuffer("sqlldr ").append(dbUtility.getDbUserName())
-								.append('/').append(dbUtility.getDbPassword())
-								.append('@').append(oracleTnsName)
-								.append(" control=").append(fileName);
-		Runtime runtime = Runtime.getRuntime();
-		Process proc = runtime.exec(cmd.toString());
-		// any error message?
-		StreamGobbler errorGobbler = new StreamGobbler(proc.getErrorStream());
-
-		// any output?
-		StreamGobbler outputGobbler = new StreamGobbler(proc.getInputStream());
-		errorGobbler.start();
-		outputGobbler.start();
-		proc.waitFor();
-
-	}
-
-	/**
-	 * This method will create control file for SQL loader.
-	 * @param connection Connection Object.
-	 * @param csvFileName CSV file name.
-	 * @param ctlFileName ctl file name
-	 * @param tableName table name
-	 * @throws IOException Generic IO Exception
-	 * @throws SQLException Generic SQL Exception.
-	 */
-	private void createCTLFiles(Connection connection, String csvFileName, String ctlFileName,
-			String tableName) throws IOException, SQLException
-	{
-		File file = new File(ctlFileName);
-		if (file.exists())
-		{
-			file.delete();
-		}
-		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(ctlFileName)));
-		String value= new StringBuffer("LOAD DATA INFILE '")
-			.append(csvFileName)
-			.append("' \nBADFILE '/sample.bad'\nDISCARDFILE '/sample.dsc'\nAPPEND \nINTO TABLE ")
-			.append(tableName)
-			.append(" \nFIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'\n").toString();
-
-		String columnName = getColumnName(connection, tableName);
-		bufferedWriter.write(value + columnName);
-		bufferedWriter.flush();
-		bufferedWriter.close();
-	}
-
-	/**
-	 * This method will retrieve the column name list for a given table.
-	 * @param connection Connection Object.
-	 * @param tableName Table Name.
-	 * @return column Name.
-	 * @throws SQLException generic SQL exception.
-	 */
-	private String getColumnName(Connection connection, String tableName) throws SQLException
-	{
-		String query = "select * from " + tableName + " where 1=2";
-		Statement stmt = null;
-		ResultSet resultSet = null;
-		try
-		{
-			stmt = connection.createStatement();
-			resultSet = stmt.executeQuery(query);
-			StringBuffer columnNameList = getColumnNameList(resultSet);
-			return columnNameList.toString();
-		}
-		finally
-		{
-			if (stmt != null)
-			{
-				stmt.close();
-			}
-			if (resultSet != null)
-			{
-				resultSet.close();
-			}
-
-		}
-	}
-
-	/**
-	 * @param resultSet ResultSet object.
-	 * @return list of column name
-	 * @throws SQLException Generic SQL Exception.
-	 */
-	private StringBuffer getColumnNameList(ResultSet resultSet) throws SQLException
-	{
-		StringBuffer columnNameList = new StringBuffer();
-		columnNameList.append('(');
-		ResultSetMetaData rsMetaData = resultSet.getMetaData();
-		int numberOfColumns = rsMetaData.getColumnCount();
-		for (int i = 1; i <= numberOfColumns; i++)
-		{
-			setColList(columnNameList, rsMetaData, i);
-		}
-		columnNameList.deleteCharAt(columnNameList.length()-1);
-		columnNameList.append(')');
-		return columnNameList;
-	}
-
-	/**
-	 * @param columnNameList column Name List.
-	 * @param rsMetaData rs Meta Data
-	 * @param intVar int Variable
-	 * @throws SQLException SQL Exception.
-	 */
-	private void setColList(StringBuffer columnNameList, ResultSetMetaData rsMetaData, int intVar)
-			throws SQLException
-	{
-		columnNameList.append(rsMetaData.getColumnName(intVar));
-		if (Types.DATE == rsMetaData.getColumnType(intVar)
-				|| Types.TIMESTAMP == rsMetaData.getColumnType(intVar))
-		{
-			columnNameList.append(" DATE 'YYYY-MM-DD'");
-		}
-		if (!("HIDDEN".equals(rsMetaData.getColumnName(intVar)))
-				&& !("FORMAT".equals(rsMetaData.getColumnName(intVar))))
-		{
-			columnNameList.append(" NULLIF ");
-			columnNameList.append(rsMetaData.getColumnName(intVar));
-			columnNameList.append("='\\\\N'");
-		}
-		columnNameList.append(',');
-	}
 }
-/**
- *This class is for output of any  message(or error message) during import.
- *
- */
-class StreamGobbler extends Thread
-{
-	/**
-	 * InputStream object.
-	 */
-	private InputStream inputStream;
 
-	/**
-	 * One argument constructor.
-	 * @param inputStream InputStream object.
-	 */
-	StreamGobbler(InputStream inputStream)
-	{
-		super();
-		this.inputStream = inputStream;
-	}
-}
---
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/OracleAutomateImpExp.java
@@ -0,0 +1,322 @@
+package edu.wustl.common.util.global;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Types;
+
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.exception.ErrorKey;
+
+/**
+ * This class is for import/export meta data for Oracle.
+ * @author ravi_kumar
+ *
+ */
+public class OracleAutomateImpExp extends AbstractAutomateImpExp
+{
+
+	/**
+	 * Index for CTL file name.
+	 */
+	private static final int  INDX_FOR_CTL_FILE_PATH=10;
+
+	/**
+	 * Index for oracle tns name.
+	 */
+	private static final int  INDX_FOR_TNS_NAME=11;
+
+	/**
+	 * oracle tns name.
+	 */
+	private String oracleTnsName;
+
+	/**
+	 * path of CTL file.
+	 */
+	private String filePathCTL;
+
+	/**
+	 * This method do some process before import/export operation.
+	 * @param args String array of parameters.
+	 * @throws ApplicationException Application Exception
+	 */
+	protected void preImpExp(String[] args)  throws ApplicationException
+	{
+		super.preImpExp(args);
+		setOracleTnsName(args[INDX_FOR_TNS_NAME]);
+		setFilePathCTL(args[INDX_FOR_CTL_FILE_PATH].replaceAll("\\\\", "//"));
+	}
+
+	/**
+	 * Method to export meta data.
+	 * @param args String array of parameters.
+	 * @throws ApplicationException Application Exception
+	 */
+	public void executeExport(String[] args) throws ApplicationException
+	{
+		preImpExp(args);
+		try
+		{
+			for (int i = 0; i < getSize(); i++)
+			{
+				String ctlFilePath = filePathCTL + getTableNamesList().get(i) + ".ctl";
+				String csvFilePath = getFilePath() + getTableNamesList().get(i) + ".csv";
+				createCTLFiles(csvFilePath, ctlFilePath,
+						getTableNamesList().get(i));
+			}
+		}
+		catch(Exception exception)
+		{
+			ErrorKey errorKey=ErrorKey.getErrorKey("");
+			throw new ApplicationException(errorKey,null,"Insufficient number of arguments");
+		}
+
+	}
+	/**
+	 * Method to import meta data.
+	 * @param args String array of parameters.
+	 * @throws ApplicationException Application Exception
+	 */
+	public void executeImport(String[] args) throws ApplicationException
+	{
+		preImpExp(args);
+		try
+		{
+		for (int i = 0; i < getSize(); i++)
+		{
+			String ctlFilePath = filePathCTL + getTableNamesList().get(i) + ".ctl";
+			if (!new File(ctlFilePath).exists())
+			{
+				String csvFilePath = getFilePath()
+					+ getTableNamesList().get(i) + ".csv";
+				createCTLFiles(csvFilePath,ctlFilePath,
+						getTableNamesList().get(i));
+			}
+			importDataOracle(ctlFilePath);
+		}
+		}
+		catch(Exception exception)
+		{
+			ErrorKey errorKey=ErrorKey.getErrorKey("");
+			throw new ApplicationException(errorKey,null,"Insufficient number of arguments");
+		}
+
+	}
+
+	/**
+	 * Method to get database connection.
+	 * @return Oracle database connection
+	 * @throws SQLException Generic SQL exception.
+	 * @throws ClassNotFoundException throws this exception if Driver class not found in class path.
+	 */
+	public Connection getConnection() throws SQLException, ClassNotFoundException
+	{
+		Connection connection=null;
+		DatabaseUtility dbUtil=getDbUtility();
+		Class.forName(dbUtil.getDbDriver());
+		String url = new StringBuffer("jdbc:oracle:thin:@").append(dbUtil.getDbServerNname())
+						.append(":").append(dbUtil.getDbServerPortNumber())
+						.append(":").append(dbUtil.getDbName()).toString();
+		connection= DriverManager.getConnection(url, dbUtil.getDbUserName()
+						,dbUtil.getDbPassword());
+		return connection;
+	}
+
+	/**
+	 * This method will insert the data to database.
+	 * @param fileName File Name
+	 * @throws Exception generic exception
+	 */
+	private void importDataOracle(String fileName) throws Exception
+	{
+		StringBuffer cmd = new StringBuffer("sqlldr ").append(getDbUtility().getDbUserName())
+								.append('/').append(getDbUtility().getDbPassword())
+								.append('@').append(oracleTnsName)
+								.append(" control=").append(fileName);
+		Runtime runtime = Runtime.getRuntime();
+		Process proc = runtime.exec(cmd.toString());
+		// any error message?
+		StreamGobbler errorGobbler = new StreamGobbler(proc.getErrorStream());
+
+		// any output?
+		StreamGobbler outputGobbler = new StreamGobbler(proc.getInputStream());
+		errorGobbler.start();
+		outputGobbler.start();
+		proc.waitFor();
+
+	}
+
+	/**
+	 * This method will create control file for SQL loader.
+	 * @param csvFileName CSV file name.
+	 * @param ctlFileName ctl file name
+	 * @param tableName table name
+	 * @throws IOException Generic IO Exception
+	 * @throws SQLException Generic SQL Exception.
+	 * @throws ClassNotFoundException throws this exception if Driver class not found in class path.
+	 */
+	private void createCTLFiles(String csvFileName, String ctlFileName,
+			String tableName) throws IOException,  SQLException, ClassNotFoundException
+	{
+		File file = new File(ctlFileName);
+		if (file.exists())
+		{
+			file.delete();
+		}
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(new File(ctlFileName)));
+		String value= new StringBuffer("LOAD DATA INFILE '")
+			.append(csvFileName)
+			.append("' \nBADFILE '/sample.bad'\nDISCARDFILE '/sample.dsc'\nAPPEND \nINTO TABLE ")
+			.append(tableName)
+			.append(" \nFIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"'\n").toString();
+
+		String columnName = getColumnName(tableName);
+		bufferedWriter.write(value + columnName);
+		bufferedWriter.flush();
+		bufferedWriter.close();
+	}
+
+	/**
+	 * This method will retrieve the column name list for a given table.
+	 * @param tableName Table Name.
+	 * @return column Name.
+	 * @throws SQLException generic SQL exception.
+	 * @throws ClassNotFoundException throws this exception if Driver class not found in class path.
+	 */
+	private String getColumnName(String tableName) throws SQLException, ClassNotFoundException
+	{
+		Statement stmt = null;
+		ResultSet resultSet = null;
+		try
+		{
+			Connection connection=getConnection();
+			String query = "select * from " + tableName + " where 1=2";
+			stmt = connection.createStatement();
+			resultSet = stmt.executeQuery(query);
+			StringBuffer columnNameList = getColumnNameList(resultSet);
+			return columnNameList.toString();
+		}
+		finally
+		{
+			if (stmt != null)
+			{
+				stmt.close();
+			}
+			if (resultSet != null)
+			{
+				resultSet.close();
+			}
+
+		}
+	}
+
+	/**
+	 * @param resultSet ResultSet object.
+	 * @return list of column name
+	 * @throws SQLException Generic SQL Exception.
+	 */
+	private StringBuffer getColumnNameList(ResultSet resultSet) throws SQLException
+	{
+		StringBuffer columnNameList = new StringBuffer();
+		columnNameList.append('(');
+		ResultSetMetaData rsMetaData = resultSet.getMetaData();
+		int numberOfColumns = rsMetaData.getColumnCount();
+		for (int i = 1; i <= numberOfColumns; i++)
+		{
+			setColList(columnNameList, rsMetaData, i);
+		}
+		columnNameList.deleteCharAt(columnNameList.length()-1);
+		columnNameList.append(')');
+		return columnNameList;
+	}
+
+	/**
+	 * @param columnNameList column Name List.
+	 * @param rsMetaData rs Meta Data
+	 * @param intVar int Variable
+	 * @throws SQLException SQL Exception.
+	 */
+	private void setColList(StringBuffer columnNameList, ResultSetMetaData rsMetaData, int intVar)
+			throws SQLException
+	{
+		columnNameList.append(rsMetaData.getColumnName(intVar));
+		if (Types.DATE == rsMetaData.getColumnType(intVar)
+				|| Types.TIMESTAMP == rsMetaData.getColumnType(intVar))
+		{
+			columnNameList.append(" DATE 'YYYY-MM-DD'");
+		}
+		if (!("HIDDEN".equals(rsMetaData.getColumnName(intVar)))
+				&& !("FORMAT".equals(rsMetaData.getColumnName(intVar))))
+		{
+			columnNameList.append(" NULLIF ");
+			columnNameList.append(rsMetaData.getColumnName(intVar));
+			columnNameList.append("='\\\\N'");
+		}
+		columnNameList.append(',');
+	}
+
+	/**
+	 * @return the oracleTnsName
+	 */
+	public String getOracleTnsName()
+	{
+		return oracleTnsName;
+	}
+
+	/**
+	 * @param oracleTnsName the oracleTnsName to set
+	 */
+	public void setOracleTnsName(String oracleTnsName)
+	{
+		this.oracleTnsName = oracleTnsName;
+	}
+
+	/**
+	 * @return the filePathCTL
+	 */
+	public String getFilePathCTL()
+	{
+		return filePathCTL;
+	}
+
+	/**
+	 * @param filePathCTL the filePathCTL to set
+	 */
+	public void setFilePathCTL(String filePathCTL)
+	{
+		this.filePathCTL = filePathCTL;
+	}
+
+
+	/**
+	 *This class is for output of any  message(or error message) during import.
+	 *
+	 */
+	class StreamGobbler extends Thread
+	{
+		/**
+		 * InputStream object.
+		 */
+		private InputStream inputStream;
+
+		/**
+		 * One argument constructor.
+		 * @param inputStream InputStream object.
+		 */
+		StreamGobbler(InputStream inputStream)
+		{
+			super();
+			this.inputStream = inputStream;
+		}
+	}
+
+}
