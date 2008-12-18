509e4d5c6c358a9388f74dce8320e1f97625c087
ravi_kumar <ravi_kumar>
2008-12-18 13:49:41 +0000

    test cases for methods in AbstractBizLogic.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2038 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/bizlogic/AbstractBizLogicTestCase.java
@@ -0,0 +1,99 @@
+package edu.wustl.common.bizlogic;
+
+import titli.model.util.TitliResultGroup;
+import edu.wustl.common.CommonBaseTestCase;
+import edu.wustl.common.domain.MyDomainObject;
+import edu.wustl.common.exception.BizLogicException;
+import edu.wustl.common.util.logger.Logger;
+import edu.wustl.dao.DAO;
+import edu.wustl.dao.MyDAOImpl;
+import edu.wustl.dao.daofactory.DAOConfigFactory;
+import edu.wustl.dao.daofactory.IDAOFactory;
+
+
+public class AbstractBizLogicTestCase extends CommonBaseTestCase
+{
+	/**
+	 * Generic Logger.
+	 */
+	private static org.apache.log4j.Logger logger = Logger.getLogger(AbstractBizLogicTestCase.class);
+
+	/**
+	 * Application name for DAO configuration.This must be same as in file ApplicationDAOProperties.xml.
+	 */
+	private static final String APP_NAME="commonpackagetest";
+	/**
+	 * DAO factory.
+	 */
+	private static final  IDAOFactory DAO_FACTORY = DAOConfigFactory.getInstance().getDAOFactory(APP_NAME);
+
+	/**
+	 *  Positive test case for public void delete(Object).
+	 */
+	public void testDelete()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=false;
+			MyDomainObject ado = getAbstractDomainObject();
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
+			defaultBizLogic.delete(ado);
+			assertTrue("Object deleted successfuly.", true);
+		}
+		catch (Exception exception)
+		{
+			fail("Not able to delete the object.");
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+	/**
+	 *  Negative test case for public void delete(Object).
+	 */
+	public void testFailDelete()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=true;
+			MyDomainObject ado = getAbstractDomainObject();
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
+			defaultBizLogic.delete(ado);
+			fail("Negative test case:must throw BizLogicException.");
+		}
+		catch (BizLogicException exception)
+		{
+			assertTrue("Negative test case: Thrown BizLogicException during delete operation.", true);
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+
+	/**
+	 *  Positive test case for public void delete(Object).
+	 */
+	public void testInsertWithSessionDataBean()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=false;
+			MyDomainObject ado = getAbstractDomainObject();
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
+			defaultBizLogic.insert(ado);
+			assertTrue("Object inserted successfuly.", true);
+		}
+		catch (Exception exception)
+		{
+			fail("Not able to insert the object.");
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+
+	/**
+	 * @return AbstractDomainObject
+	 */
+	private MyDomainObject getAbstractDomainObject()
+	{
+		MyDomainObject ado= new MyDomainObject();
+		ado.setActivityStatus("Active");
+		ado.setId(Long.valueOf(1));
+		return ado;
+	}
+}
