be7be1892e98225466edd6ea52da7b4277b6c417
ravi_kumar <ravi_kumar>
2008-12-19 08:15:23 +0000

    added more test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2050 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/bizlogic/AbstractBizLogicTestCase.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/bizlogic/AbstractBizLogicTestCase.java
@@ -1,7 +1,10 @@
 package edu.wustl.common.bizlogic;
 
-import titli.model.util.TitliResultGroup;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import edu.wustl.common.CommonBaseTestCase;
+import edu.wustl.common.domain.AbstractDomainObject;
 import edu.wustl.common.domain.MyDomainObject;
 import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.util.logger.Logger;
@@ -9,6 +12,7 @@ import edu.wustl.dao.DAO;
 import edu.wustl.dao.MyDAOImpl;
 import edu.wustl.dao.daofactory.DAOConfigFactory;
 import edu.wustl.dao.daofactory.IDAOFactory;
+import edu.wustl.dao.exception.DAOException;
 
 
 public class AbstractBizLogicTestCase extends CommonBaseTestCase
@@ -35,7 +39,7 @@ public class AbstractBizLogicTestCase extends CommonBaseTestCase
 		try
 		{
 			MyDAOImpl.isTestForFail=false;
-			MyDomainObject ado = getAbstractDomainObject();
+			MyDomainObject ado = getAbstractDomainObject(1);
 			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
 			defaultBizLogic.delete(ado);
 			assertTrue("Object deleted successfuly.", true);
@@ -54,7 +58,7 @@ public class AbstractBizLogicTestCase extends CommonBaseTestCase
 		try
 		{
 			MyDAOImpl.isTestForFail=true;
-			MyDomainObject ado = getAbstractDomainObject();
+			MyDomainObject ado = getAbstractDomainObject(1);
 			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
 			defaultBizLogic.delete(ado);
 			fail("Negative test case:must throw BizLogicException.");
@@ -67,14 +71,14 @@ public class AbstractBizLogicTestCase extends CommonBaseTestCase
 	}
 
 	/**
-	 *  Positive test case for public void delete(Object).
+	 *  Positive test case for public final void insert(Object obj).
 	 */
 	public void testInsertWithSessionDataBean()
 	{
 		try
 		{
 			MyDAOImpl.isTestForFail=false;
-			MyDomainObject ado = getAbstractDomainObject();
+			MyDomainObject ado = getAbstractDomainObject(1);
 			MyDefaultBizLogic defaultBizLogic = new MyDefaultBizLogic();
 			defaultBizLogic.insert(ado);
 			assertTrue("Object inserted successfuly.", true);
@@ -87,14 +91,14 @@ public class AbstractBizLogicTestCase extends CommonBaseTestCase
 	}
 
 	/**
-	 *  Positive test case for public void delete(Object).
+	 *  Negative test case for public final void insert(Object obj).
 	 */
 	public void testFailInsertWithSessionDataBean()
 	{
 		try
 		{
 			MyDAOImpl.isTestForFail=true;
-			MyDomainObject ado = getAbstractDomainObject();
+			MyDomainObject ado = getAbstractDomainObject(1);
 			MyDefaultBizLogic defaultBizLogic = new MyDefaultBizLogic();
 			defaultBizLogic.insert(ado);
 			fail("Negative test case:must throw BizLogicException.");
@@ -107,13 +111,110 @@ public class AbstractBizLogicTestCase extends CommonBaseTestCase
 	}
 
 	/**
+	 *  Positive test case for
+	 *  public final void insert(Collection objCollection,
+			SessionDataBean sessionDataBean, boolean isInsertOnly).
+	 */
+	public void testInsertCollection()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=false;
+			Collection<AbstractDomainObject> adoList= new ArrayList<AbstractDomainObject>();
+			adoList.add(getAbstractDomainObject(1));
+			adoList.add(getAbstractDomainObject(2));
+			MyDefaultBizLogic defaultBizLogic = new MyDefaultBizLogic();
+			defaultBizLogic.insert(adoList,null,true);
+			assertTrue("Object inserted successfuly.", true);
+		}
+		catch (BizLogicException exception)
+		{
+			fail("Not able to insert the object.");
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+	
+	/**
+	 *  Negative test case for
+	 *  public final void insert(Collection objCollection,
+			SessionDataBean sessionDataBean, boolean isInsertOnly).
+	 */
+	public void testFailInsertCollection()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=true;
+			Collection<AbstractDomainObject> adoList= new ArrayList<AbstractDomainObject>();
+			adoList.add(getAbstractDomainObject(1));
+			adoList.add(getAbstractDomainObject(2));
+			MyDefaultBizLogic defaultBizLogic = new MyDefaultBizLogic();
+			defaultBizLogic.insert(adoList,null,true);
+			fail("Negative test case:must throw BizLogicException.");
+		}
+		catch (BizLogicException exception)
+		{
+			assertTrue("Negative test case: Thrown BizLogicException during collection insert.", true);
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+	
+	/**
+	 *  Negative test case for
+	 *  public final void insert(Collection objCollection,
+			SessionDataBean sessionDataBean, boolean isInsertOnly).
+	 */
+	public void testinsertMultipleWithoutAutorization()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=false;
+			DAO myJdbcDao = DAO_FACTORY.getDAO();
+			Collection<AbstractDomainObject> adoList= new ArrayList<AbstractDomainObject>();
+			adoList.add(getAbstractDomainObject(1));
+			adoList.add(getAbstractDomainObject(2));
+			DefaultBizLogic defaultBizLogic = new DefaultBizLogic();
+			defaultBizLogic.insertMultiple(adoList,myJdbcDao,null);
+			fail("Negative test case:must throw BizLogicException.");
+		}
+		catch (BizLogicException exception)
+		{
+			assertTrue("Negative test case: Thrown BizLogicException during collection insert.", true);
+			logger.fatal(exception.getMessage(),exception);
+		}
+		catch (DAOException exception)
+		{
+			fail("Negative test case:must throw BizLogicException.");
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+
+	/**
+	 *  Positive test case for
+	 *  public final void update(Object currentObj).
+	 */
+	public void testUpdate()
+	{
+		try
+		{
+			MyDAOImpl.isTestForFail=false;
+			MyDefaultBizLogic defaultBizLogic = new MyDefaultBizLogic();
+			defaultBizLogic.update(getAbstractDomainObject(1));
+			assertTrue("Object updated successfuly.", true);
+		}
+		catch (BizLogicException exception)
+		{
+			fail("Not able to update the object.");
+			logger.fatal(exception.getMessage(),exception);
+		}
+	}
+	/**
 	 * @return AbstractDomainObject
 	 */
-	private MyDomainObject getAbstractDomainObject()
+	private MyDomainObject getAbstractDomainObject(int id)
 	{
 		MyDomainObject ado= new MyDomainObject();
 		ado.setActivityStatus("Active");
-		ado.setId(Long.valueOf(1));
+		ado.setId(Long.valueOf(id));
 		return ado;
 	}
 }
