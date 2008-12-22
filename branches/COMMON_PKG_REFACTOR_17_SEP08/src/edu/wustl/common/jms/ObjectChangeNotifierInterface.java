1fdeacec58945af7575b6dcf63e16fac779097fe
ravi_kumar <ravi_kumar>
2008-12-22 05:20:50 +0000

    code refactoring.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2080 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/jms/ObjectChangeNotifierInterface.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/jms/ObjectChangeNotifierInterface.java
@@ -4,16 +4,17 @@ package edu.wustl.common.jms;
 import java.io.Serializable;
 
 /**
- * 
+ *
  * @author kalpana_thakur
- * TODO This interface is used to read the Object 
- * 
+ * TODO This interface is used to read the Object
+ *
  */
 public interface ObjectChangeNotifierInterface extends Serializable
 {
 
 	/**
-	 * This method is used to read the object 
+	 * This method is used to read the object.
+	 *  @param obj Object to read.
 	 * */
 	void read(Object obj);
 }
