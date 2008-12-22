0a6ecf22c1db1e4c5332d51cf6d949546f2445ec
ravi_kumar <ravi_kumar>
2008-12-22 05:22:19 +0000

    modified javadoc
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2081 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/jms/MessagePublisher.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/jms/MessagePublisher.java
@@ -2,22 +2,22 @@
 package edu.wustl.common.jms;
 
 /**
- * 
+ *
  * @author kalpana_thakur
- * TODO This interface is used to initiate the connection and to 
- * publish the message or data   
+ * TODO This interface is used to initiate the connection and to
+ * publish the message or data
  */
 public interface MessagePublisher
 {
 
 	/**
-	 * This method is used to initialize the connection 
+	 * This method is used to initialize the connection.
 	 * */
 	void initialize();
 
 	/**
 	 * @param obj
-	 * This method is used to publish message or data 
+	 * This method is used to publish message or data
 	 */
 	void update(Object obj);
 }
--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/jms/MessageSubscriber.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/jms/MessageSubscriber.java
@@ -2,15 +2,15 @@
 package edu.wustl.common.jms;
 
 /**
- * 
+ *
  * @author kalpana_thakur
- * TODO This interface is used to initiate the connection 
+ * TODO This interface is used to initiate the connection.
  */
 public interface MessageSubscriber
 {
 
 	/**
-	 * This method is used to initialize the connection 
+	 * This method is used to initialize the connection.
 	 * */
 	void initialize();
 }
