ed4b8904babf4549decdcf4f07d3f6329a12ac30
ravi_kumar <ravi_kumar>
2008-12-23 13:48:52 +0000

    changed for ParseException
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2125 38c78225-d16a-144f-a689-2cd41fac0795

--- test/src/edu/wustl/common/factory/AbstractFactoryConfigTestCase.java
+++ test/src/edu/wustl/common/factory/AbstractFactoryConfigTestCase.java
@@ -1,7 +1,7 @@
 package edu.wustl.common.factory;
 
 import edu.wustl.common.CommonBaseTestCase;
-import edu.wustl.common.exception.ParseException;
+import edu.wustl.common.exception.ApplicationException;
 import edu.wustl.common.util.logger.Logger;
 
 
@@ -20,7 +20,7 @@ public class AbstractFactoryConfigTestCase extends CommonBaseTestCase
 			"bizLogicFactory");
 			assertEquals(true, factory instanceof MyBizLogicFactory);
 		}
-		catch (ParseException exception)
+		catch (ApplicationException exception)
 		{
 			fail("Didn't get Factory classes.");
 			logger.debug("Didn't get Factory classes.",exception);
