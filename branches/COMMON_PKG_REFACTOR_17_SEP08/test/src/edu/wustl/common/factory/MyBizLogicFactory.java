cf81564dd22d6fa69d73ebc9fdbe0aa93daef860
ravi_kumar <ravi_kumar>
2008-12-24 06:32:58 +0000

    this class also implements IForwordToFactory.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2132 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/factory/MyBizLogicFactory.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/factory/MyBizLogicFactory.java
@@ -1,12 +1,25 @@
 package edu.wustl.common.factory;
 
 import edu.wustl.common.bizlogic.IBizLogic;
+import edu.wustl.common.util.AbstractForwardToProcessor;
 
 
-public class MyBizLogicFactory implements IFactory
+public class MyBizLogicFactory implements IFactory,IForwordToFactory
 {
 	public IBizLogic getBizLogic(int formId)
 	{
+		return null;
+	}
+
+	public AbstractForwardToProcessor getForwardToPrintProcessor()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public AbstractForwardToProcessor getForwardToProcessor()
+	{
+		// TODO Auto-generated method stub
 		return null;
 	}
 }
