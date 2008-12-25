9a83c84006b820c1bb15e60368211ee8f5943fc3
ravi_kumar <ravi_kumar>
2008-12-25 12:53:48 +0000

    added more methods implementation
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2166 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/factory/MyBizLogicFactory.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/common/factory/MyBizLogicFactory.java
@@ -1,10 +1,14 @@
 package edu.wustl.common.factory;
 
+import edu.wustl.common.actionForm.AbstractActionForm;
 import edu.wustl.common.bizlogic.IBizLogic;
+import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.exception.AssignDataException;
 import edu.wustl.common.util.AbstractForwardToProcessor;
 
 
-public class MyBizLogicFactory implements IFactory,IForwordToFactory
+public class MyBizLogicFactory implements IFactory,IForwordToFactory,IDomainObjectFactory
 {
 	public IBizLogic getBizLogic(int formId)
 	{
@@ -18,6 +22,26 @@ public class MyBizLogicFactory implements IFactory,IForwordToFactory
 	}
 
 	public AbstractForwardToProcessor getForwardToProcessor()
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public AbstractDomainObject getDomainObject(int formType, AbstractActionForm form)
+			throws AssignDataException
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public String getDomainObjectName(int formType)
+	{
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public AbstractActionForm getFormBean(Object domainObject, String operation)
+			throws ApplicationException
 	{
 		// TODO Auto-generated method stub
 		return null;
