75eda8b92defaf88b48a5466b8455be329e6b8b4
deepti_shelar <deepti_shelar>
2008-12-30 09:21:25 +0000

    Merging with latest DAO
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2192 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/util/ExportReport.java
+++ src/edu/wustl/common/util/ExportReport.java
@@ -425,6 +425,6 @@ public class ExportReport
 		DAO dao = daofactory.getJDBCDAO();
 
 		dao.openSession(null);
-		return (List<Object>)dao.executeQuery(sql, null, false, null);
+		return (List<Object>)dao.executeQuery(sql);
 	}
 }
