29441e9fc29492880426a12fabf3fe21427ddbbd
deepti_shelar <deepti_shelar>
2008-12-31 12:54:45 +0000

    Chnaged file name
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2217 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/CSMGroupLocator.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/CSMGroupLocator.java
@@ -29,7 +29,7 @@ public final class CSMGroupLocator
 	/**
 	 * File name for CSM group configuration.
 	 */
-	private static final String CSM_GROUP_CONF_FILE = "CSMGroup.xml";
+	private static final String CSM_GROUP_CONF_FILE = "CSMGroupConf.xml";
 
 	/**
 	 * Specifies parse Exception Message.
