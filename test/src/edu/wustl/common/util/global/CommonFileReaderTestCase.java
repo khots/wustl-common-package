91fbbd6ce54f868844cc6e2761de1428152a4ad6
ravi_kumar <ravi_kumar>
2008-12-31 11:19:10 +0000

    test class for CommonFileReader
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2213 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ test/src/edu/wustl/common/util/global/CommonFileReaderTestCase.java
@@ -0,0 +1,16 @@
+package edu.wustl.common.util.global;
+
+import edu.wustl.common.CommonBaseTestCase;
+
+
+public class CommonFileReaderTestCase extends CommonBaseTestCase
+{
+	public void testReadData()
+	{
+		String fileName= new StringBuffer(System.getProperty("user.dir")).
+		append(System.getProperty("file.separator")).
+		append("src/edu/wustl/common/util/global/CommonFileReader.java").toString();
+		String data=new CommonFileReader().readData(fileName);
+		assertTrue(data.length()>0);
+	}
+}
