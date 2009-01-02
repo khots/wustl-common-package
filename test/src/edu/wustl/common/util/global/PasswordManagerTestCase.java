79eac326cf4cb0f79489b01607adc5b92a54b42c
ravi_kumar <ravi_kumar>
2009-01-02 09:38:52 +0000

    Added more test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2250 38c78225-d16a-144f-a689-2cd41fac0795

--- test/src/edu/wustl/common/util/global/PasswordManagerTestCase.java
+++ test/src/edu/wustl/common/util/global/PasswordManagerTestCase.java
@@ -1,8 +1,10 @@
 package edu.wustl.common.util.global;
 
 import edu.wustl.common.CommonBaseTestCase;
+import edu.wustl.common.beans.SessionDataBean;
 import edu.wustl.common.exception.PasswordEncryptionException;
 import edu.wustl.common.util.logger.Logger;
+import edu.wustl.dao.MyDAOImpl;
 
 
 public class PasswordManagerTestCase extends CommonBaseTestCase
@@ -51,6 +53,200 @@ public class PasswordManagerTestCase extends CommonBaseTestCase
 		{
 			logger.debug("Encode-Decode not successful.",exception);
 			fail("Encode-Decode not successful.");
+		}
+	}
+
+	public void testFailValidatePasswordOnFormBeanForPwdChangeInSameSession()
+	{
+		try
+		{
+			String oldPassword="oldPassword";
+			String newPassword="newPassword";
+			Boolean pwdChangedInsameSession=true;
+			int result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_SAME_SESSION, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password if it has changed in same session.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testFailValidatePasswordOnFormBeanForMinLength()
+	{
+		try
+		{
+			String oldPassword="oldPassword";
+			String newPassword="abc";
+			Boolean pwdChangedInsameSession=false;
+			int result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_LENGTH, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testFailValidatePasswordOnFormBeanForOldPassword()
+	{
+		try
+		{
+			String oldPassword="oldPassword";
+			String newPassword="oldPassword";
+			Boolean pwdChangedInsameSession=false;
+			int result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_SAME_AS_OLD, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testFailValidatePasswordUpLowerNumSpaceCharInPass()
+	{
+		try
+		{
+			String oldPassword="oldPassword";
+
+			//Password must contains at least One Upper character.
+			String newPassword="login123";
+			Boolean pwdChangedInsameSession=false;
+			int result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_IN_PATTERN, result);
+
+			//Password must contains at least One Lower character.
+			newPassword="LOGINABCD";
+			result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_IN_PATTERN, result);
+
+			//Password must contains at least One digit.
+			newPassword="Loginabcd";
+			result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_IN_PATTERN, result);
+
+			//Password must not contains any space.
+			newPassword="Login abcd";
+			result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.FAIL_IN_PATTERN, result);
+
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testValidatePasswordOnFormBean()
+	{
+		try
+		{
+			String oldPassword="oldPassword";
+			String newPassword="Login123";
+			Boolean pwdChangedInsameSession=false;
+			int result = PasswordManager.validatePasswordOnFormBean(
+					newPassword,oldPassword,pwdChangedInsameSession);
+			assertEquals(PasswordManager.SUCCESS, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testFailValidateForWrongOldPassword()
+	{
+		try
+		{
+			MyDAOImpl.returnUserPassword=true;
+			String oldPassword="oldPassword";
+			String newPassword="Login123";
+			Boolean pwdChangedInsameSession=false;
+			SessionDataBean sessionData= new SessionDataBean();
+			int result = PasswordManager.validate(
+					newPassword,oldPassword,pwdChangedInsameSession,sessionData);
+			assertEquals(PasswordManager.FAIL_WRONG_OLD_PASSWORD, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testFailValidateForMail()
+	{
+		try
+		{
+			MyDAOImpl.returnUserPassword=true;
+			String oldPassword="Login123";
+			String newPassword="Login1234";
+			Boolean pwdChangedInsameSession=false;
+			SessionDataBean sessionData= new SessionDataBean();
+			sessionData.setUserName("Login1234@pspl.com");
+			int result = PasswordManager.validate(
+					newPassword,oldPassword,pwdChangedInsameSession,sessionData);
+			assertEquals(PasswordManager.FAIL_SAME_AS_USERNAME, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testValidateForUserName()
+	{
+		try
+		{
+			MyDAOImpl.returnUserPassword=true;
+			String oldPassword="Login123";
+			String newPassword="ravi_kumar@pspl.co.in";
+			Boolean pwdChangedInsameSession=false;
+			SessionDataBean sessionData= new SessionDataBean();
+			sessionData.setUserName("ravi_kumar@pspl.co.in");
+			int result = PasswordManager.validate(
+					newPassword,oldPassword,pwdChangedInsameSession,sessionData);
+			assertEquals(PasswordManager.FAIL_SAME_AS_USERNAME, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
+		}
+	}
+
+	public void testValidate()
+	{
+		try
+		{
+			MyDAOImpl.returnUserPassword=true;
+			String oldPassword="Login123";
+			String newPassword="Login1234";
+			Boolean pwdChangedInsameSession=false;
+			SessionDataBean sessionData= new SessionDataBean();
+			sessionData.setUserName("ravi_kumar@pspl.co.in");
+			int result = PasswordManager.validate(
+					newPassword,oldPassword,pwdChangedInsameSession,sessionData);
+			assertEquals(PasswordManager.SUCCESS, result);
+		}
+		catch (Exception exception)
+		{
+			fail("Not validating password for minimum length.");
+			logger.debug("Encode-Decode not successful.",exception);
 		}
 	}
 
