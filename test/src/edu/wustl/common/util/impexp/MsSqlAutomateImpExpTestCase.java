a19ccd91eff0f2b71943b15ce46ada1abfefaa90
ravi_kumar <ravi_kumar>
2009-01-02 14:56:59 +0000

    removed hardcoding for db configuration.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2267 38c78225-d16a-144f-a689-2cd41fac0795

--- test/src/edu/wustl/common/util/impexp/MsSqlAutomateImpExpTestCase.java
+++ test/src/edu/wustl/common/util/impexp/MsSqlAutomateImpExpTestCase.java
@@ -6,12 +6,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.util.global.HibernateProperties;
 import edu.wustl.common.util.logger.Logger;
 
 /**
  * This is for testing of Automate import/export function for MSSQL server database.
  * Please make sure that all the parameters passed into this are correct i.e Check the static array 'ARGS'.
- * Also database server must contains csv file because.   
+ * Also database server must contains csv file because database searches file on server.
  * @author ravi_kumar
  *
  */
@@ -21,20 +22,22 @@ public class MsSqlAutomateImpExpTestCase extends CommonAutomateImpExpTestCase
 	 * Generic Logger.
 	 */
 	private static org.apache.log4j.Logger logger = Logger.getLogger(MsSqlAutomateImpExpTestCase.class);
-	private static final String ARGS[]={
-			"ps4057",
-			"1433",
-			"mssql",
-			"testravi",
-			"sa",
-			"ps4057!@#",
-			"com.microsoft.sqlserver.jdbc.SQLServerDriver",
-			"import",
-			System.getProperty("user.dir")+"/SQL/Common/test/Permissible_values/dumpFileColumnInfo.txt",
-			System.getProperty("user.dir")+"/SQL/Common/test/Permissible_values/"
-	};
+	private static final String []ARGS= new String[10];
+	static
+	{
+			ARGS[0]=HibernateProperties.getValue("mssql.db.host");
+			ARGS[1]=HibernateProperties.getValue("mssql.db.port");
+			ARGS[2]="mssql";
+			ARGS[3]=HibernateProperties.getValue("mssql.db.name");
+			ARGS[4]=HibernateProperties.getValue("mssql.db.username");
+			ARGS[5]=HibernateProperties.getValue("mssql.db.password");
+			ARGS[6]=HibernateProperties.getValue("mssql.db.driver");
+	}
 	public void testMsSqlAutomateImport()
 	{
+		ARGS[7]="import";
+		ARGS[8]=System.getProperty("user.dir")+"/SQL/Common/test/Permissible_values/dumpFileColumnInfo.txt";
+		ARGS[9]=System.getProperty("user.dir")+"/SQL/Common/test/Permissible_values/";
 		try
 		{	preImport();
 			AutomateImport.main(ARGS);
@@ -44,9 +47,9 @@ public class MsSqlAutomateImpExpTestCase extends CommonAutomateImpExpTestCase
 		{
 			fail("Fail to import metadata.");
 			logger.debug("Fail to import metadata.", exception);
-		}	
+		}
 	}
-	
+
 	public void preImport() throws IOException, SQLException, ClassNotFoundException, ApplicationException
 	{
 		DatabaseUtility dbUtility= new DatabaseUtility();
@@ -55,7 +58,7 @@ public class MsSqlAutomateImpExpTestCase extends CommonAutomateImpExpTestCase
 		try
 		{
 			runSQLFile(System.getProperty("user.dir")+"/SQL/MSSQL/testAutomateImpExp.sql");
-			executeQuery(getConnection(),"SET IDENTITY_INSERT CATISSUE_PERMISSIBLE_VALUE ON");			
+			executeQuery(getConnection(),"SET IDENTITY_INSERT CATISSUE_PERMISSIBLE_VALUE ON");
 			runSQLFile(System.getProperty("user.dir")+"/SQL/Common/test/CDE_DummyData_Common.sql");
 			executeQuery(getConnection(),"SET IDENTITY_INSERT CATISSUE_PERMISSIBLE_VALUE OFF");
 		}
@@ -64,11 +67,10 @@ public class MsSqlAutomateImpExpTestCase extends CommonAutomateImpExpTestCase
 			getConnection().close();
 		}
 	}
-	
+
 	public void executeQuery(Connection conn,String query) throws SQLException
 	{
 		Statement statement=conn.createStatement();
 		statement.execute(query);
-		
 	}
 }
