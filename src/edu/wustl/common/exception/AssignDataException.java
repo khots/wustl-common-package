ed895096667025ef32b57cdbd5d6617c42229839
kalpana_thakur <kalpana_thakur>
2009-01-05 10:01:25 +0000

    Modified
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2285 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/exception/AssignDataException.java
+++ src/edu/wustl/common/exception/AssignDataException.java
@@ -26,35 +26,5 @@ public class AssignDataException extends ApplicationException
 	{
 		super(errorKey, throwable, msgValues);
 	}
-
-	/**
-	 * Parameterised constructor.
-	 * @param message exeption message.
-	 */
-	public AssignDataException(String message)
-	{
-		this(message, null);
-	}
-
-	/**
-	 * Parameterised constructor.
-	 * @param exception Exception
-	 */
-	public AssignDataException(Exception exception)
-	{
-		this("", exception);
-	}
-
-	/**
-	 * Parameterised constructor.
-	 * @param message exeption message.
-	 * @param wrapException The wrapException to set.
-	 */
-	public AssignDataException(String message, Exception wrapException)
-	{
-		/**Added by amit_doshi to fix the bug related to exception chaining
-		 * code reviewer :- abhijit_naik
-		 */
-		super(ErrorKey.getErrorKey("errors.item"), wrapException, message);
-	}
+	
 }
