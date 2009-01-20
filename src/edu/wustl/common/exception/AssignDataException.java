d506ac7ffb4d4148d77a8350c3c5f50ad4cb7f73
prashant_bandal <prashant_bandal>
2009-01-20 08:43:13 +0000

    code refactoring.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2351 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/exception/AssignDataException.java
+++ src/edu/wustl/common/exception/AssignDataException.java
@@ -19,11 +19,11 @@ public class AssignDataException extends ApplicationException
 	 * The Only public constructor to restrict creating object without
 	 * initializing mandatory members.
 	 * @param errorKey The object which will represent the root cause of the error.
-	 * @param throwable root exception, if any, which caused this error.
+	 * @param exception root exception, if any, which caused this error.
 	 * @param msgValues custom message, additional information.
 	 */
-	public AssignDataException(ErrorKey errorKey, Throwable throwable, String msgValues)
+	public AssignDataException(ErrorKey errorKey, Exception exception, String msgValues)
 	{
-		super(errorKey, throwable, msgValues);
+		super(errorKey, exception, msgValues);
 	}
 }
--- src/edu/wustl/common/exception/AuditException.java
+++ src/edu/wustl/common/exception/AuditException.java
@@ -18,11 +18,11 @@ public class AuditException extends ApplicationException
 	/**
 	 * The Only public constructor to restrict creating object without
 	 * initializing mandatory members.
-	 * @param throwable root exception, if any, which caused this error.
+	 * @param exception root exception, if any, which caused this error.
 	 * @param msgValues custom message, additional information.
 	 */
-	public AuditException(Throwable throwable, String msgValues)
+	public AuditException(Exception exception, String msgValues)
 	{
-		super(ErrorKey.getErrorKey("error.audit.fail"),throwable,msgValues);
+		super(ErrorKey.getErrorKey("error.audit.fail"),exception,msgValues);
 	}
 }
