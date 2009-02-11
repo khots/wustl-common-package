4f47e394ecfb3642dc04c59f52262d74cf0e1abe
prashant_bandal <prashant_bandal>
2009-02-11 10:52:31 +0000

    Refactored Logger class.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2364 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/logger/Logger.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/logger/Logger.java
@@ -11,12 +11,7 @@
 
 package edu.wustl.common.util.logger;
 
-import java.io.File;
-
-import javax.servlet.ServletContext;
-
-import org.apache.log4j.PropertyConfigurator;
-
+import org.apache.log4j.Level;
 
 /**
  * This is an utility class which provides functions to get logger objects.
@@ -27,105 +22,193 @@ public final class Logger
 {
 
 	/**
-	 * @deprecated
+	 * @deprecated.
 	 */
-	public static org.apache.log4j.Logger out;
+	@Deprecated
+	public static org.apache.log4j.Logger out = org.apache.log4j.Logger.getLogger("");
 	/**
-	 * Indicates whether Logger is configured or not.
+	 * org.apache.log4j.Logger instance.
 	 */
-	private static boolean isConfigured = false;
+	private org.apache.log4j.Logger logger;
 
 	/**
-	 * Configures the logger with the properties of the specified category name in the log4j.xml file.
-	 * The category should be present in the log4j.xml file in the JBOSS_HOME/server/default/conf folder.
-	 * @param categoryName The category name.
+	 * Logger class should not get instantiated from outside. Hence the constructor is private.
+	 * @param apacheLogger org.apache.log4j.Logger instance.
 	 */
-	public static void configure(String categoryName)
+	private Logger(org.apache.log4j.Logger apacheLogger)
 	{
-		if (out == null)
-		{
-			out = getLogger(Logger.class);
-		}
+		this.logger = apacheLogger;
 	}
 
 	/**
 	 * Creates new logger object for a given class.
 	 * @param className java class for which logger need to be created.
+	 * @return new commonLogger object.
+	 */
+	public static Logger getCommonLogger(Class className)
+	{
+		return new Logger(LoggerConfig.getConfiguredLogger(className));
+	}
+
+	/**
+	 * Log a message object with the DEBUG level.
+	 * This method first checks if this category is DEBUG enabled by comparing the level of this category
+	 * with the DEBUG level. If this category is DEBUG enabled, then it converts the message object
+	 * (passed as parameter) to a string by invoking the appropriate ObjectRenderer.
+	 * It then proceeds to call all the registered appenders in this category and
+	 * also higher in the hierarchy depending on the value of the additivity flag.
+	 * @param message the message object to log.
+	 */
+	public void debug(Object message)
+	{
+		if (logger.isEnabledFor(Level.DEBUG))
+		{
+			logger.debug(message);
+		}
+	}
+
+	/**
+	 * Log a message object with the DEBUG level including the stack trace of
+	 * the Throwable t passed as parameter.
+	 * @param message the message object to log.
+	 * @param throwable the exception to log, including its stack trace.
+	 */
+	public void debug(Object message, Throwable throwable)
+	{
+		if (logger.isEnabledFor(Level.DEBUG))
+		{
+			logger.debug(message, throwable);
+		}
+	}
+
+	/**
+	 * Log a message object with the ERROR Level.
+	 * This method first checks if this category is ERROR enabled
+	 * by comparing the level of this category with ERROR Level.
+	 * If this category is ERROR enabled, then it converts the
+	 * message object passed as parameter to a string by invoking the appropriate ObjectRenderer.
+	 * It proceeds to call all the registered appenders in this category
+	 * and also higher in the hierarchy depending on the value of the additivity flag.
+	 * @param message the message object to log.
+	 */
+	public void error(Object message)
+	{
+		if (logger.isEnabledFor(Level.ERROR))
+		{
+			logger.error(message);
+		}
+	}
+
+	/**
+	 * Log a message object with the ERROR level including the stack trace of
+	 * the Throwable t passed as parameter.
+	 * @param message the message object to log.
+	 * @param throwable the exception to log, including its stack trace.
+	 */
+	public void error(Object message, Throwable throwable)
+	{
+		if (logger.isEnabledFor(Level.ERROR))
+		{
+			logger.error(message, throwable);
+		}
+	}
+
+	/**
+	 * Log a message object with the FATAL Level.
+	 * This method first checks if this category is FATAL enabled by comparing
+	 * the level of this category with FATAL Level. If the category is FATAL enabled,
+	 * then it converts the message object passed as parameter to a string by invoking
+	 * the appropriate ObjectRenderer. It proceeds to call all the registered appenders
+	 * in this category and also higher in the hierarchy depending on the value of the additivity flag.
+	 * @param message the message object to log.
+	 */
+	public void fatal(Object message)
+	{
+		if (logger.isEnabledFor(Level.FATAL))
+		{
+			logger.fatal(message);
+		}
+	}
+
+	/**
+	 * Log a message object with the FATAL level including the stack trace of
+	 * the Throwable t passed as parameter.
+	 * @param message the message object to log.
+	 * @param throwable the exception to log, including its stack trace.
+	 */
+	public void fatal(Object message, Throwable throwable)
+	{
+		if (logger.isEnabledFor(Level.FATAL))
+		{
+			logger.fatal(message, throwable);
+		}
+	}
+
+	/**
+	 * Log a message object with the INFO Level.
+	 * This method first checks if this category is INFO enabled by comparing the level
+	 * of this category with INFO Level. If the category is INFO enabled, then it converts
+	 * the message object passed as parameter to a string by invoking the appropriate ObjectRenderer.
+	 * It proceeds to call all the registered appenders
+	 * in this category and also higher in the hierarchy depending on the value of the additivity flag.
+	 * @param message the message object to log.
+	 */
+	public void info(Object message)
+	{
+		if (logger.isEnabledFor(Level.INFO))
+		{
+			logger.info(message);
+		}
+	}
+
+	/**
+	 * Log a message object with the INFO level including the stack trace of
+	 * the Throwable t passed as parameter.
+	 * @param message the message object to log.
+	 * @param throwable the exception to log, including its stack trace.
+	 */
+	public void info(Object message, Throwable throwable)
+	{
+		if (logger.isEnabledFor(Level.INFO))
+		{
+			logger.info(message, throwable);
+		}
+	}
+
+	/**
+	 * Log a message object with the WARN Level.
+	 * @param message the message object to log.
+	 */
+	public void warn(Object message)
+	{
+		if (logger.isEnabledFor(Level.WARN))
+		{
+			logger.warn(message);
+		}
+	}
+
+	/**
+	 * Log a message with the WARN level including the stack trace of the Throwable t passed as parameter.
+	 * @param message the message object to log.
+	 * @param throwable the exception to log, including its stack trace.
+	 */
+	public void warn(Object message, Throwable throwable)
+	{
+		if (logger.isEnabledFor(Level.WARN))
+		{
+			logger.warn(message, throwable);
+		}
+	}
+
+	/**
+	 * @deprecated Please now use getCommonLogger(Class className).
+	 * @see getCommonLogger(Class className)
+	 * @param className java class for which logger need to be created.
 	 * @return new logger object.
 	 */
 	public static org.apache.log4j.Logger getLogger(Class className)
 	{
-		org.apache.log4j.Logger logger;
-		if (!isConfigured )
-		{
-			out=org.apache.log4j.Logger.getLogger("");
-			out.warn
-			("Application specific logger configuration is not done. Please use Logger.configureLogger(path) before using getLogger()");
-			logger = out;
-		}
-		else
-		{
-			logger = org.apache.log4j.Logger.getLogger(className);
-		}
-		return logger;
+		return LoggerConfig.getConfiguredLogger(className);
 	}
 
-	/**
-	 * This method configure Logger.
-	 * @param propDirPath Path of directory containing properties file.
-	 */
-	public static void configureLogger(String propDirPath)
-	{
-		if(!isConfigured)
-		{
-			isConfigured = true;
-			StringBuffer log4jFilePath = new StringBuffer(propDirPath);
-			try
-			{
-				log4jFilePath.append(File.separator).append("log4j.properties");
-				PropertyConfigurator.configure(log4jFilePath.toString());
-				out = org.apache.log4j.Logger.getLogger(Logger.class);
-			}
-			catch (Exception malformedURLEx)
-			{
-				configure(Logger.class.getName());
-				out.fatal
-				("Logger not configured. Invalid config file " + log4jFilePath.toString());
-			}
-		}
-	}
-
-	/**
-	 * Configures the logger properties from the log4j.properties file.
-	 * The log4j.properties file should be present in the classpath for configuring.
-	 */
-	@Deprecated
-	public static void configure()
-	{
-		if (out == null)
-		{
-			out = org.apache.log4j.Logger.getLogger("Catissue");
-		}
-	}
-
-	/**
-	 * Logger class should not get instantiated from outside. Hence the constructor is private.
-	 */
-	private Logger()
-	{
-	}
-	/**
-	 * This method is used to configure logger with default values.
-	 * @param servletContext generic ServletContext object.
-	 */
-	public static void configDefaultLogger(ServletContext servletContext)
-	{
-		String applicationResourcesPath = servletContext.getRealPath("WEB-INF")
-				+ System.getProperty("file.separator") + "classes"
-				+ System.getProperty("file.separator")
-				+ servletContext.getInitParameter("applicationproperties");
-
-		configure(applicationResourcesPath);
-
-	}
 }
