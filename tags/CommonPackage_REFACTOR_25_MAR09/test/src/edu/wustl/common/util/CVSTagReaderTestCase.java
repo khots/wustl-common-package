0f55addbaee263c15c6b610921bee645fcec6025
ravi_kumar <ravi_kumar>
2009-03-25 16:08:34 +0000

    two change in this tag: 1-for logger and 2 in Utility.getForm... method.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2456 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ tags/CommonPackage_REFACTOR_25_MAR09/test/src/edu/wustl/common/util/CVSTagReaderTestCase.java
@@ -0,0 +1,58 @@
+/*
+ * Created on Jun 29, 2006
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+
+package edu.wustl.common.util;
+
+import edu.wustl.common.test.BaseTestCase;
+
+/**
+ * @author kapil_kaveeshwar
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class CVSTagReaderTestCase extends BaseTestCase
+{
+
+	/**
+	 * Constructer.
+	 */
+	public CVSTagReaderTestCase()
+	{
+		super("CVSTagReaderTestCase");
+	}
+
+	/**
+	 * Test the reading tag.
+	 */
+	public void testReadTagPositive()
+	{
+		CVSTagReader cvsTagReader = new CVSTagReader();
+		String tag = cvsTagReader.readTag("Testdata/ApplicationVersionInfo.txt");
+		assertEquals("Test_tag", tag);
+	}
+
+	/**
+	 * Test file not found read tag.
+	 */
+	public void testReadTagFileNotFound()
+	{
+		CVSTagReader cvsTagReader = new CVSTagReader();
+		String tag = cvsTagReader.readTag("ApplicationVersionInfo1.txt");
+		assertEquals(null, tag);
+	}
+
+	/**
+	 * Test reading of wrong pattern.
+	 */
+	public void testReadTagWrongPattern()
+	{
+		CVSTagReader cvsTagReader = new CVSTagReader();
+		String tag = cvsTagReader.readTag("Testdata/ApplicationVersionInfo2.txt");
+		assertEquals(null, tag);
+	}
+}
