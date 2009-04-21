13313db261ad93fe71730004a91934ad8e060bf6
ravi_kumar <ravi_kumar>
2009-04-21 08:38:07 +0000

    moved MYSQL_DUPL_KEY_MSGfrom DAO to CommonPackage
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2504 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/Constants.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/util/global/Constants.java
@@ -376,5 +376,12 @@ public final class Constants
 	 */
 	public static final String CONSTRAINT_VOILATION_ERROR =" Submission failed since a " +
 			"{0} with the same {1} already exists";
+	
+	/**
+	 *  The unique key error message is "Duplicate entry %s for key %d".
+	 *  This string is used for searching " for key " string in the above error message
+	 */
+	public static final String MYSQL_DUPL_KEY_MSG = " for key ";
+
 
 }
