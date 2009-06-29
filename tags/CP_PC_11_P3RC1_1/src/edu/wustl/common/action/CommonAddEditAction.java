60bd09f1f4411772b83f6e28effdcbba0c79d80f
preeti_lodha <preeti_lodha>
2009-06-29 06:26:36 +0000

    Tag applied on 29th June 2009. This was applied for pcatissue P3 RC1 build
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2584 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ tags/CP_PC_11_P3RC1_1/src/edu/wustl/common/action/CommonAddEditAction.java
@@ -0,0 +1,89 @@
+/**
+ * <p>Title: CommonAddEditAction Class>
+ * <p>Description:	This is common class used to Add/Edit the data in the database.</p>
+ * Copyright:    Copyright (c) year
+ * Company: Washington University, School of Medicine, St. Louis.
+ * @author Gautam Shetty
+ * @version 1.00
+ * Created on Apr 21, 2005
+ */
+
+package edu.wustl.common.action;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.struts.action.Action;
+import org.apache.struts.action.ActionError;
+import org.apache.struts.action.ActionErrors;
+import org.apache.struts.action.ActionForm;
+import org.apache.struts.action.ActionForward;
+import org.apache.struts.action.ActionMapping;
+
+import edu.wustl.common.actionForm.AbstractActionForm;
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.util.global.Constants;
+import edu.wustl.common.util.logger.Logger;
+
+/**
+ * This Class is used to Add/Edit data in the database.
+ * @author gautam_shetty
+ */
+public class CommonAddEditAction extends Action
+{
+
+	/**
+	 * LOGGER Logger - Generic LOGGER.
+	 */
+	private static final Logger LOGGER = Logger.getCommonLogger(CommonAddEditAction.class);
+
+	/**
+	 * Overrides the execute method of Action class.
+	 * Adds / Updates the data in the database.
+	 * @param mapping	ActionMapping
+	 * @param form	ActionForm
+	 * @param request	HttpServletRequest
+	 * @param response	HttpServletResponse
+	 * @return ActionForward
+	 * @throws IOException Generic exception
+	 * @throws ServletException Generic exception
+	 * */
+	public ActionForward execute(ActionMapping mapping, ActionForm form,
+			HttpServletRequest request, HttpServletResponse response) throws IOException,
+			ServletException
+	{
+
+		LOGGER.info("in execute method");
+		BaseAddEditAction addEditAction;
+		AbstractActionForm abstractForm = (AbstractActionForm) form;
+		ActionForward actionfwd;
+		try
+		{
+			if (abstractForm.isAddOperation())
+			{
+				addEditAction = new CommonAddAction();
+			}
+			else
+			{
+				addEditAction = new CommonEdtAction();
+
+			}
+			actionfwd = addEditAction.execute(mapping, abstractForm, request, response);
+		}
+		catch (ApplicationException applicationException)
+		{
+			LOGGER.error("Common Add/Edit failed.." + applicationException.getCustomizedMsg());
+					
+			ActionErrors actionErrors = new ActionErrors();
+			ActionError actionError = new ActionError("errors.item",applicationException.getCustomizedMsg());
+			actionErrors.add(ActionErrors.GLOBAL_ERROR, actionError);
+			saveErrors(request, actionErrors);
+
+			actionfwd = mapping.findForward(Constants.FAILURE);
+		}
+		return actionfwd;
+	}
+}
