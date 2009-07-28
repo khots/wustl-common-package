3225e188bbaa6cd6669f3187d6458f443865e433
nitesh_marwaha <nitesh_marwaha>
2009-07-28 13:22:35 +0000

    This class provides the exact implementation to insert the DataAuditEventLog.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2620 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ branches/Common_Package_v1.0.1_17JUNE/src/edu/wustl/common/audit/DataAuditManager.java
@@ -0,0 +1,30 @@
+package edu.wustl.common.audit;
+
+import java.util.Iterator;
+
+import edu.wustl.common.domain.AuditEventDetails;
+import edu.wustl.common.domain.DataAuditEventLog;
+import edu.wustl.dao.DAO;
+import edu.wustl.dao.exception.DAOException;
+
+/**
+ * This class provides the exact implementation to insert the DataAuditEventLog. 
+ * @author niharika_sharma
+ *
+ */
+public class DataAuditManager {
+
+	protected void insertAuditEventLogs(DAO dao,
+			DataAuditEventLog auditEventLog) throws DAOException {
+
+		Iterator<AuditEventDetails> auditEventDetailsIterator = auditEventLog
+				.getAuditEventDetailsCollcetion().iterator();
+		while (auditEventDetailsIterator.hasNext()) {
+			AuditEventDetails auditEventDetails = (AuditEventDetails) auditEventDetailsIterator
+					.next();
+			auditEventDetails.setAuditEventLog(auditEventLog);
+			dao.insert(auditEventDetails,false,"");
+		}
+	}
+
+}
