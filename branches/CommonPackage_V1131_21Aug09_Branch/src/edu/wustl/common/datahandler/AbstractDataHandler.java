b1fe60ebc86e8277f6230cc8bcd2d6eb0e5d5424
kalpana_thakur <kalpana_thakur>
2009-08-21 11:46:54 +0000

    New branch created for DE.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2655 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ branches/CommonPackage_V1131_21Aug09_Branch/src/edu/wustl/common/datahandler/AbstractDataHandler.java
@@ -0,0 +1,48 @@
+
+package edu.wustl.common.datahandler;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * 
+ * @author ravindra_jain
+ * @version 1.0
+ * @created 21-Apr-2009 6:57:50 PM
+ */
+public abstract class AbstractDataHandler
+{
+	protected String fileName;
+	
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public abstract void openFile() throws IOException;
+	
+	/**
+	 * 
+	 * @param values
+	 * @throws IOException 
+	 */
+	public abstract void appendData(List<Object> values) throws IOException;
+
+	/**
+	 * 
+	 * @param values
+	 */
+	public abstract void appendData(String values);
+
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public abstract void closeFile() throws IOException;
+	
+	/**
+	 * 
+	 * @throws IOException
+	 */
+	public abstract void flush() throws IOException;
+
+}
