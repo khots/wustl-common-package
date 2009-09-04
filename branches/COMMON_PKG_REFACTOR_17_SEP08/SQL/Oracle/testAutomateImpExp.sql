86b07e6395b96ee4ec3ac333c56375d7e6dbe6cc
ravi_kumar <ravi_kumar>
2009-09-04 15:58:36 +0000

    added sql script createUser.sql to setup database for  junit testcases
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2660 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/SQL/Oracle/testAutomateImpExp.sql
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/SQL/Oracle/testAutomateImpExp.sql
@@ -1,6 +1,6 @@
 #This file has been used for junit testing of AutomateImport for oracle databse.
-drop table CATISSUE_PERMISSIBLE_VALUE;
-drop table CATISSUE_CDE;
+#drop table CATISSUE_PERMISSIBLE_VALUE;
+#drop table CATISSUE_CDE;
 create table CATISSUE_PERMISSIBLE_VALUE (IDENTIFIER number(19,0) not null ,CONCEPT_CODE varchar(40),DEFINITION varchar2(500),PARENT_IDENTIFIER number(19,0),VALUE varchar(225),PUBLIC_ID varchar(30),primary key (IDENTIFIER));
 create table CATISSUE_CDE (PUBLIC_ID varchar(30) not null,LONG_NAME varchar(200),DEFINITION varchar2(500),VERSION varchar(50),LAST_UPDATED date,primary key (PUBLIC_ID));
 alter table CATISSUE_PERMISSIBLE_VALUE  add constraint FK57DDCE153B5435E foreign key (PARENT_IDENTIFIER) references CATISSUE_PERMISSIBLE_VALUE;
