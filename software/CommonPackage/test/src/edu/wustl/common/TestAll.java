cf654a5b4400ec2b7aeeecc4978ba73fa2fd9ee4
nightlybuild <nightlybuild>
2010-01-27 12:17:34 +0000

    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/CommonPackage_BDA_v118_27Jan10@2782 38c78225-d16a-144f-a689-2cd41fac0795

--- software/CommonPackage/test/src/edu/wustl/common/TestAll.java
+++ software/CommonPackage/test/src/edu/wustl/common/TestAll.java
@@ -4,21 +4,23 @@ package edu.wustl.common;
 /**
  * Main class to run junit test cases.
  */
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import edu.wustl.common.audit.AuditManagerTestCase;
+import edu.wustl.common.audit.AuditTestCase;
 import edu.wustl.common.bizlogic.AbstractBizLogicTestCase;
 import edu.wustl.common.bizlogic.DefaultBizLogicTestCase;
 import edu.wustl.common.datatypes.DataTypeTestCase;
 import edu.wustl.common.factory.AbstractFactoryConfigTestCase;
-import edu.wustl.common.jobmanager.TestJobManager;
 import edu.wustl.common.util.ExportReportTestCase;
 import edu.wustl.common.util.XMLPropertyHandlerTestCase;
 import edu.wustl.common.util.global.ApplicationPropertiesTestCase;
 import edu.wustl.common.util.global.CommonServiceLocatorTestCase;
 import edu.wustl.common.util.global.HibernatePropertiesTestCase;
+import edu.wustl.common.util.global.PasswordEncrypterTestCase;
 import edu.wustl.common.util.global.PasswordManagerTestCase;
+import edu.wustl.common.util.global.SendEmailTestCase;
 import edu.wustl.common.util.global.StatusTestCase;
 import edu.wustl.common.util.global.ValidatorTestCase;
 import edu.wustl.common.util.global.XMLParserUtilityTestCase;
@@ -53,18 +55,18 @@ public class TestAll extends TestCase
 		suite.addTestSuite(DataTypeTestCase.class);
 		suite.addTestSuite(AbstractFactoryConfigTestCase.class);
 		suite.addTestSuite(CommonServiceLocatorTestCase.class);
-		//suite.addTestSuite(AuditManagerTestCase.class);
+		suite.addTestSuite(AuditManagerTestCase.class);
 		suite.addTestSuite(XMLPropertyHandlerTestCase.class);
 		suite.addTestSuite(AutomateImportTest.class);
 		suite.addTestSuite(ExportReportTestCase.class);
 		suite.addTestSuite(ApplicationPropertiesTestCase.class);
-		//suite.addTestSuite(SendEmailTestCase.class);
+		suite.addTestSuite(SendEmailTestCase.class);
 		suite.addTestSuite(HibernatePropertiesTestCase.class);
 		suite.addTestSuite(XMLParserUtilityTestCase.class);
 		suite.addTestSuite(PasswordManagerTestCase.class);
-		//suite.addTestSuite(PasswordEncrypterTestCase.class);
+		suite.addTestSuite(PasswordEncrypterTestCase.class);
 		suite.addTestSuite(ValidatorTestCase.class);
-		suite.addTestSuite(TestJobManager.class);
+		suite.addTestSuite(AuditTestCase.class);
 		return suite;
 	}
 }
