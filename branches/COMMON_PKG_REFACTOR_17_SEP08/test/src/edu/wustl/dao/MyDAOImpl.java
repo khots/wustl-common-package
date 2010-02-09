aee259f1b8955abe5b88ee61a64a0fda396ce8f0
kalpana_thakur <kalpana_thakur>
2010-02-09 06:24:00 +0000

    latest DAO 1.9.1 jar
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2807 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/dao/MyDAOImpl.java
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/test/src/edu/wustl/dao/MyDAOImpl.java
@@ -937,4 +937,14 @@ public class MyDAOImpl implements DAO, HibernateDAO, IDAOFactory, JDBCDAO, IConn
 		// TODO Auto-generated method stub
 		return null;
 	}
+	public List executeQuery(String arg0, List<ColumnValueBean> arg1)
+			throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	public List executeParamHQL(String arg0, List<ColumnValueBean> arg1)
+			throws DAOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
