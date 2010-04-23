0242d2bd85d202cbe0181375dcb66b0866cd1344
deepali_ahirrao <deepali_ahirrao>
2010-04-23 14:23:44 +0000

    Added Unique ID generation code. Added test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/COMMON_PKG_REFACTOR_17_SEP08@2847 38c78225-d16a-144f-a689-2cd41fac0795

--- test/src/edu/wustl/common/TestAll.java
+++ test/src/edu/wustl/common/TestAll.java
@@ -12,6 +12,7 @@ import edu.wustl.common.bizlogic.AbstractBizLogicTestCase;
 import edu.wustl.common.bizlogic.DefaultBizLogicTestCase;
 import edu.wustl.common.datatypes.DataTypeTestCase;
 import edu.wustl.common.factory.AbstractFactoryConfigTestCase;
+import edu.wustl.common.idgenerator.KeySequenceGeneratorTestCases;
 import edu.wustl.common.jobmanager.TestJobManager;
 import edu.wustl.common.util.ExportReportTestCase;
 import edu.wustl.common.util.XMLPropertyHandlerTestCase;
@@ -65,6 +66,7 @@ public class TestAll extends TestCase
 		//suite.addTestSuite(PasswordEncrypterTestCase.class);
 		suite.addTestSuite(ValidatorTestCase.class);
 		suite.addTestSuite(TestJobManager.class);
+		suite.addTestSuite(KeySequenceGeneratorTestCases.class);
 		return suite;
 	}
 }
