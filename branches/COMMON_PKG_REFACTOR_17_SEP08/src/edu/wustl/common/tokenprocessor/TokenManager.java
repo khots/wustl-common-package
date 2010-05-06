b786775ea3259a5280d9f4da393d76dce2b741b0
suhas_khot <suhas_khot>
2010-05-06 10:50:34 +0000

    Changes made for Label value generator
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2856 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ branches/COMMON_PKG_REFACTOR_17_SEP08/src/edu/wustl/common/tokenprocessor/TokenManager.java
@@ -0,0 +1,73 @@
+/**
+ *
+ */
+
+package edu.wustl.common.tokenprocessor;
+
+import edu.wustl.common.exception.ApplicationException;
+import edu.wustl.common.util.global.Constants;
+
+/**
+ * @author suhas_khot
+ *
+ */
+public class TokenManager
+{
+
+	/**
+	 * Given a object and format returns value.
+	 * @param object
+	 * @param format
+	 * @throws ApplicationException
+	 */
+	public static String getLabelValue(final Object object, final String format)
+			throws ApplicationException
+	{
+		StringBuffer tokenLabel = new StringBuffer();
+		parseFormat(object, format, tokenLabel);
+		return tokenLabel.toString();
+	}
+
+	/**
+	 * Parse format and generate value
+	 * @param object
+	 * @param format
+	 * @param tokenValue
+	 * @param delimiter
+	 * @param fileName
+	 * @throws ApplicationException
+	 */
+	public static void parseFormat(final Object object, final String format,
+			final StringBuffer tokenValue) throws ApplicationException
+	{
+
+		final int tokenDelimiterIndex = format.indexOf(Constants.TOKEN_DELIMITER);
+		if (tokenDelimiterIndex > 0)
+		{
+			tokenValue.append(format.substring(0, tokenDelimiterIndex));
+			parseFormat(object, format.substring(tokenDelimiterIndex), tokenValue);
+		}
+		else if (tokenDelimiterIndex == 0)
+		{
+			String token;
+			try
+			{
+				token = format.substring(tokenDelimiterIndex + 1, format.indexOf(
+						Constants.TOKEN_DELIMITER, tokenDelimiterIndex + 1));
+			}
+			catch (StringIndexOutOfBoundsException e)
+			{
+				throw new ApplicationException(null, e, e.getMessage());
+			}
+			final String tokenLabel = TokenFactory.getInstance(token).getTokenValue(object);
+			tokenValue.append(tokenLabel);
+			final int nextDelimiterIndex = token.length() + 2;
+			parseFormat(object, format.substring(nextDelimiterIndex), tokenValue);
+		}
+		else if (tokenDelimiterIndex == -1)
+		{
+			tokenValue.append(format);
+		}
+	}
+
+}
