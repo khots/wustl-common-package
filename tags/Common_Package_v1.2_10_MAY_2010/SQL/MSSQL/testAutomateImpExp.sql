b31abe9464c68e5fb46bdc5cf6c29446ff0b847c
nitesh_marwaha <nitesh_marwaha>
2010-05-10 06:56:26 +0000

    creating tag for release CP_V1.2, whihc have new feature for token manager, see Bug #17016 - Moving TokenManager code from ClinPortal to CommonPackage
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@2861 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ tags/Common_Package_v1.2_10_MAY_2010/SQL/MSSQL/testAutomateImpExp.sql
@@ -0,0 +1,22 @@
+#This file has been used for junit testing of AutomateImport for mssql server databse.
+IF EXISTS(SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'CATISSUE_PERMISSIBLE_VALUE') DROP TABLE CATISSUE_PERMISSIBLE_VALUE;
+IF EXISTS(SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = 'CATISSUE_CDE') DROP TABLE CATISSUE_CDE;
+create table CATISSUE_PERMISSIBLE_VALUE (
+   IDENTIFIER bigint not null identity,
+   CONCEPT_CODE varchar(40),
+   DEFINITION varchar(500),
+   PARENT_IDENTIFIER bigint,
+   VALUE varchar(225),
+   PUBLIC_ID varchar(30),
+   primary key (IDENTIFIER)
+);
+create table CATISSUE_CDE (
+   PUBLIC_ID varchar(30) not null,
+   LONG_NAME varchar(200),
+   DEFINITION varchar(500),
+   VERSION varchar(50),
+   LAST_UPDATED datetime,
+   primary key (PUBLIC_ID)
+);
+alter table CATISSUE_PERMISSIBLE_VALUE  add constraint FK57DDCE153B5435E foreign key (PARENT_IDENTIFIER) references CATISSUE_PERMISSIBLE_VALUE  ;
+alter table CATISSUE_PERMISSIBLE_VALUE  add constraint FK57DDCE1FC56C2B1 foreign key (PUBLIC_ID) references CATISSUE_CDE ;
