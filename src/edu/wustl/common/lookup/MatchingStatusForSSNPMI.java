9a56758321d5b17cf88e4db05d91c780610ec5b2
akaushal <akaushal>
2011-04-21 13:45:16 +0000

    Changes made for caTIES.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/CP_Branch_For_KeySequenceGen_26_APR_2010@2906 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/lookup/MatchingStatusForSSNPMI.java
+++ src/edu/wustl/common/lookup/MatchingStatusForSSNPMI.java
@@ -7,7 +7,31 @@ package edu.wustl.common.lookup;
 public enum MatchingStatusForSSNPMI
 {
 	/**
-	 * MatchingStatusForSSNPMI enum values.
+	 * MatchingStatusForSSNPMI enum valueues.
 	 */
-	EXACT, ONEMATCHOTHERNULL,ONEMATCHOTHERMISMATCH, NOMATCH;
+	EXACT("EXACT"), ONEMATCHOTHERNULL("ONEMATCHOTHERNULL"),ONEMATCHOTHERMISMATCH("ONEMATCHOTHERMISMATCH"), NOMATCH("NOMATCH");
+
+	private String value;
+
+	MatchingStatusForSSNPMI(String value) {
+		this.value = value;
+	}
+
+	public String toString() {
+		return value;
+	}
+
+	public static MatchingStatusForSSNPMI toMatchingStatusForSSNPMI(String value) {
+		MatchingStatusForSSNPMI result = null;
+		if(EXACT.toString().equals(value)){
+			result = EXACT;
+		}else if(ONEMATCHOTHERNULL.toString().equals(value)){
+			result = ONEMATCHOTHERNULL;
+		}else if(ONEMATCHOTHERMISMATCH.toString().equals(value)){
+			result = ONEMATCHOTHERMISMATCH;
+		}else if(NOMATCH.toString().equals(value)){
+			result = NOMATCH;
+		}
+		return result;
+	}
 }
