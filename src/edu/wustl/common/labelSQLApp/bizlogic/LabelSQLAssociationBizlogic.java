3548deb4068c8208583e6b67958ed5a3c0f69944
amujawar <amujawar>
2012-05-10 10:14:57 +0000

    Checking in code for study dashboard.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/CommonPackage_v128_cp2.2_Branch@2943 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/labelSQLApp/bizlogic/LabelSQLAssociationBizlogic.java
@@ -0,0 +1,185 @@
+
+package edu.wustl.labelSQLApp.bizlogic;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import edu.wustl.labelSQLApp.domain.LabelSQL;
+import edu.wustl.labelSQLApp.domain.LabelSQLAssociation;
+
+import org.hibernate.Session;
+
+import edu.wustl.common.hibernate.HibernateDatabaseOperations;
+import edu.wustl.common.hibernate.HibernateUtil;
+
+public class LabelSQLAssociationBizlogic
+{
+
+	/**
+	 * Gives the LabelSQLAsoociation from the LabelSQLAssociation id
+	 * @param labelSQLAssocId
+	 * @return
+	 * @throws Exception
+	 */
+	public LabelSQLAssociation getLabelSQLAssocById(Long labelSQLAssocId) throws Exception
+	{
+		LabelSQLAssociation labelSQLAssociation = null;
+
+		Session session = HibernateUtil.newSession();
+		try
+		{
+
+			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
+					session);
+			labelSQLAssociation = dbHandler.retrieveById(LabelSQLAssociation.class.getName(),
+					labelSQLAssocId);
+		}
+		finally
+		{
+			session.close();
+		}
+
+		return labelSQLAssociation;
+	}
+
+	/**
+	 * Get all the LabelSQL associations for a CP/CS 
+	 * @param CPId
+	 * @return
+	 * @throws Exception
+	 */
+	public List<LabelSQLAssociation> getLabelSQLAssocCollectionByCPId(Long CPId) throws Exception
+	{
+		List<Object> values = new ArrayList<Object>();
+		values.add(CPId);
+
+		List<?> result = CommonBizlogic.executeHQL("getLabelSQLAssocByCPId", values);//runs the HQL
+
+		return (List<LabelSQLAssociation>) result;
+	}
+
+	/**
+	 * inserting record into LabelSQLAssociation
+	 * @param cpId
+	 * @param labelSQLId
+	 * @param userDefinedLabel
+	 * @param order
+	 * @throws Exception
+	 */
+	public void insertLabelSQLAssociation(Long cpId, Long labelSQLId, String userDefinedLabel,
+			int order) throws Exception
+	{
+		LabelSQLAssociation labelSQLAssociation = new LabelSQLAssociation();
+		labelSQLAssociation.setLabelSQL(new LabelSQLBizlogic().getLabelSQLById(labelSQLId));
+		labelSQLAssociation.setLabelSQLCollectionProtocol(cpId);
+		labelSQLAssociation.setUserDefinedLabel(userDefinedLabel);
+		labelSQLAssociation.setSeqOrder(order);
+
+		Session session = HibernateUtil.newSession();
+		try
+		{
+			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
+					session);
+			dbHandler.insert(labelSQLAssociation);
+		}
+		finally
+		{
+			session.close();
+		}
+
+	}
+
+	/**
+	 * deleting record from LabelSQLAssociation
+	 * @param cpId
+	 * @param labelSQLId
+	 * @throws Exception
+	 */
+	public void deleteLabelSQLAssociation(Long cpId, Long labelSQLId) throws Exception
+	{
+		LabelSQLAssociation labelSQLAssociation = new LabelSQLAssociation();
+		labelSQLAssociation = getLabelSQLAssocByCPIdAndLabelSQLId(cpId, labelSQLId);//get the labelSQLAssociation from cpId and labelSQLId
+
+		Session session = HibernateUtil.newSession();
+		try
+		{
+			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
+					session);
+
+			dbHandler.delete(labelSQLAssociation);
+		}
+		finally
+		{
+			session.close();
+		}
+
+	}
+
+	/**
+	 * Get display name and association for a CP/CS 
+	 * @param CPId
+	 * @return
+	 * @throws Exception
+	 */
+	public LinkedHashMap<String, Long> getAssocAndDisplayNameMapByCPId(Long CPId) throws Exception
+	{
+		LinkedHashMap<String, Long> displayNameAssocMap = new LinkedHashMap<String, Long>();
+
+		//Retrieving LabelSQLAssociation list by CPId.  
+		List<LabelSQLAssociation> labelSQLAssociations = getLabelSQLAssocCollectionByCPId(CPId);
+
+		for (LabelSQLAssociation labelSQLAssociation : labelSQLAssociations)
+		{
+			String query = labelSQLAssociation.getLabelSQL().getQuery();//retrieving query by LabelSQLAssociation
+			String displayName = new CommonBizlogic().getLabelByLabelSQLAssocId(labelSQLAssociation
+					.getId());//retrieve label by association id
+
+			if (query != null && !"".equals(query))
+			{ //Associating the displayname name and association id for dashboard items
+				if (!"".equals(displayName) && displayName != null)
+				{
+					displayNameAssocMap.put(displayName, labelSQLAssociation.getId());
+				}
+			}
+			else
+			{
+				//For group heading map with display name and dummy association
+				displayNameAssocMap.put(displayName, new Long(0));
+			}
+
+		}
+		return displayNameAssocMap;
+	}
+
+	/**
+	 * Returns the LabelSQLAssociation based on CPId and LabelSQLId
+	 * @param cpId
+	 * @param labelSQLId
+	 * @return
+	 * @throws Exception
+	 */
+	public LabelSQLAssociation getLabelSQLAssocByCPIdAndLabelSQLId(Long cpId, Long labelSQLId)
+			throws Exception
+	{
+		List<Object> values = new ArrayList<Object>();
+		values.add(cpId);
+		values.add(labelSQLId);
+
+		List<?> result = CommonBizlogic.executeHQL("getLabelSQLAssocByCPIdAndLabelSQLId", values);
+
+		if (result.size() != 0)
+		{
+			return (LabelSQLAssociation) result.get(0);
+		}
+		else
+		{
+			return null;
+		}
+
+	}
+
+}
---
+++ src/edu/wustl/common/labelSQLApp/bizlogic/LabelSQLBizlogic.java
@@ -0,0 +1,150 @@
+
+package edu.wustl.labelSQLApp.bizlogic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.wustl.labelSQLApp.domain.LabelSQL;
+
+import org.hibernate.Session;
+
+import edu.wustl.common.hibernate.HibernateDatabaseOperations;
+import edu.wustl.common.hibernate.HibernateUtil;
+
+public class LabelSQLBizlogic
+{
+
+	/**
+	 * Gives the LabelSQL from the LabelSQL id
+	 * @param labelSQLId
+	 * @return
+	 * @throws Exception
+	 */
+	public LabelSQL getLabelSQLById(Long labelSQLId) throws Exception
+	{
+		LabelSQL labelSQL = null;
+
+		Session session = HibernateUtil.newSession();
+		try
+		{
+			Long id = labelSQLId;
+			HibernateDatabaseOperations<LabelSQL> dbHandler = new HibernateDatabaseOperations<LabelSQL>(
+					session);
+			labelSQL = dbHandler.retrieveById(LabelSQL.class.getName(), id);
+		}
+		finally
+		{
+			session.close();
+		}
+
+		return labelSQL;
+	}
+
+	/**
+	 * Retrieves the LabelSQL on the basis of label
+	 * @param label
+	 * @return
+	 * @throws Exception
+	 */
+	public List<LabelSQL> getLabelSQLByLabel(String label) throws Exception
+	{
+		List<LabelSQL> labelSQLList = null;
+		Session session = HibernateUtil.newSession();
+		try
+		{
+			HibernateDatabaseOperations<LabelSQL> dbHandler = new HibernateDatabaseOperations<LabelSQL>(
+					session);
+			labelSQLList = dbHandler.retrieve(LabelSQL.class.getName(), "label", label);
+		}
+		finally
+		{
+			session.close();
+		}
+
+		return labelSQLList;
+	}
+
+	/**
+	 * Insert LabelSQL
+	 * @param label
+	 * @param sql
+	 * @return
+	 * @throws Exception
+	 */
+	public long insertLabelSQL(String label, String sql) throws Exception
+	{
+		LabelSQL labelSQL = new LabelSQL();
+		labelSQL.setLabel(label);
+		labelSQL.setQuery(sql);
+
+		Session session = HibernateUtil.newSession();
+		try
+		{
+			HibernateDatabaseOperations<LabelSQL> dbHandler = new HibernateDatabaseOperations<LabelSQL>(
+					session);
+
+			dbHandler.insert(labelSQL);
+
+			session.flush();
+			return labelSQL.getId();
+		}
+		finally
+		{
+			session.close();
+		}
+
+	}
+
+	/**
+	 * Gives all LabelSQL
+	 * @param labelSQLId
+	 * @return
+	 * @throws Exception
+	 */
+	public List<LabelSQL> getAllLabelSQL() throws Exception
+	{
+		List<LabelSQL> labelSQLs = null;
+
+		Session session = HibernateUtil.newSession();
+		try
+		{
+
+			HibernateDatabaseOperations<LabelSQL> dbHandler = new HibernateDatabaseOperations<LabelSQL>(
+					session);
+			labelSQLs = dbHandler.retrieve(LabelSQL.class.getName());
+		}
+		finally
+		{
+			session.close();
+		}
+
+		return labelSQLs;
+	}
+
+	/**
+	 * Returns the LabelSQL based on label or display name.
+	 * @param labelSQLId
+	 * @return
+	 * @throws Exception
+	 */
+	public Long getLabelSQLIdByLabelOrDisplayName(Long CPId,String label) throws Exception
+	{
+		List<Object> values = new ArrayList<Object>();
+		values.add(CPId);
+		values.add(label);
+		values.add(label);
+
+		List<?> result = CommonBizlogic.executeHQL("getLabelSQLIdByLabelOrDisplayName", values);
+
+		if (result.size() != 0)
+		{
+			return Long.parseLong(result.get(0).toString());
+		}
+		else
+		{
+			return null;
+		}
+		
+	}
+
+}
