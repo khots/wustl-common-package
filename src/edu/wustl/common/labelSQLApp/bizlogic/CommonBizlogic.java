41b9e0eb71623f6a633e710aaecee27e285f5e14
lales <lales>
2012-05-23 13:41:02 +0000

    merge from cp 2.5
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/commonpackage_cp2.2_for_rpr@2976 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ src/edu/wustl/common/labelSQLApp/bizlogic/CommonBizlogic.java
@@ -0,0 +1,229 @@
+
+package edu.wustl.common.labelSQLApp.bizlogic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.sql.Clob;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import edu.wustl.common.hibernate.HibernateUtil;
+import edu.wustl.common.labelSQLApp.domain.LabelSQLAssociation;
+import edu.wustl.common.util.logger.Logger;
+
+public class CommonBizlogic
+{
+
+	/**
+	 * This method executes the HQL in LabelSQL_HQL.hbm.xml
+	 * @param queryName
+	 * @param values
+	 * @return
+	 * @throws HibernateException
+	 */
+	public static List<?> executeHQL(String queryName, List<Object> values)
+			throws HibernateException
+	{
+		System.out.println("creating session executeHQL...");
+		Session session = HibernateUtil.newSession();
+		System.out.println("creating session executeHQL done...");
+
+		List<?> records = null;
+		try
+		{
+			Query q = session.getNamedQuery(queryName);
+			if (values != null)
+			{
+				for (int counter = 0; counter < values.size(); counter++)
+				{
+					Object value = values.get(counter);
+					String objectType = value.getClass().getSimpleName();
+
+					if ("Long".equals(objectType))
+					{
+						//sets the value for parameter of Long type
+						q.setLong(counter, Long.parseLong(value.toString()));
+					}
+					else if ("String".equals(objectType))
+					{
+						//sets the value for parameter of String type
+						q.setString(counter, (String) value);
+					}
+					else
+					{
+						q.setEntity(counter, value);
+					}
+				}
+			}
+			System.out.println("executing HQL...");
+			records = q.list();
+			System.out.println("executing HQL done...");
+		}
+		finally
+		{
+			session.close();
+		}
+
+		return records;
+
+	}
+
+	/**
+	 * Retrieves the label for dashboard item can be predefined label or display name
+	 * @param labelSQLAssocId
+	 * @return
+	 * @throws Exception
+	 */
+	public String getLabelByLabelSQLAssocId(Long labelSQLAssocId) throws Exception
+	{
+
+		List<Object> values = new ArrayList<Object>();
+		values.add(labelSQLAssocId);
+		values.add(labelSQLAssocId);
+
+		List<?> result = executeHQL("getLabelByLabelSQLAssocId", values);
+
+		if (result.size() != 0)
+		{
+			return (String) result.get(0);
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+	/**
+	 * Returns the query associated with label id
+	 * @param labelSQLId
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Clob> getQueryByLabelSQLAssocId(Long labelSQLId) throws Exception
+	{
+		List<Object> values = new ArrayList<Object>();
+		values.add(labelSQLId);
+
+		List<?> result = executeHQL("getQueryByLabelSQLAssocId", values);
+
+		return (List<Clob>) result;
+	}
+
+	/**
+	 * Returns map with the label/display name and query count
+	 * @param CPId
+	 * @return
+	 * @throws Exception
+	 */
+	public LinkedHashMap<String, Integer> getLabelQueryResultMapByCPId(Long CPId) throws Exception
+	{
+		LinkedHashMap<String, Integer> labelQueryResultMap = new LinkedHashMap<String, Integer>();
+		LabelSQLAssociationBizlogic labelSQLAssociationBizlogic = new LabelSQLAssociationBizlogic();
+
+		/*Retrieve all the LabelSQL association associated with the CP/CS*/
+		List<LabelSQLAssociation> labelSQLAssociations = labelSQLAssociationBizlogic
+				.getLabelSQLAssocCollection(CPId);
+
+		for (LabelSQLAssociation labelSQLAssociation : labelSQLAssociations)
+		{
+
+			Long labelAssocId = labelSQLAssociation.getId();
+
+			/*insert label and query count in map*/
+			String label = getLabelByLabelSQLAssocId(labelAssocId);
+			int result = executeQuery(
+					clobToStringConversion((getQueryByLabelSQLAssocId(labelAssocId)).get(0)), CPId);
+
+			if (!"".equals(label) && label != null)
+			{
+				labelQueryResultMap.put(label, result);
+			}
+
+		}
+
+		return labelQueryResultMap;
+	}
+
+	/**
+	 * Returns String with labelSQLAsociation and query result
+	 * @param labelSQLAssocID
+	 * @return
+	 * @throws Exception
+	 */
+	public String getQueryResultByLabelSQLAssocId(Long labelSQLAssocID) throws Exception
+	{
+		/*Retrieve all the LabelSQL association associated with the CP/CS*/
+		LabelSQLAssociation labelSQLAssociation = new LabelSQLAssociationBizlogic()
+				.getLabelSQLAssocById(labelSQLAssocID);
+
+		int result = executeQuery(
+				clobToStringConversion((getQueryByLabelSQLAssocId(labelSQLAssociation.getId()))
+						.get(0)), labelSQLAssociation.getLabelSQLCollectionProtocol());//retrieve the query from labelSQLAssocID and execute it
+		String resultString = labelSQLAssocID.toString() + "," + result;
+		return resultString;
+	}
+
+	/**
+	* Executes the stored SQL to give the count
+	* @param sql
+	* @param CPId
+	* @return
+	* @throws Exception
+	*/
+	public int executeQuery(String sql, Long CPId) throws Exception
+	{
+		Session session = HibernateUtil.newSession();
+		if (sql.endsWith(";"))
+		{
+			sql = sql.substring(0, sql.length() - 1);
+		}
+		PreparedStatement stmt = session.connection().prepareStatement(sql);
+		ResultSet rs = null;
+		try
+		{
+			if (CPId != null)
+			{
+				stmt.setLong(1, CPId);
+			}
+			rs = stmt.executeQuery();
+			rs.next();
+			return rs.getInt(1);
+		}
+		catch (Exception e)
+		{
+			Logger.out.error("Error executing query -> " + sql);
+			e.printStackTrace();
+			return -1;
+		}
+		finally
+		{
+			session.close();
+
+		}
+	}
+
+	private static String clobToStringConversion(Clob clb) throws IOException, SQLException
+	{
+		if (clb == null)
+			return "";
+
+		StringBuffer str = new StringBuffer();
+		String strng;
+
+		BufferedReader bufferRead = new BufferedReader(clb.getCharacterStream());
+
+		while ((strng = bufferRead.readLine()) != null)
+			str.append(strng);
+
+		return str.toString();
+	}
+
+}
---
+++ src/edu/wustl/common/scheduler/scheduleProcessor/FilesCleanupProcessor.java
@@ -0,0 +1,62 @@
+
+package edu.wustl.common.scheduler.scheduleProcessor;
+
+
+import edu.wustl.common.scheduler.constants.SchedulerConstants;
+import edu.wustl.common.scheduler.domain.Schedule;
+import edu.wustl.common.scheduler.propertiesHandler.SchedulerConfigurationPropertiesHandler;
+import edu.wustl.common.scheduler.util.SchedulerDataUtility;
+import edu.wustl.common.util.logger.Logger;
+
+
+public class FilesCleanupProcessor extends AbstractScheduleProcessor
+{
+
+	private static final Logger log = Logger.getCommonLogger(FilesCleanupProcessor.class);
+
+	/**
+	 * 
+	 */
+	public FilesCleanupProcessor()
+	{
+		super();
+		Schedule cleanUpSchedule = new Schedule();
+		cleanUpSchedule.setId(Long.MAX_VALUE);
+		cleanUpSchedule.setName("CleanUpSchedule");
+		cleanUpSchedule.setInterval(SchedulerConstants.DAILY_INTERVAL);
+		this.scheduleObject = cleanUpSchedule;
+	}
+
+	/* (non-Javadoc)
+	 * @see main.java.scheduler.scheduleProcessors.AbstractScheduleProcessor#executeSchedule()
+	 */
+	protected void executeSchedule() throws Exception
+	{
+		Long threshold = Long.valueOf((String) SchedulerConfigurationPropertiesHandler
+				.getInstance().getProperty(SchedulerConstants.CLEANUP_THRESHOLD));
+
+		SchedulerDataUtility.cleanOldFilesFromDir(threshold);
+	}
+
+	@Override
+	protected void postScheduleExecution() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void mail() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void generateTicket() throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+}
