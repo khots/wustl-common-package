841e81ab95ccc3558387bbf3877b38182becb38f
amujawar <amujawar>
2012-06-14 11:37:37 +0000

    Old test cases.
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/CommonPackage_v128_cp2.2_Branch@2993 38c78225-d16a-144f-a689-2cd41fac0795

---
+++ test/oldTestCases/src/edu/wustl/common/jobmanager/TestJobManager.java
@@ -0,0 +1,36 @@
+package edu.wustl.common.jobmanager;
+
+import edu.wustl.common.CommonBaseTestCase;
+import edu.wustl.common.jobmanager.JobManager;
+
+
+public class TestJobManager extends CommonBaseTestCase
+{
+
+	public void testJobManager()
+	{
+		JobStatusListener jobStatusListener =new DefaultJobStatusListner();
+		JobManager jobmanager=JobManager.getInstance();
+		jobmanager.start();
+
+
+		DummyJob dummyJob=new DummyJob("test", "1", jobStatusListener);
+		JobData jobData=new JobData("test", "1", jobStatusListener);
+		jobmanager.addJob(dummyJob);
+		System.out.println("successfull");
+//		try
+//		{
+////			Thread.sleep(4000);
+//		}
+//		catch (InterruptedException e)
+//		{
+//			e.printStackTrace();
+//		}
+		Object[] keys= new Object[]{"LOG_FILE_KEY","NO_OF_RECORDS_PROCESSED","NO_OF_FAILED_RECORDS","NO_OF_TOTAL_RECORDS","JOB_STATUS_KEY","TIME_TAKEN_KEY",};
+		Object[] values=new Object[]{"abc.zip","3","4","45","InProgress","231"};
+
+//		jobStatusListener.jobStatusUpdated(jobData);
+		jobData.updateJobStatus(keys, values, "Completed");
+		jobmanager.shutdownJobManager();
+	}
+}
