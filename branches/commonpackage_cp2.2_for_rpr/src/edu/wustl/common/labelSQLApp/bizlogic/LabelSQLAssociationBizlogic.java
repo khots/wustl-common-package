9f6449c44534cd02e015cbaecb9fe719de501f5e
ganachas <ganachas>
2012-06-29 06:09:57 +0000

    code refactored
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@3024 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/commonpackage_cp2.2_for_rpr/src/edu/wustl/common/labelSQLApp/bizlogic/LabelSQLAssociationBizlogic.java
+++ branches/commonpackage_cp2.2_for_rpr/src/edu/wustl/common/labelSQLApp/bizlogic/LabelSQLAssociationBizlogic.java
@@ -2,15 +2,16 @@
 package edu.wustl.common.labelSQLApp.bizlogic;
 
 import java.util.ArrayList;
-
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
-import edu.wustl.common.labelSQLApp.domain.LabelSQLAssociation;
-import org.hibernate.Session;
-import edu.wustl.common.hibernate.HibernateDatabaseOperations;
-import edu.wustl.common.hibernate.HibernateUtil;
+import java.util.Map;
 
-public class LabelSQLAssociationBizlogic
+import edu.wustl.common.labelSQLApp.domain.LabelSQLAssociation;
+import edu.wustl.dao.query.generator.DBTypes;
+import edu.wustl.dao.util.NamedQueryParam;
+
+public class LabelSQLAssociationBizlogic extends CommonBizlogic
 {
 
 	/**
@@ -24,22 +25,23 @@ public class LabelSQLAssociationBizlogic
 		LabelSQLAssociation labelSQLAssociation = null;
 
 		System.out.println("create session getLabelSQLAssocById...");
-		Session session = HibernateUtil.newSession();
+		//Session session = HibernateUtil.newSession();
 		System.out.println("create session getLabelSQLAssocById done...");
 		try
 		{
 
-			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
-					session);
+			labelSQLAssociation =(LabelSQLAssociation) retrieve(LabelSQLAssociation.class.getName(),labelSQLAssocId);
+//			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
+//					session);
 			System.out.println("retrieveById getLabelSQLAssocById...");
-			labelSQLAssociation = dbHandler.retrieveById(LabelSQLAssociation.class.getName(),
-					labelSQLAssocId);
+//			labelSQLAssociation = dbHandler.retrieveById(LabelSQLAssociation.class.getName(),
+//					labelSQLAssocId);
 			System.out.println("retrieveById getLabelSQLAssocById done...");
 		}
 		finally
 		{
 			System.out.println("close session getLabelSQLAssocById...");
-			session.close();
+//			session.close();
 			System.out.println("close session getLabelSQLAssocById done...");
 		}
 
@@ -55,16 +57,19 @@ public class LabelSQLAssociationBizlogic
 	public List<LabelSQLAssociation> getLabelSQLAssocCollection(Long CPId) throws Exception
 	{
 		List<?> result = null;
-		if (CPId == null)
+		Map<String, NamedQueryParam> substParams = new HashMap<String, NamedQueryParam>();
+
+		
+		String hql = "getSystemLabelSQLAssoc";
+
+		if (CPId != null)
 		{
-			result = CommonBizlogic.executeHQL("getSystemLabelSQLAssoc", null);
+			substParams.put("0", new NamedQueryParam(DBTypes.LONG, CPId));
+			hql = "getLabelSQLAssocByCPId";
+
 		}
-		else
-		{
-			List<Object> values = new ArrayList<Object>();
-			values.add(Long.valueOf(CPId));
-			result = CommonBizlogic.executeHQL("getLabelSQLAssocByCPId", values);//runs the HQL
-		}
+
+		result = executeHQL(hql, substParams);//runs the HQL
 
 		return (List<LabelSQLAssociation>) result;
 	}
@@ -87,20 +92,21 @@ public class LabelSQLAssociationBizlogic
 		labelSQLAssociation.setSeqOrder(order);
 
 		System.out.println("create session insertLabelSQLAssociation...");
-		Session session = HibernateUtil.newSession();
+//		Session session = HibernateUtil.newSession();
 		System.out.println("create session insertLabelSQLAssociation done...");
 		try
 		{
-			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
-					session);
+//			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
+//					session);
 			System.out.println("insert insertLabelSQLAssociation...");
-			dbHandler.insert(labelSQLAssociation);
+			insert(labelSQLAssociation);
+//			dbHandler.insert(labelSQLAssociation);
 			System.out.println("insert insertLabelSQLAssociation done...");
 		}
 		finally
 		{
 			System.out.println("close session insertLabelSQLAssociation...");
-			session.close();
+			//session.close();
 			System.out.println("close session insertLabelSQLAssociation done...");
 		}
 
@@ -118,20 +124,21 @@ public class LabelSQLAssociationBizlogic
 		labelSQLAssociation = getLabelSQLAssocByCPIdAndLabelSQLId(cpId, labelSQLId);//get the labelSQLAssociation from cpId and labelSQLId
 
 		System.out.println("create session deleteLabelSQLAssociation...");
-		Session session = HibernateUtil.newSession();
+	//	Session session = HibernateUtil.newSession();
 		System.out.println("create session deleteLabelSQLAssociation done...");
 		try
 		{
-			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
-					session);
+//			HibernateDatabaseOperations<LabelSQLAssociation> dbHandler = new HibernateDatabaseOperations<LabelSQLAssociation>(
+//					session);
 			System.out.println("delete deleteLabelSQLAssociation...");
-			dbHandler.delete(labelSQLAssociation);
+			delete(labelSQLAssociation);
+			//dbHandler.delete(labelSQLAssociation);
 			System.out.println("delete deleteLabelSQLAssociation done...");
 		}
 		finally
 		{
 			System.out.println("close session deleteLabelSQLAssociation...");
-			session.close();
+		//	session.close();
 			System.out.println("close session deleteLabelSQLAssociation done...");
 		}
 
@@ -156,12 +163,10 @@ public class LabelSQLAssociationBizlogic
 			String displayName = new CommonBizlogic().getLabelByLabelSQLAssocId(labelSQLAssociation
 					.getId());//retrieve label by association id
 
-			if (query != null && !"".equals(query))
+			if (query != null && !"".equals(query) && !"".equals(displayName)
+					&& displayName != null)
 			{ //Associating the displayname name and association id for dashboard items
-				if (!"".equals(displayName) && displayName != null)
-				{
-					displayNameAssocMap.put(displayName, labelSQLAssociation.getId());
-				}
+				displayNameAssocMap.put(displayName, labelSQLAssociation.getId());
 			}
 			else
 			{
@@ -183,15 +188,20 @@ public class LabelSQLAssociationBizlogic
 	public LabelSQLAssociation getLabelSQLAssocByCPIdAndLabelSQLId(Long cpId, Long labelSQLId)
 			throws Exception
 	{
-		List<Object> values = new ArrayList<Object>();
-		String hql="getSysLabelSQLAssocByCPIdAndLabelSQLId";
-		if(cpId!=null)
+		Map<String, NamedQueryParam> substParams = new HashMap<String, NamedQueryParam>();
+
+		String hql = "getSysLabelSQLAssocByCPIdAndLabelSQLId";
+		if (cpId != null)
 		{
-			hql="getLabelSQLAssocByCPIdAndLabelSQLId";
-			values.add(cpId);
-		} 
-		values.add(labelSQLId);
-		List<?> result = CommonBizlogic.executeHQL(hql, values);
+			hql = "getLabelSQLAssocByCPIdAndLabelSQLId";
+			substParams.put("0", new NamedQueryParam(DBTypes.LONG, cpId));
+			substParams.put("1", new NamedQueryParam(DBTypes.LONG, labelSQLId));
+		}
+		else{
+			substParams.put("0", new NamedQueryParam(DBTypes.LONG, labelSQLId));
+		}
+		
+		List<?> result = executeHQL(hql, substParams);
 		if (result.size() != 0)
 		{
 			return (LabelSQLAssociation) result.get(0);
