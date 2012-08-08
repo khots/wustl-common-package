108ec52e8c1ed8baae7109dec03212c7f83c95cc
ganachas <ganachas>
2012-08-08 13:29:27 +0000

    Added AbStractDTOObject and modified Transformer Interface and related things. Added API's to to transfer "Form To DTO" and "DTO To AbstractDomainObject".
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@3062 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/commonpackage_cp2.2_for_rpr/src/edu/wustl/common/transformer/TransformerUtil.java
+++ branches/commonpackage_cp2.2_for_rpr/src/edu/wustl/common/transformer/TransformerUtil.java
@@ -3,6 +3,7 @@ package edu.wustl.common.transformer;
 import edu.wustl.common.actionForm.AbstractActionForm;
 import edu.wustl.common.actionForm.IValueObject;
 import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.common.dto.AbstractDTOObject;
 import edu.wustl.common.exception.AssignDataException;
 import edu.wustl.common.exception.BizLogicException;
 import edu.wustl.common.factory.AbstractFactoryConfig;
@@ -13,20 +14,62 @@ public class TransformerUtil {
 
 	private static final Logger LOGGER = Logger.getCommonLogger(TransformerUtil.class);
 	
-	public static <T> T transform(IValueObject form, AbstractDomainObject abstractDomain) throws AssignDataException 
+	public static AbstractDomainObject transform(IValueObject form,AbstractDomainObject abstractDomain) throws AssignDataException 
 	{
 		try
 		{
-			T result = null;
 			ITransformerFactory transformerFactory = AbstractFactoryConfig.getInstance().getTransformFactory();
 			AbstractActionForm abstractForm = (AbstractActionForm) form;
 			int formId = abstractForm.getFormId();
-			Transformer<T> transformer = transformerFactory.getTransformer(formId);
+			Transformer transformer = transformerFactory.getTransformer(formId);
 			if (transformer == null) 
 			{
 				throw new RuntimeException("No transformer registered for " + formId);
 			}
-			return transformer.transform(abstractDomain, form);
+			return transformer.transform(abstractDomain,form);
+		}
+		catch (BizLogicException exp) 
+		{
+			LOGGER.error(exp.getMessage(), exp);
+			throw new AssignDataException(null, exp,"Exception occureed during Transformer-"+exp.getMessage());
+		}
+	}
+	
+	
+	public static AbstractDTOObject formToDTOTransform(IValueObject form) throws AssignDataException 
+	{
+		try
+		{
+			ITransformerFactory transformerFactory = AbstractFactoryConfig.getInstance().getTransformFactory();
+			AbstractActionForm abstractForm = (AbstractActionForm) form;
+			int formId = abstractForm.getFormId();
+			Transformer transformer = transformerFactory.getTransformer(formId);
+			if (transformer == null) 
+			{
+				throw new RuntimeException("No transformer registered for " + formId);
+			}
+			return transformer.formToDTOTransform(form);
+		}
+		catch (BizLogicException exp) 
+		{
+			LOGGER.error(exp.getMessage(), exp);
+			throw new AssignDataException(null, exp,"Exception occureed during Transformer-"+exp.getMessage());
+		}
+	}
+	
+	public static AbstractDomainObject dtoToDomainTransform(AbstractDTOObject dto,AbstractDomainObject abstractDomain) throws AssignDataException 
+	{
+		try
+		{
+			ITransformerFactory transformerFactory = AbstractFactoryConfig.getInstance().getTransformFactory();
+			AbstractDTOObject abstractDtonObject = (AbstractDTOObject) dto;
+			int formId = abstractDtonObject.getRelatedFormId();
+			Transformer transformer = transformerFactory.getTransformer(formId);
+			if (transformer == null) 
+			{
+				throw new RuntimeException("No transformer registered for " + formId);
+			}
+			return transformer.dtoToDomainTransform(abstractDtonObject,abstractDomain);
 		}
 		catch (BizLogicException exp) 
 		{
