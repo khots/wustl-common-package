d44927950cbe9cd7a7c4ca544aac75190c8f6540
ganachas <ganachas>
2012-08-08 13:29:27 +0000

    Added AbStractDTOObject and modified Transformer Interface and related things. Added API's to to transfer "Form To DTO" and "DTO To AbstractDomainObject".
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage/branches/commonpackage_cp2.2_for_rpr@3062 38c78225-d16a-144f-a689-2cd41fac0795

--- src/edu/wustl/common/transformer/Transformer.java
+++ src/edu/wustl/common/transformer/Transformer.java
@@ -2,15 +2,17 @@ package edu.wustl.common.transformer;
 
 import edu.wustl.common.actionForm.IValueObject;
 import edu.wustl.common.domain.AbstractDomainObject;
+import edu.wustl.common.dto.AbstractDTOObject;
 import edu.wustl.common.exception.AssignDataException;
 
 /**
  * 
  * @author vinod_gaikwad
  *
- * @param <T>
  */
-public interface Transformer<T> 
+public interface Transformer
 {
-	 public T transform(AbstractDomainObject abstractdominobject,IValueObject form) throws AssignDataException;
+	 public AbstractDomainObject transform(AbstractDomainObject abstractDomainObject,IValueObject form) throws AssignDataException;
+	 public AbstractDTOObject formToDTOTransform(IValueObject form) throws AssignDataException;
+	 public AbstractDomainObject dtoToDomainTransform(AbstractDTOObject abstractDtonObject, AbstractDomainObject abstractDomainObject) throws AssignDataException;
 }
