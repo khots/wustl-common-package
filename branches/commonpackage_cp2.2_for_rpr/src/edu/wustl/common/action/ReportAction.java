d87fdf9cc226983c93cd8a1e1456301201008dc3
sengupts <sengupts>
2012-08-10 10:21:03 +0000

    Added missing changes from 2.5's Common Package
    git-svn-id: https://ncisvn.nci.nih.gov/svn/wustl_common_package/CommonPackage@3063 38c78225-d16a-144f-a689-2cd41fac0795

--- branches/commonpackage_cp2.2_for_rpr/src/edu/wustl/common/action/ReportAction.java
+++ branches/commonpackage_cp2.2_for_rpr/src/edu/wustl/common/action/ReportAction.java
@@ -18,10 +18,11 @@ import edu.wustl.common.report.bean.FileDetails;
 import edu.wustl.common.report.bean.ReportBean;
 import edu.wustl.common.scheduler.bizLogic.ReportAuditDataBizLogic;
 import edu.wustl.common.scheduler.domain.ReportAuditData;
+import edu.wustl.common.scheduler.propertiesHandler.SchedulerConfigurationPropertiesHandler;
 import edu.wustl.common.scheduler.util.ReportSchedulerUtil;
+import edu.wustl.common.scheduler.util.SchedulerDataUtility;
 import edu.wustl.common.util.SendFile;
 import edu.wustl.common.util.Utility;
-
 
 public class ReportAction extends SecureAction
 {
@@ -44,7 +45,8 @@ public class ReportAction extends SecureAction
 			{
 				ReportAuditData customReportAudit = ReportSchedulerUtil.generateTicket(hsForm,
 						sessionDataBean);
-				reportGenerator.generateReport(customReportAudit.getId());
+				reportGenerator.generateReport(customReportAudit.getId(),
+						SchedulerDataUtility.getCustomReportParamList());
 				customReportAudit = (ReportAuditData) new ReportAuditDataBizLogic().retrieve(
 						ReportAuditData.class.getName(), customReportAudit.getId());
 				if (customReportAudit.getJobStatus() != null
